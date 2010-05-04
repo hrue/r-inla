e6555a65b3d732a85a2469a5bf56fcef7fe18fe1
Finn Lindgren <finn.lindgren@gmail.com>
2010-05-04 13:16:45 +0200

    Prepared for alternate Steiner point methods.

--- fmesher/src/meshc.h
+++ fmesher/src/meshc.h
@@ -186,7 +186,7 @@ namespace fmesh {
     Dart splitTriangleDelaunay(const Dart& td, int v);
     Dart splitEdgeDelaunay(const Dart& ed, int v);
     Dart bisectEdgeDelaunay(const Dart& d);
-    bool killTriangle(const Dart& d);
+    void calcSteinerPoint(const Dart& d, Point& c);
     bool insertNode(int v, const Dart& ed);
 
     bool isSegment(const Dart& d) const;
