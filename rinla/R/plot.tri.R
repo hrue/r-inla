757be33e36326392022e0c130153960e6ee00042
Finn Lindgren <finn.lindgren@gmail.com>
2010-06-03 18:55:51 +0200

    Rudimentary trimesh plotting function for R

---
+++ rinla/R/plot.tri.R
@@ -0,0 +1,34 @@
+`plot.inla.trimesh` =
+  function(TV,S,color = NULL)
+  {
+    tTV = t(TV);
+    tETV = t(TV[,c(1,2,3,1,NA)]);
+    Tx = S[tTV,1]
+    Ty = S[tTV,2]
+    Tz = S[tTV,3]
+    Tcol = color[tTV]
+    Ex = S[tETV,1]
+    Ey = S[tETV,2]
+    Ez = S[tETV,3]
+    Ecol = rgb(0.3,0.3,0.3)
+    points3d(S,color="black")
+    lines3d(Ex,Ey,Ez,color=Ecol,lwd=1)
+    triangles3d(Tx,Ty,Tz,color=Tcol,specular="black")
+  }
+
+# library(geometry)
+# S = cbind(x=rnorm(30), y=rnorm(30), z=0)
+# TV = delaunayn(S[,1:2]) # NOTE: inconsistent triangle orders, only for test.
+# trimesh(TV,S)
+#
+# colors = rgb(runif(30),runif(30),runif(30))
+# rgl.viewpoint(0,0,fov=20)
+# plot.inla.trimesh(TV,S,colors)
+
+#    Ecol = col2rgb(color)/256
+##    Ecol = Ecol*0.5+(1-0.5)*0 # Rescale towards black
+#    Ecol = 1-Ecol # Invert
+##    Ecol = Ecol[,c(2,3,1)] # Permute
+#    Ecol = rgb(Ecol[1,],Ecol[2,],Ecol[3,],maxColorValue = 1)
+#    Ecol = Ecol[tETV]
+
