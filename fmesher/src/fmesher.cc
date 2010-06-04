37b75af2e6f52d6fd7d882eeba78e58fa760b505
Finn Lindgren <finn.lindgren@gmail.com>
2010-06-04 20:04:37 +0200

    Initial command line handling in fmesher

--- fmesher/src/fmesher.cc
+++ fmesher/src/fmesher.cc
@@ -12,6 +12,7 @@ using std::ios;
 using std::ifstream;
 using std::ofstream;
 using std::string;
+using std::cin;
 using std::cout;
 using std::endl;
 
@@ -34,9 +35,9 @@ using fmesh::Vector3;
 
 const bool useVT = true;
 const bool useTTi = true;
-const bool useX11 = true;
+bool useX11 = true;
 const bool useX11text = false;
-const double x11_delay_factor = 1.0;
+double x11_delay_factor = 1.0;
 
 
 template <class T>
@@ -101,16 +102,90 @@ void print_SM_old(string filename,
 
 
 
-int main(int argc, const char* argv[])
+int main(int argc, char* argv[])
 {
-  string prefix("fmesher.io.");
-  if (argc>=2)
-    prefix = string(argv[1]);
+  
+  gengetopt_args_info args_info;
+  struct cmdline_params params;
+  
+  /* initialize the parameters structure */
+  cmdline_params_init(&params);
+     
+  /* call the command line parser */
+  if (cmdline(argc, argv, &args_info) != 0) {
+    cmdline_free(&args_info);
+    return 1;
+  }
+
+  cout << "Config given:\t" << args_info.config_given << endl;
+    
+  /* Read an optional config file, but don't override given options */
+  if (args_info.config_given) {
+    params.initialize = 0;
+    params.override = 0;
+    /* Call the config file parser */
+    if (cmdline_config_file(args_info.config_arg, &args_info, &params) != 0) {
+      cmdline_free(&args_info);
+      return 1;
+    }
+  }
+
+  int cet_sides = 8;
+  double cet_margin = -0.05;
+  if ((args_info.cet_given>0) && (args_info.cet_arg[0] != 0))
+    cet_sides = args_info.cet_arg[0];
+  if (args_info.cet_given>1)
+    cet_margin = args_info.cet_arg[1];
+
+  double rcdt_min_angle = 21;
+  double rcdt_big_limit = -1.0;
+  double rcdt_big_limits = -0.5;
+  if ((args_info.rcdt_given>0) && (args_info.rcdt_arg[0] != 0))
+    rcdt_min_angle = args_info.rcdt_arg[0];
+  if (args_info.rcdt_given>1)
+    rcdt_big_limit = args_info.rcdt_arg[1];
+  if (args_info.rcdt_given>2)
+    rcdt_big_limits = args_info.rcdt_arg[2];
+
+  useX11 = (args_info.x11_given>0);
+  x11_delay_factor = args_info.x11_arg;
+
+  cout << "CET given:\t" << args_info.cet_given << endl;
+  cout << "RCDT given:\t" << args_info.rcdt_given << endl;
+  cout << "X11 given:\t" << args_info.x11_given << endl;
+
+  cout << "CET sides:\t" << cet_sides << endl;
+  cout << "CET margin:\t" << cet_margin << endl;
+  cout << "RCDT mininmum angle:\t" << rcdt_min_angle << endl;
+  cout << "RCDT maximum edge length:\t" << rcdt_big_limit << endl;
+  cout << "RCDT maximum edge lengths:\t" << rcdt_big_limits << endl;
+  cout << "X11 delay factor:\t" << x11_delay_factor << endl;
+
+
+
+  string iprefix("-");
+  string oprefix("-");
+  if (args_info.inputs_num>0)
+    iprefix = string(args_info.inputs[0]);
+  if (iprefix=="-")
+    oprefix = "fmesher.output.";
+  else
+    oprefix = iprefix;
+  if (args_info.inputs_num>1)
+    oprefix = string(args_info.inputs[1]);
+
+
+  cout << "Input:\t" << iprefix << endl;
+  cout << "Output:\t" << oprefix << endl;
 
   Matrix<double> iS0;
-  ifstream I((prefix+"s0").c_str(), ios::in | ios::binary);
-  IOHelperM<double>().D(&iS0).binary().IH(I).ID(I).ascii().OH(cout << "S0: ");
-  I.close();
+  if (iprefix=="-") {
+    IOHelperM<double>().D(&iS0).binary().IH(cin).ID(cin).ascii().OH(cout << "S0: ");
+  } else {
+    ifstream I((iprefix+"s0").c_str(), ios::in | ios::binary);
+    IOHelperM<double>().D(&iS0).binary().IH(I).ID(I).ascii().OH(cout << "S0: ");
+    I.close();
+  }
 
   /* Check the input. */
   if (iS0.cols()<2) {
@@ -143,7 +218,6 @@ int main(int argc, const char* argv[])
 
   cout << "Initial mesh:" << endl << M;
 
-  /* Rudimentary default biglimit construction: */
   Point mini(S0(0));
   Point maxi(S0(0));
   for (int v=1; v<nV; v++)
@@ -159,8 +233,18 @@ int main(int argc, const char* argv[])
 	  : (sz[2] < sz[1] ? sz[1] : sz[2]));
 
   double biglim[nV];
-  for (int v=0;v<nV;v++)
-    biglim[v] = diam/std::sqrt(nV)*0.5;
+  if (rcdt_big_limits==0.0) {
+    /* Read limits from a file */
+    NOT_IMPLEMENTED;
+    return 0;
+  } else if (rcdt_big_limits>0.0) {
+    for (int v=0;v<nV;v++)
+      biglim[v] = rcdt_big_limits;
+  } else {
+    /* Rudimentary default biglimit construction: */
+    for (int v=0;v<nV;v++)
+      biglim[v] = diam/std::sqrt(nV)*(-rcdt_big_limits);
+  }
 
   if (useX11) {
     if (issphere) {
@@ -182,23 +266,27 @@ int main(int argc, const char* argv[])
   fmesh::vertexListT vertices;
   for (int v=0;v<nV;v++)
     vertices.push_back(v);
-  MC.CET(8,-0.1);
+  MC.CET(cet_sides,cet_margin);
   MC.DT(vertices);
 
-  /* Calculate the RCDT: */
-  MC.RCDT(21,diam*2.0,biglim,nV);
+  if (args_info.rcdt_given) {
+    /* Calculate the RCDT: */
+    if (rcdt_big_limit<0.0)
+      rcdt_big_limit = -rcdt_big_limit*diam;
+    MC.RCDT(rcdt_min_angle,rcdt_big_limit,biglim,nV);
+  }
 
   cout << "Final mesh:" << endl << M;
 
-  print_M(prefix+"s",M.S());
-  print_M(prefix+"tv",M.TV());
-  print_M(prefix+"tt",M.TT());
+  print_M(oprefix+"s",M.S());
+  print_M(oprefix+"tv",M.TV());
+  print_M(oprefix+"tt",M.TT());
   M.useTTi(true);
-  print_M(prefix+"tti",M.TTi());
-  print_SM(prefix+"vv",M.VV());
+  print_M(oprefix+"tti",M.TTi());
+  print_SM(oprefix+"vv",M.VV());
 
-  print_M_old(prefix+"S.dat",M.S());
-  print_M_old(prefix+"FV.dat",M.TV(),false);
+  print_M_old(oprefix+"S.dat",M.S());
+  print_M_old(oprefix+"FV.dat",M.TV(),false);
 
   {
     SparseMatrix<double> C0;
@@ -210,15 +298,15 @@ int main(int argc, const char* argv[])
 
     K = G-B1;
 
-    print_SM(prefix+"c0",C0,fmesh::IOMatrixtype_diagonal);
-    print_SM(prefix+"c1",C1,fmesh::IOMatrixtype_symmetric);
-    print_SM(prefix+"b1",B1,fmesh::IOMatrixtype_symmetric);
-    print_SM(prefix+"g1",G,fmesh::IOMatrixtype_symmetric);
-    print_SM(prefix+"k1",K,fmesh::IOMatrixtype_symmetric);
+    print_SM(oprefix+"c0",C0,fmesh::IOMatrixtype_diagonal);
+    print_SM(oprefix+"c1",C1,fmesh::IOMatrixtype_symmetric);
+    print_SM(oprefix+"b1",B1,fmesh::IOMatrixtype_symmetric);
+    print_SM(oprefix+"g1",G,fmesh::IOMatrixtype_symmetric);
+    print_SM(oprefix+"k1",K,fmesh::IOMatrixtype_symmetric);
 
-    print_SM_old(prefix+"C.dat",C0,true,fmesh::IOMatrixtype_diagonal);
-    print_SM_old(prefix+"G.dat",G,false,fmesh::IOMatrixtype_symmetric);
-    print_SM_old(prefix+"K.dat",K,false,fmesh::IOMatrixtype_symmetric);
+    print_SM_old(oprefix+"C.dat",C0,true,fmesh::IOMatrixtype_diagonal);
+    print_SM_old(oprefix+"G.dat",G,false,fmesh::IOMatrixtype_symmetric);
+    print_SM_old(oprefix+"K.dat",K,false,fmesh::IOMatrixtype_symmetric);
 
     SparseMatrix<double> C0inv = inverse(C0,true);
     SparseMatrix<double> tmp = G*C0inv;
@@ -226,8 +314,8 @@ int main(int argc, const char* argv[])
       G = tmp*G;
       std::stringstream ss;
       ss << i+2;
-      print_SM(prefix+"g"+ss.str(),G,fmesh::IOMatrixtype_symmetric);
-      print_SM_old(prefix+"G"+ss.str()+".dat",G,
+      print_SM(oprefix+"g"+ss.str(),G,fmesh::IOMatrixtype_symmetric);
+      print_SM_old(oprefix+"G"+ss.str()+".dat",G,
 		   false,fmesh::IOMatrixtype_symmetric);
     }
     tmp = C0inv*K;
@@ -235,13 +323,15 @@ int main(int argc, const char* argv[])
       K = K*tmp;
       std::stringstream ss;
       ss << i+2;
-      print_SM(prefix+"k"+ss.str(),K,fmesh::IOMatrixtype_symmetric);
-      print_SM_old(prefix+"K"+ss.str()+".dat",K,
+      print_SM(oprefix+"k"+ss.str(),K,fmesh::IOMatrixtype_symmetric);
+      print_SM_old(oprefix+"K"+ss.str()+".dat",K,
 		   false,fmesh::IOMatrixtype_symmetric);
     }
 
 
   }
+
+  cmdline_free(&args_info);
 
   return 0;
 }
