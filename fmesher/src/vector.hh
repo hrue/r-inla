3302b58affef9f4ffb76c0cbe01ecdc7e0f9e2b0
Finn Lindgren <finn.lindgren@gmail.com>
2010-06-08 21:33:32 +0200

    Code refactor, and mesh node areas output.

--- fmesher/src/vector.hh
+++ fmesher/src/vector.hh
@@ -38,6 +38,10 @@ namespace fmesh {
   template <class T>
   SparseMatrix<T> inverse(const SparseMatrix<T>& M1,
 			  bool diagonal = false);
+  template <class T>
+  SparseMatrix<T> diag(const Matrix<T>& M1);
+  template <class T>
+  Matrix<T> diag(const SparseMatrix<T>& M1);
 
 
 
@@ -69,30 +73,13 @@ namespace fmesh {
 			      cap_(0) {
       cols(set_cols);
     };
-    Matrix(size_t set_rows, size_t set_cols, const T* vals = NULL)
-      : data_(NULL), rows_(0), cols_(0), cap_(0) {
-      cols(set_cols);
-      capacity(set_rows);
-      rows_ = set_rows;
-      if (vals) {
-	std::memcpy(data_,vals,sizeof(T)*rows_*cols_);
-      }
-    };
-    const Matrix<T>& operator=(const Matrix<T>& from) {
-      clear();
-      cols(from.cols_);
-      capacity(from.cap_);
-      rows_ = from.rows_;
-      if (data_) {
-	std::memcpy(data_,from.data_,sizeof(T)*rows_*cols_);
-      }
-      return *this;
-    };
+    Matrix(size_t set_rows, size_t set_cols, const T* vals = NULL);
+    const Matrix<T>& operator=(const Matrix<T>& from);
     ~Matrix() {
       if (data_)
 	delete[] data_;
     };
-    void clear(void) {
+    Matrix<T>& clear(void) {
       if (data_) {
 	delete[] data_;
 	data_ = NULL;
@@ -100,6 +87,7 @@ namespace fmesh {
       cap_ = 0;
       rows_ = 0;
       cols_ = 0;
+      return *this;
     };
     void zeros(const size_t from_row = 0) {
       for (size_t i=from_row*cols_; i<cap_*cols_; i++)
@@ -111,79 +99,14 @@ namespace fmesh {
     }
 
     size_t capacity() const { return cap_; };
-    bool capacity(size_t cap) {
-      if (cap <= cap_) {
-	return true;
-      }
-      size_t old_cap = cap_;
-      if ((cap_==0) && 
-	  (cap < capacity_step_size_))
-	cap_ = cap;
-      while (cap > cap_) {
-	if (cap_ < capacity_step_size_)
-	  cap_ = capacity_step_size_;
-	else {
-	  if (cap_ < capacity_doubling_limit_)
-	    cap_ *= 2;
-	  else
-	    cap_ += capacity_step_size_;
-	}
-      }
+    bool capacity(size_t cap);
 
-      T* data_new_ = new T[cap_*cols_];
+    bool append(const Matrix<T>& toappend);
 
-      if (data_) { /* Copy existing data: */
-	std::memcpy(data_new_,data_,
-		    sizeof(T)*old_cap*cols_);
-	delete[] data_;
-      }
-
-      data_ = data_new_;
-      zeros(old_cap);
-      return true;
-    };
-
-    bool append(const Matrix<T>& toappend) {
-      if (cols_ != toappend.cols_) return false;
-      if (!capacity(rows_+toappend.rows_)) return false;
-      std::memcpy(data_+rows_*cols_,toappend.data_,
-		  sizeof(T)*toappend.rows_*cols_);
-      rows_ += toappend.rows_;
-      return true;
-    };
-
-    int rows(size_t set_rows) {
-      if (set_rows>rows_)
-	capacity(set_rows);
-      else if (set_rows<rows_)
-	truncate(set_rows);
-      return rows_;
-    };
+    Matrix<T>& rows(size_t set_rows);
     int rows(void) const { return rows_; };
     int cols(void) const { return cols_; };
-    int cols(size_t set_cols) {
-      /* When set, cannot alter number of columns,
-	 unless we only need to expand a single row. */
-      if ((cols_ > 0) &&
-	  (!((rows_<=1) && cols_<=set_cols))) {
-	return cols_;
-      }
-      if ((cols_>0) && (rows_>0)) { /* We already have some data, and
-				       need to carefully make sure the
-				       capacity is enough */
-	/* Pre-data-size: cap_*cols_
-	   Post-data_size needed cap_*set_cols
-	   capacity is set as r*cols_
-	   Requirement: r*cols_ >= cap_*set_cols
-	   r = (cap_*set_cols)/cols_+1
-	 */
-	capacity((cap_*set_cols)/cols_+1);
-	cols_ = set_cols;
-	cap_ = rows_; /* This makes sure we dont' overestimate the true cap_. */
-      } else
-	cols_ = set_cols;
-      return cols_;
-    };
+    Matrix<T>& cols(size_t set_cols);
 
     const T (* operator[](const int r) const) {
       if (r >= (int)rows_) {
@@ -204,22 +127,20 @@ namespace fmesh {
       if (c >= (int)cols_)
 	cols(c+1);
       return operator()(r)[c]; 
-      /*
-      if (r >= (int)rows_) {
-	capacity(r+1);
-	rows_ = r+1;
-      }
-      return data_[r*cols_+c]; 
-      */
     };
 
     const T& operator()(const int r, const int c, const T& val) {
-      operator()(r,c) = val; 
-      return data_[r*cols_+c]; 
+      return (operator()(r,c) = val); 
     };
 
     const T (* raw(void) const) { return data_; }
     T* raw(void) { return data_; }
+
+
+    friend
+    SparseMatrix<T> diag<T>(const Matrix<T>& M1);
+    friend
+    Matrix<T> diag<T>(const SparseMatrix<T>& M1);
 
   };
 
@@ -370,9 +291,10 @@ namespace fmesh {
     Matrix1() : Matrix<T>(1) {};
     Matrix1(size_t set_rows, const ValueRaw* vals = NULL)
       : Matrix<T>(set_rows,1,(T*)vals) {};
-    void clear(void) {
+    Matrix1<T>& clear(void) {
       Matrix<T>::clear();
       Matrix<T>::cols(1);
+      return *this;
     };
 
     const T& operator[](const int r) const {
@@ -406,9 +328,10 @@ namespace fmesh {
       : Matrix<T>(set_rows,3,(T*)vals) {};
     Matrix3(size_t set_rows, const ValueRaw* vals = NULL)
       : Matrix<T>(set_rows,3,(T*)vals) {};
-    void clear(void) {
+    Matrix3<T>& clear(void) {
       Matrix<T>::clear();
       Matrix<T>::cols(3);
+      return *this;
     };
 
     const ValueRow& operator[](const int r) const {
@@ -740,6 +663,10 @@ namespace fmesh {
     friend
     SparseMatrix<T> inverse<T>(const SparseMatrix<T>& M1,
 			       bool diagonal);
+    friend
+    SparseMatrix<T> diag<T>(const Matrix<T>& M1);
+    friend
+    Matrix<T> diag<T>(const SparseMatrix<T>& M1);
 
 
     /*! To list, general, symmetric, or diagonal. */
@@ -919,83 +846,13 @@ namespace fmesh {
 
 
 
-  template <class T>
-  SparseMatrix<T> operator*(const SparseMatrix<T>& M1,
-			    const SparseMatrix<T>& M2)
-  {
-    SparseMatrix<T> M;
-    int M1rows = M1.rows();
-    int M2rows = M2.rows();
-    M.cols(M2.cols()).rows(M1rows);
-    for (int i=0; i<M1rows; i++) {
-      SparseMatrixRow<T>& Mi = M(i);
-      const SparseMatrixRow<T>& M1i = M1[i];
-      if (M1i.size() > 0) {
-	for (typename SparseMatrixRow<T>::ColCIter M1k = M1i.begin();
-	     (M1k != M1i.end()) && (M1k->first < M2rows);
-	     M1k++) {
-	  int k = M1k->first;
-	  const T& M1ik = M1i[k];
-	  const SparseMatrixRow<T>& M2k = M2[k];
-	  for (typename SparseMatrixRow<T>::ColCIter M2j = M2k.begin();
-	       (M2j != M2k.end());
-	       M2j++) {
-	    Mi(M2j->first) += (M1ik * M2j->second);
-	  }
-	}
-      }
-    }
-    return M;
-  }
-
-  template <class T>
-  SparseMatrix<T> operator-(const SparseMatrix<T>& M1,
-			    const SparseMatrix<T>& M2)
-  {
-    SparseMatrix<T> M(M1);
-    for (int r=0; (r<M1.rows()) && (r<M2.rows()); r++) {
-      SparseMatrixRow<T>& Mr = M(r);
-      const SparseMatrixRow<T>& M2r = M2[r];
-      for (typename SparseMatrixRow<T>::ColCIter c = M2r.begin();
-	   (c != M2r.end()) && (c->first < M1.cols());
-	   c++) {
-	Mr(c->first) -= c->second;
-      }
-    }
-    return M;
-  }
-
-  template <class T>
-  SparseMatrix<T> inverse(const SparseMatrix<T>& M1,
-			  bool diagonal)
-  {
-    SparseMatrix<T> M;
-    M.cols(M1.cols()).rows(M1.rows());
-    if (!diagonal) {
-      /* NOT IMPLEMENTED */
-      return M;
-    }
-    for (int r=0; (r<M1.rows()) && (r<M1.cols()); r++) {
-      const T& val = M1[r][r];
-      if (!(val==T()))
-	M(r,r) = 1/val;
-    }
-    return M;
-  }
 
 
 
-
-
-
-  template <class T>
-  const T fmesh::Matrix<T>::zero_ = T();
-  template <class T>
-  const T fmesh::SparseMatrixRow<T>::zero_ = T();
-  template <class T>
-  const T fmesh::SparseMatrix<T>::zero_ = T();
 
 
 } /* namespace fmesh */
+
+#include "vector.tcc"
 
 #endif
