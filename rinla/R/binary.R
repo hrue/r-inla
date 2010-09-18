a5d9703965318ed81a11a43fa5da340793a700c6
Håvard Rue <haavard.rue@kaust.edu.sa>
2010-09-19 00:11:14 +0200

    Did work on the functions in binary.R with a huge speedup...

--- rinla/R/binary.R
+++ rinla/R/binary.R
@@ -1,52 +1,148 @@
 ### Binary I/O and interpretation of vectors
 
+## keep old version in the source for a while...
+
+`inla.interpret.vector.OLDVERSION` = function(xx)
+{
+    if (is.null(xx))
+        return (NULL)
+    
+    len = length(xx)
+    ii = 1L
+    res = numeric()
+    by = 2L
+    np = NA
+    while(ii <= len) {
+        np = as.integer(xx[ii+1L])
+        if (np > 0) {
+            start = ii + 2L
+            end = ii + 2L*np + 1L
+            idx = seq.int(start, end, by=by)
+            res = cbind(res, xx[idx], xx[idx+1L])
+            ii = end + 1L
+        } else {
+            ii = ii + 2L
+        }
+    }
+    return(res)
+}
+
 `inla.interpret.vector` = function(xx)
 {
     if (is.null(xx))
         return (NULL)
     
+    ## This new version first determine the size of the matrix, then
+    ## just fills in the matrix. This is much more efficient than
+    ## doing this dynamically.
+
     len = length(xx)
-    ii = 1
+    ii = 1L
     res = numeric()
-    by = 2
+    by = 2L
+    m = 0L
+    np = NA
+    ## first do the counting
     while(ii <= len) {
-        np = xx[ii+1]
+        np = as.integer(xx[ii + 1L])
+        if (np) {
+            m = m + 2L
+        }
+        ii = ii + 2L*np + 2L
+    }
+
+    ## then allocate the storage
+    res.ncol = m
+    res = matrix(NA, np, res.ncol)
+
+    m = 1L
+    ii = 1L
+    while(ii <= len) {
+        np = as.integer(xx[ii+1L])
         if (np > 0) {
-            start = ii+2
-            end = ii+2*np+1
-            res = cbind(res,cbind(xx[seq(start, end, by=by)], xx[seq(start+1, end, by=by)]))
-            ii = end + 1
+            start = ii + 2L
+            end = ii + 2L*np + 1L
+            idx = seq.int(start, end, by=by)
+            res[, m] = xx[idx]
+            res[, m+1] = xx[idx+1]
+            m = m + 2L
+            ii = end + 1L
         } else {
-            ii = ii + 2
+            ii = ii + 2L
         }
     }
-    rm(xx)
+    stopifnot(res.ncol == m-1L) ## since we start m at 1L
     return(res)
 }
 
-`inla.interpret.vector.idx` = function(xx)
+`inla.interpret.vector.idx.OLDVERSION` = function(xx)
 {
     if (is.null(xx))
         return (NULL)
     
     len = length(xx)
-    ii = 1
+    ii = 1L
     res = numeric()
-    by = 2
+    by = 2L
+    np = NA
     while(ii <= len) {
-        idx = xx[ii]
-        np = xx[ii+1]
+        idx = as.integer(xx[ii])
+        np = as.integer(xx[ii + 1L])
         if (np > 0) {
-            start = ii+2
-            end = ii+2*np+1
-            res = cbind(res,cbind(rep(idx, length(seq(start,end,by=by))),
-                xx[seq(start, end, by=by)], xx[seq(start+1, end, by=by)]))
-            ii = end + 1
+            start = ii + 2L
+            end = ii + 2L*np + 1L
+            jj = seq.int(start, end, by=by)
+            res = cbind(res, rep(idx, length(jj)), xx[jj], xx[jj + 1L])
+            ii = end + 1L
         } else {
-            ii = ii + 2;
+            ii = ii + 2L;
         }
     }
-    rm(xx)
+    return(res)
+}
+
+`inla.interpret.vector.idx` = function(xx)
+{
+    ## same here, count first...
+
+    if (is.null(xx))
+        return (NULL)
+    
+    len = length(xx)
+    ii = 1L
+    res = numeric()
+    by = 2L
+    m = 0L
+    np = NA
+    while(ii <= len) {
+        np = as.integer(xx[ii + 1L])
+        if (np) {
+            m = m + 3L
+        }
+        ii = ii + 2L*np + 2L
+    }
+    res.ncol = m
+    res = matrix(NA, np, res.ncol)
+
+    m = 1L
+    ii = 1L
+    while(ii <= len) {
+        idx = as.integer(xx[ii])
+        np = as.integer(xx[ii+1L])
+        if (np > 0) {
+            start = ii + 2L
+            end = ii + 2L*np + 1L
+            jj = seq.int(start, end, by=by)
+            res[, m] = idx
+            res[, m+1] = xx[jj]
+            res[, m+2] = xx[jj+1L]
+            m = m + 3L
+            ii = end + 1L
+        } else {
+            ii = ii + 2L;
+        }
+    }
+    stopifnot(res.ncol == m-1L) ## since we start from 1L
     return(res)
 }
 
@@ -56,23 +152,23 @@
         return (NULL)
     
     len = length(xx)
-    ind = ii = 1
+    ind = ii = 1L
     res = list()
-    by=2
+    by=2L
     while(ii <= len) {
-        np = xx[ii+1]
+        np = as.integer(xx[ii+1L])
         if (np>0) {
-            start = ii+2
-            end = ii+2*np+1
-            res[[ind]] = cbind(xx[seq(start, end, by=by)], xx[seq(start+1, end, by=by)])
-            ind = ind+1
-            ii = end + 1
+            start = ii + 2L
+            end = ii + 2L*np + 1L
+            jj = seq.int(start, end, by=by)
+            res[[ind]] = cbind(xx[jj], xx[jj+1L])
+            ind = ind + 1L
+            ii = end + 1L
         } else {
             res[[ind]] = NA
-            ii = ii + 2
+            ii = ii + 2L
         }
     }
-    rm(xx)
     return(res)
 }
 
