67af81c1aaf1894c0d05999bd3f0cab9d3934d8a
Havard Rue <havard.rue@math.ntnu.no>
2010-09-18 22:23:59 +0200

    added class to the output from inla.fmesher.mesh(), and a plot function for it to plot the mesh. Did some indenting of the code; seems like FL and me have different setup...

--- rinla/R/spde.R
+++ rinla/R/spde.R
@@ -1,17 +1,17 @@
 ## create a spde model using fmesher.
 
 `inla.fmesher.call` = function(
-  fmesher.call = inla.getOption("fmesher.call"),
-  all.args,prefix)
-  {
+        fmesher.call = inla.getOption("fmesher.call"),
+        all.args,prefix)
+{
     if (inla.os("linux") || inla.os("mac")) {
         echoc = system(paste(shQuote(fmesher.call), all.args, prefix))
     }
     else if(inla.os("windows")) {
         if (!remote) {
             echoc = try(system(paste(shQuote(fmesher.call),
-                                     all.args,
-                                     prefix)), silent=TRUE)
+                    all.args,
+                    prefix)), silent=TRUE)
             echoc = 0
         } else {
             echoc = try(inla.cygwin.run.command(
@@ -23,81 +23,81 @@
     }
     else
         stop("\n\tNot supported architecture.")
-  }
+}
 
 `inla.fmesher.make.prefix` = function(dir = tempdir(), prefix = NULL)
 {
-  if (is.null(prefix)) {
-    if (is.null(dir))
-      prefix = paste(tempfile(), ".", sep="")
-    else {
-      ## create the directory if not already there.
-      if (!inla.is.dir(dir)) {
-        if (!dir.create(dir, recursive=TRUE)) {
-          stop(paste("Failed to create directory:", dir))
+    if (is.null(prefix)) {
+        if (is.null(dir))
+            prefix = paste(tempfile(), ".", sep="")
+        else {
+            ## create the directory if not already there.
+            if (!inla.is.dir(dir)) {
+                if (!dir.create(dir, recursive=TRUE)) {
+                    stop(paste("Failed to create directory:", dir))
+                }
+            }
+            prefix = paste(tempfile(tmpdir=dir), ".", sep="")
         }
-      }
-      prefix = paste(tempfile(tmpdir=dir), ".", sep="")
+    } else {
+        ## Make sure the prefix directory exists.
+        if (!inla.is.dir(inla.dirname(prefix))) {
+            if (!dir.create(inla.dirname(prefix), recursive=TRUE)) {
+                stop(paste("Failed to create directory:", inla.dirname(prefix)))
+            }
+        }
     }
-  } else {
-    ## Make sure the prefix directory exists.
-    if (!inla.is.dir(inla.dirname(prefix))) {
-      if (!dir.create(inla.dirname(prefix), recursive=TRUE)) {
-        stop(paste("Failed to create directory:", inla.dirname(prefix)))
-      }
-    }
-  }
-  return (prefix)
+    return (prefix)
 }
 
 `inla.create.spde` = function(mesh=NULL, prefix = NULL,
-  fem=NULL, sph0=NULL, sph=NULL, bspline=NULL)
-  {
+        fem=NULL, sph0=NULL, sph=NULL, bspline=NULL)
+{
     ## create files using fmesher
 
     if (is.null(mesh) && is.null(prefix))
-      stop("At least one of mesh and prefix must be specified.")
+        stop("At least one of mesh and prefix must be specified.")
 
     prefix = inla.fmesher.make.prefix(NULL,prefix)
 
     if (is.null(mesh)) {
-      ## Need to know the size of the graph.
-      s = inla.read.fmesher.file(paste(prefix, "s", sep=""))
-      stopifnot(is.matrix(s))
-      n = nrow(s)
-      s.dim = ncol(s)
+        ## Need to know the size of the graph.
+        s = inla.read.fmesher.file(paste(prefix, "s", sep=""))
+        stopifnot(is.matrix(s))
+        n = nrow(s)
+        s.dim = ncol(s)
     } else {
-      stopifnot(is.matrix(mesh$s))
-      n = nrow(mesh$s)
-      s.dim = ncol(mesh$s)
+        stopifnot(is.matrix(mesh$s))
+        n = nrow(mesh$s)
+        s.dim = ncol(mesh$s)
     }
 
     if (s.dim==1)
-      stop("1-D models not implemented yet.")
+        stop("1-D models not implemented yet.")
     stopifnot(s.dim>=2, s.dim<=3)
 
     if (!is.null(mesh)) {
-      stopifnot(is.matrix(mesh$tv))
-      inla.write.fmesher.file(mesh$s, filename = paste(prefix, "s", sep=""))
-      inla.write.fmesher.file(inla.affirm.integer(mesh$tv-1L),
-                              filename = paste(prefix, "tv", sep=""))
+        stopifnot(is.matrix(mesh$tv))
+        inla.write.fmesher.file(mesh$s, filename = paste(prefix, "s", sep=""))
+        inla.write.fmesher.file(inla.affirm.integer(mesh$tv-1L),
+                                filename = paste(prefix, "tv", sep=""))
     }
     all.args = "--input=s,tv"
 
     ## additional argumets
     if (!is.null(fem)) {
-      all.args = paste(all.args," --fem=",fem,sep="")
+        all.args = paste(all.args," --fem=",fem,sep="")
     }
     if (!is.null(sph0)) {
-      all.args = paste(all.args," --sph0=",sph0,sep="")
+        all.args = paste(all.args," --sph0=",sph0,sep="")
     }
     if (!is.null(sph)) {
-      all.args = paste(all.args," --sph=",sph,sep="")
+        all.args = paste(all.args," --sph=",sph,sep="")
     }
     if (!is.null(bspline)) {
-      all.args = paste(all.args," --bspline=",
-        bspline[1],",",bspline[2],",",bspline[3],
-        sep="")
+        all.args = paste(all.args," --bspline=",
+                bspline[1],",",bspline[2],",",bspline[3],
+                sep="")
     }
     all.args = paste(all.args, inla.getOption("fmesher.arg"))
 
@@ -106,18 +106,14 @@
     return (list(prefix = prefix, locations = mesh$s, n = n))
 }
 
-
-
-
-
 `inla.fmesher.mesh` = function(
-  locations,
-  boundary = NULL,
-  dir = tempdir(),
-  prefix = NULL,
-  rcdt = c(21,-1.0,-0.5),
-  x11.delay = NULL,
-  min.input.distance = 0)
+        locations,
+        boundary = NULL,
+        dir = tempdir(),
+        prefix = NULL,
+        rcdt = c(21,-1.0,-0.5),
+        x11.delay = NULL,
+        min.input.distance = 0)
 {
 
     ## create files using fmesher
@@ -128,7 +124,7 @@
     stopifnot(loc.dim>=2, loc.dim<=3)
 
     if (!is.null(boundary))
-      stop("Boundary handling is not implemented in the R interface yet.")
+        stop("Boundary handling is not implemented in the R interface yet.")
 
     prefix = inla.fmesher.make.prefix(dir,prefix)
 
@@ -141,26 +137,26 @@
     node.coord[node.i.max,] = locations[loc.i,]
     map.loc.to.node[[loc.i]] = node.i.max
     for (loc.i in 2:loc.n) {
-      loc.to.node.dist = 
-        sqrt(rowSums((as.matrix(rep(1,node.i.max)) %*% locations[loc.i,] -
-                      node.coord[1:node.i.max,])^2))
-      if (min(loc.to.node.dist) > min.input.distance) {
-        node.i.max = node.i.max+1
-        node.coord[node.i.max,] = locations[loc.i,]
-        map.loc.to.node[[loc.i]] = node.i.max
-      } else {
-        excluded = c(excluded,loc.i)
-      }
+        loc.to.node.dist = 
+            sqrt(rowSums((as.matrix(rep(1,node.i.max)) %*% locations[loc.i,] -
+                          node.coord[1:node.i.max,])^2))
+        if (min(loc.to.node.dist) > min.input.distance) {
+            node.i.max = node.i.max+1
+            node.coord[node.i.max,] = locations[loc.i,]
+            map.loc.to.node[[loc.i]] = node.i.max
+        } else {
+            excluded = c(excluded,loc.i)
+        }
     } 
     ## Remove excess nodes.
     node.coord = node.coord[1:node.i.max,]
     ## Identify nearest nodes for excluded locations.
     for (loc.i in excluded) {
-      loc.to.node.dist = 
-        sqrt(rowSums((as.matrix(rep(1,node.i.max)) %*% locations[loc.i,] -
-                      node.coord)^2))
-      node.i = which.min(loc.to.node.dist);
-      map.loc.to.node[loc.i] = node.i
+        loc.to.node.dist = 
+            sqrt(rowSums((as.matrix(rep(1,node.i.max)) %*% locations[loc.i,] -
+                          node.coord)^2))
+        node.i = which.min(loc.to.node.dist);
+        map.loc.to.node[loc.i] = node.i
     }
 
     s0 = node.coord
@@ -168,23 +164,41 @@
 
     ## The default name for the input locations in fmesher is s0:
     loc.file = inla.write.fmesher.file(s0,
-      filename = paste(prefix, "s0", sep=""))
+            filename = paste(prefix, "s0", sep=""))
 
     ## additional arguments
     all.args = ""
     if (!is.null(rcdt)) {
-      all.args = paste(all.args," --rcdt=",rcdt[1],",",rcdt[2],",",rcdt[3],sep="")
+        all.args = paste(all.args," --rcdt=",rcdt[1],",",rcdt[2],",",rcdt[3],sep="")
     }
     if (!is.null(x11.delay)) {
-      all.args = paste(all.args," --x11=",x11.delay,sep="")
+        all.args = paste(all.args," --x11=",x11.delay,sep="")
     }
     all.args = paste(all.args, inla.getOption("fmesher.arg"))
 
     echoc = inla.fmesher.call(all.args=all.args,prefix=prefix)
     
     mesh = list(
-      tv = 1L+inla.read.fmesher.file(paste(prefix, "tv", sep="")),
-      s = inla.read.fmesher.file(paste(prefix, "s", sep="")))
+            tv = 1L+inla.read.fmesher.file(paste(prefix, "tv", sep="")),
+            s = inla.read.fmesher.file(paste(prefix, "s", sep="")))
     
-    return (list(prefix = prefix, mesh = mesh, locations.idx = idx))
+    m = list(prefix = prefix, mesh = mesh, locations.idx = idx,
+            call = match.call(expand.dots=TRUE))
+    class(m) = "inla.fmesher.mesh"
+    return (m)
+}
+
+`plot.inla.fmesher.mesh` = function(m, color = "green", size = 2, lwd=2, ...)
+{
+    ## a simple function that plots the mesh from inla.fmesher.mesh()
+
+    if (length(color) == 1)
+        color = rep(color, dim(m$mesh$s)[1])
+
+    require(rgl)
+    rgl.open()
+    rgl.points(m$mesh$s[m$locations.idx, ], size=2*size, lwd=lwd, color = "blue", ...)
+    plot.inla.trimesh(m$mesh$tv, m$mesh$s, color = color, size=size, lwd=lwd, ...)
+
+    return (invisible())
 }
