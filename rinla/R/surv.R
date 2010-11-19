428c991bcb0d04a1b175e16e5b46ac26f1e67616
Håvard Rue <haavard.rue@kaust.edu.sa>
2010-11-19 21:43:54 +0100

    did some codecheck for surv.R

--- rinla/R/surv.R
+++ rinla/R/surv.R
@@ -48,8 +48,7 @@
     if (!is.numeric(time))
         stop ("'Time' variable is not numeric")
     nn = length(time)
-    if (any(is.na(time)))
-    {
+    if (any(is.na(time))) {
         ## fixup the rest of the argument if some of 'time' is NA; in
         ## this case just fill in some dummy values so the rest of the
         ## routine still goes through
@@ -62,63 +61,59 @@
             truncation[idx] = 0
     }
 
-    ##check that no time varible is negative
-    if (sum(time[!is.na(time)]<0)>0) stop("Negative times are not allowed")
-    if (!missing(time2))
-    {
-        if (sum(time2[!is.na(time2)] < 0) != 0)
-            stop("Negative times are not allowed")
+    ## check that no time varible is negative
+    if (any(time[!is.na(time)] < 0))
+        stop("Negative times are not allowed")
+    if (!missing(time2)) {
+        if (any(time2[!is.na(time2)] < 0))
+            stop("Negative times2 are not allowed")
     }
-    if (!missing(truncation))
-    {
-        if (sum(is.na(truncation)) > 0)
+    if (!missing(truncation)) {
+        if (any(is.na(truncation)))
             stop("Non valid values for 'truncation")
-        if (sum(truncation<0) > 0)
-            stop("Negative times are not allowed")
+        if (any(truncation<0))
+            stop("Negative truncation times are not allowed")
     }
-    ##if event is totally missing assume that all data are failures
-    if (missing(event))
-    {
+    ## if event is totally missing assume that all data are failures
+    if (missing(event)) {
         event = rep(1,nn)
         warning("'event' is missing: assuming all are observed failures")
-    }
-    else if (sum(is.na(event))>0)
-    {
-        ##if some of the element in event are missing assume that they're
-        ##observed failures (give a warning)
+    } else if (any(is.na(event))) {
+        ## if some of the element in event are missing assume that
+        ## they're observed failures (give a warning)
         event[is.na(event)] = 1
-        warning("Some elements in `event' are missing: assuming all are observed failures")
+        warning("Some elements in `event' are NA: set them to observed failures.")
     }
       
-    ##check that event is 0,1,2,3
-    if (sum(event==0 | event==1 | event==2 | event==3)!=nn)
+    ## check that event is 0,1,2,3
+    if (!all(is.element(event, 0:3)))
         stop("Invalid value for event")
   
-    ##check that for event=3 time2 is present
-    interval = event==3
+    ## check that for event=3 time2 is present
+    interval = (event==3)
     if (sum(interval)>0 && missing(time2))
         stop("'time2' has to be present for interval censored data")
-    ##and warn that time2 is ignored for even!=3
+    ## and warn that time2 is ignored for even!=3
     if (sum(interval)==0 && !missing(time2))
         warning("'time2' is ignored for data that are not interval censored")
     if (missing(time2))
         time2 = rep(0,nn)
 
-    ##if truncation is missing set it to 0
+    ## if truncation is missing set it to 0
     if (missing(truncation))
         truncation = rep(0,nn)
-    ##check that it has the correct length
-    if (length(truncation)!=nn)
+    ## check that it has the correct length
+    if (length(truncation) != nn)
         stop("'truncation' is of the wrong dimension")
   
     surv.time = numeric(nn)
     surv.upper = numeric(nn)
     surv.lower = numeric(nn)
 
-    observed = event==1
-    right = event==0
-    left = event==2
-    interval = event==3
+    observed = (event==1)
+    right = (event==0)
+    left = (event==2)
+    interval = (event==3)
 
     surv.time[observed] = time[observed]
     surv.lower[right] = time[right]
@@ -149,25 +144,17 @@
     plot(xax,yax,type="n",xlab="time",ylab="",axes=FALSE)
     axis(1)
     axis(2,label=F,tick=FALSE)
-    for(i in 1:nn)
-    {
-        if (event[i]==1)
-        {
+    for(i in 1:nn) {
+        if (event[i]==1) {
             lines(c(truncation[i],time[i]),c( i, i),type="l")
             text(time[i], i,"*")
-        }
-        else if (event[i]==0)
-        {
+        } else if (event[i]==0) {
             lines(c(truncation[i],lower[i]),c( i, i),type="l")
             text(lower[i], i,">")
-        }
-        else if (event[i]==2)
-        {
+        } else if (event[i]==2) {
             lines(c(truncation[i],upper[i]),c( i, i),type="l")
             text(upper[i], i,"<")
-        }
-        else if (event[i]==3)
-        {
+        } else if (event[i]==3) {
             lines(c(truncation[i],upper[i]),c( i, i),type="l")
             text(upper[i], i,"|")
             text(lower[i], i,"|")
@@ -178,18 +165,15 @@
     {
         leg.text = "failure"
         leg.symb = "*"
-        if (sum(event==0)>0)
-        {
+        if (any(event==0)) {
             leg.text = c(leg.text,"right cens")
             leg.symb = paste(leg.symb,">",sep="")
         }
-        if (sum(event==2)>0)
-        {
+        if (any(event==2)) {
             leg.text = c(leg.text,"left cens")
             leg.symb = paste(leg.symb,"<",sep="")
         }
-        if (sum(event==3)>0)
-        {
+        if (any(event==3)) {
             leg.text = c(leg.text,"interval cens")
             leg.symb = paste(leg.symb,"|",sep="")
         }
@@ -207,15 +191,16 @@
     class(x) = NULL
     nn = length(x$event)
 
-    interval = x$event==3
+    interval = (x$event==3)
     out = character(nn)
     out[interval] = paste("[",x$lower[interval],",",x$upper[interval],"]",sep="")
-    right = x$event==0
+    right = (x$event==0)
     out[right] = paste(x$lower[right],"+",sep="")
-    left = x$event==2
+    left = (x$event==2)
     out[left] = paste(x$upper[left],"-",sep="")
-    failure = x$event==1
+    failure = (x$event==1)
     out[failure] = paste(x$time[failure],sep="")
+
     return (out)
 }
 
@@ -223,8 +208,7 @@
 
 `as.inla.surv.1` = function(x)
 {
-    if (is.list(x))
-    {
+    if (is.list(x)) {
         for (nm in names(x))
             if (!is.element(nm, c("event", "time", "lower", "upper", "truncation")))
                 stop(paste("Wrong name:", nm))
@@ -257,36 +241,33 @@
             event[idx] = 0
     }
 
-    ##check that no time varible is negative
-    if (sum(time[!is.na(time)]<0)>0) 
+    ## check that no time varible is negative
+    if (any(time[!is.na(time)] < 0))
         stop("Negative times are not allowed")
     
-    ## here event is multiple events
-    ##if event is totally missing assume that no tumor/event is found
-    if (missing(event))
-    {
+    ## here event is multiple events if event is totally missing
+    ## assume that no tumor/event is found
+    if (missing(event)) {
         event = rep(0,nn)
         warning("'event' is missing: assuming no events")
-    }
-    else if (sum(is.na(event))>0)
-    {
-        ##if some of the element in event are missing assume that they're
-        ##no event observed (give a warning)
+    } else if (any(is.na(event))) {
+        ## if some of the element in event are missing assume that
+        ## they're no event observed (give a warning)
         event[is.na(event)] = 0
-        warning("Some elements in `event' are missing: assuming no event detected for these cases.")
+        warning("Some elements in `event' are NA: assume no event detected for these cases.")
     }
       
     
-    ##check that event is 0,1
-    if (sum(event==0 | event==1 )!=nn)
+    ## check that event is 0,1
+    if (!all(is.element(event, 0:1)))
         stop("Invalid value for event")
   
     if (missing(subject))
         stop("'subject' is missing")
     
     surv.time = numeric(nn)
-    detect = event ==1
-    notdetect= event== 0
+    detect = (event ==1)
+    notdetect= (event== 0)
 
     surv.time = time
     surv.time[detect] = time[detect]
@@ -317,27 +298,25 @@
     axis(1)
     axis(2,label=F,tick=FALSE)
      
-    for(i in 1 :nn)
-    {
- 
+    for(i in 1 :nn) {
         lines(time[subject==i],rep(i,length(time[subject==i])),type="l")
-        points(time[subject==i & event==1], rep(i, length(time[subject==i & event==1])),         
+        points(time[subject==i & event==1],
+               rep(i, length(time[subject==i & event==1])),         
                pch=8,col="red")
-        points(time[subject==i & event==0], rep(i, length(time[subject==i & event==0])), pch=10, 
+        points(time[subject==i & event==0],
+               rep(i, length(time[subject==i & event==0])), pch=10, 
                col="green")
-
     }
 
     if (legend) {
         leg.text = "detect"
         leg.symb = "*"
         leg.col="red"
-        if (sum(event==0)>0) {
+        if (any(event==0)) {
             leg.text = c(leg.text,"not detect")
             leg.symb = paste(leg.symb,"o",sep="")
             leg.col=c(leg.col,"green")
         }
-     
         legend("topright",leg.text,pch=leg.symb,col=leg.col,inset=0.02)
     }
 }
@@ -352,22 +331,23 @@
     class(x) = NULL
     nn = length(x$event)
     out = character(nn)
-    detect = x$event==1
+    detect = (x$event==1)
     out[detect] = paste(x$time[detect],sep="")
-    notdetect  = x$event==0
+    notdetect  = (x$event==0)
     out[notdetect] = paste(x$time[notdetect],"+",sep="")
+
     return (out)
 }
 
 `as.inla.surv.2` = function(x)
 {
-    if (is.list(x))
-    {
-        for (nm in names(x))
+    if (is.list(x)) {
+        for (nm in names(x)) {
             if (!is.element(nm, c("event", "time", "subject")))
                 stop(paste("Wrong name:", nm))
-
+        }
         class(x) = "inla.surv"
+
         return(x)
     }
     if (is.data.frame(x))
