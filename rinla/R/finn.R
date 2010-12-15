f5317cbc1624926cdb4274e367e0d2f808efd325
Håvard Rue <haavard.rue@kaust.edu.sa>
2010-12-15 22:10:02 +0100

    forgot to add finn.R

---
+++ rinla/R/finn.R
@@ -0,0 +1,32 @@
+## this function interface with mode = finn in the inlaprogram, which
+## computes whatever Finn wants...
+
+`inla.finn` = function(C)
+{
+    if (is.matrix(C) || is(C, "dgTMatrix")) {
+        finn.file = inla.sparse2file(inla.matrix2sparse(C), c.indexing = TRUE)
+        remove = TRUE
+    } else if (is.character(C)) {
+        finn.file = C
+        remove = FALSE
+    } else {
+        finn.file = inla.sparse2file(C, c.indexing = TRUE)
+        remove = TRUE
+    }
+        
+    if (inla.os("linux") || inla.os("mac")) {
+        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m finn", finn.file), intern=TRUE)
+    } else if(inla.os("windows")) {
+        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m finn", finn.file), intern=TRUE)
+    } else {
+        stop("\n\tNot supported architecture.")
+    }
+
+    if (remove) {
+        unlink(finn.file)
+    }
+
+    s = as.numeric(s)
+    n = length(s) %/% 2L
+    return ( list(sample = s[1L:n], reordering = s[n + (1L:n)] ))
+}
