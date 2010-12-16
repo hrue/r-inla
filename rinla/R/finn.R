0ca18dc008bed0855782383617350453a3592560
Håvard Rue <haavard.rue@kaust.edu.sa>
2010-12-16 08:59:40 +0100

    Added option reordering into inla.finn()

--- rinla/R/finn.R
+++ rinla/R/finn.R
@@ -1,8 +1,11 @@
 ## this function interface with mode = finn in the inlaprogram, which
 ## computes whatever Finn wants...
 
-`inla.finn` = function(C)
+`inla.finn` = function(C,
+        reordering = c("auto", "default", "identity", "band", "metis", "genmmd", "amd", "md", "mmd"))
 {
+    reordering = match.arg(reordering)
+
     if (is.matrix(C) || is(C, "dgTMatrix")) {
         finn.file = inla.sparse2file(inla.matrix2sparse(C), c.indexing = TRUE)
         remove = TRUE
@@ -15,9 +18,11 @@
     }
         
     if (inla.os("linux") || inla.os("mac")) {
-        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m finn", finn.file), intern=TRUE)
+        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m finn",
+                "-r", reordering, finn.file), intern=TRUE)
     } else if(inla.os("windows")) {
-        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m finn", finn.file), intern=TRUE)
+        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m finn",
+                "-r", reordering, finn.file), intern=TRUE)
     } else {
         stop("\n\tNot supported architecture.")
     }
@@ -28,5 +33,5 @@
 
     s = as.numeric(s)
     n = length(s) %/% 2L
-    return ( list(sample = s[1L:n], reordering = s[n + (1L:n)] ))
+    return ( list(sample = s[1L:n], reordering = s[n + (1L:n)] + 1L))
 }
