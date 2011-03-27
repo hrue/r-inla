d109788d6267805715c97149c3f1983467636c91
Havard Rue <havard.rue@math.ntnu.no>
2011-03-27 13:19:16 +0200

    added drafts of the doc for the (asymmetric) laplace distribution. need to revised them and make them up-to-date. (Thanks YRY!)

---
+++ r-inla.org/doc/likelihood/example-laplace.R
@@ -0,0 +1,22 @@
+
+
+library(INLA)
+inla.update(testing=T)
+
+rald <- function(n, tau=0.5, mu=0, delta=1){
+	xi <- (1-2*tau)/(tau - tau^2)		
+	sigma <- sqrt(2/(tau - tau^2))
+	w <- rexp(n, delta)
+	mu + xi*w + sigma*sqrt(w/delta)*rnorm(n)
+}
+
+n <- 1000
+mu <- rnorm(1)
+tau <- 0.8
+delta <- 0.5
+y <- rald(n, tau, mu, delta)
+
+formula <- y ~ mu - 1
+r <- inla(formula, family = "laplace", data = data.frame(mu,y), 
+			control.data = list(alpha=tau, gamma=2, epsilon=0.01))
+summary(r)
