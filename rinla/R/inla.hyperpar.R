fc29fbef102abf3cf5cbd948f1445a643a648630
Havard Rue <havard.rue@math.ntnu.no>
2011-04-13 20:09:06 +0200

    removed inla.hook and inla.hook.arg, and rewrote parts of inla.hyperpar() [thank BS]

--- rinla/R/inla.hyperpar.R
+++ rinla/R/inla.hyperpar.R
@@ -74,92 +74,54 @@
 
     stopifnot(class(object) == "inla")
     
-    verbose.save = verbose
-    dz.save = dz
-    quantiles.save = quantiles
-    diff.logdens.save = diff.logdens
-    keep.save = keep
-
-    for (nm in names(object))
-        inla.eval(paste(nm, " = object$", nm, sep=""))
-
-    verbose = verbose.save
-    dz = dz.save
-    quantiles = quantiles.save
-    diff.logdens = diff.logdens.save
-    keep = keep.save
-
-    ## if these are not in the `object', then they are NULL
-    if (!is.element("E", names(object)))
-        E=NULL
-    if (!is.element("offset", names(object)))
-        offset=NULL
-    if (!is.element("Ntrials", names(object)))
-        Ntrials=NULL
-    if (!is.element("scale", names(object)))
-        scale=NULL
-    if (!is.element("inla.arg", names(object)))
-        inla.arg=NULL
-    if (!is.element("lincomb", names(object)))
-        lincomb=NULL
-    if (!is.element("lincomb", names(object)))
-        lincomb=NULL
-    if (!is.element("control.lincomb", names(object)))
-        control.lincomb=list()
-
-    ## call inla() with the attached variables. Note that variables in
-    ## the call of this function, has precedance over the attached'
-    ## ones.
-
-    result = inla(
-            formula = as.formula(formula),
-            family = family, 
-            data = data,
+    result = INLA::inla(
+            formula = as.formula(object$formula),
+            family = object$family, 
+            data = object$data,
             quantiles = quantiles,
-            E = E,
-            offset = offset,
-            Ntrials = Ntrials,
-            scale = scale,
+            E = object$E,
+            offset = object$offset, 
+            Ntrials = object$Ntrials,
+            scale = object$scale,
             verbose = verbose,      
-            lincomb = lincomb,
-            control.lincomb = control.lincomb,
+            lincomb = object$lincomb,
+            control.lincomb = object$control.lincomb,
             control.compute=list(hyperpar=TRUE, dic=FALSE,
                     mlik=TRUE, cpo=FALSE,
-                    smtp=control.compute$smtp,
-                    strategy = control.compute$strategy),
+                    smtp=object$control.compute$smtp,
+                    strategy = object$control.compute$strategy),
             control.predictor = list(compute=FALSE,
                     cdf=NULL, quantiles=NULL,
-                    hyper = control.predictor$hyper, 
-                    cross = control.predictor$cross,
-                    A = control.predictor$A, 
-                    precision = control.predictor$precision), 
-            control.data = control.data,
+                    hyper = object$control.predictor$hyper, 
+                    cross = object$control.predictor$cross,
+                    A = object$control.predictor$A, 
+                    precision = object$control.predictor$precision), 
+            control.data = object$control.data,
             control.inla = list(strategy=NULL,
-                    int.strategy="grid",
-                    h=inla.ifelse(is.null(h), control.inla$h, h),
+                    int.strategy = "grid",
+                    h=inla.ifelse(is.null(h), object$control.inla$h, h),
                     dz=dz,
                     diff.logdens= diff.logdens,
                     print.joint.hyper=TRUE,
                     force.diagonal=TRUE,
                     adjust.weights = FALSE,
                     skip.configurations=skip.configurations,
-                    tolerance = control.inla$tolerance,
-                    lincomb.derived.only = control.inla$lincomb.derived.only,
-                    step.len = control.inla$step.len),
-            control.fixed = control.fixed,
+                    tolerance = object$control.inla$tolerance,
+                    lincomb.derived.only = object$control.inla$lincomb.derived.only,
+                    step.len = object$control.inla$step.len),
+            control.fixed = object$control.fixed,
             control.results = list(return.marginals.random=FALSE,
                     return.marginals.predictor=FALSE),
             control.mode = list(result = object, restart=restart),
-            inla.call = inla.call,
-            inla.arg = inla.arg,
+            inla.call = object$inla.call,
+            inla.arg = NULL,
+            num.threads = object$num.threads, 
             only.hyperparam = TRUE,
             keep = keep,
             working.directory = NULL,
-            silent = silent,
-            user.hook = inla.ifelse(exists("user.hook"), user.hook, NULL),
-            user.hook.arg = inla.ifelse(exists("user.hook.arg"), user.hook.arg, NULL),
+            silent = object$silent,
             ##
-            .internal = .internal
+            .internal = object$.internal
             )
 
     ## these are the entries that we want to replace
