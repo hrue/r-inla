fb95c331766ad66732053633b6a2b756bec3585d
Håvard Rue <haavard.rue@kaust.edu.sa>
2011-05-04 23:08:14 +0200

    added back check for NAN of INF in Q; this was disabled while debugging Finn:s problems...

--- gmrflib/GMRFLibP.h
+++ gmrflib/GMRFLibP.h
@@ -203,10 +203,10 @@ typedef long unsigned int GMRFLib_sizeof_tp;
 #define GMRFLib_GLOBAL_NODE(n) IMAX(100, (n) / 20)	       /* the limit for a ``global node'' */
 
 #define GMRFLib_STOP_IF_NAN_OR_INF(value)				\
-	if (0 && gsl_isnan(value)) {						\
+	if (gsl_isnan(value)) {						\
 		fprintf(stderr, "\n\t%s\n\tFunction: %s(), Line: %1d, Thread: %1d\n\tVariable evaluates to NAN/INF\n", RCSId, __GMRFLib_FuncName, __LINE__, omp_get_thread_num()); \
-		if (0)abort();						\
-		if (0)exit(1);						\
+		if (1)abort();						\
+		if (1)exit(1);						\
 	}
 
 #define GMRFLib_SET_PREC(arg_)						\
