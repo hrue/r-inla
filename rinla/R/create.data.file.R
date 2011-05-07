092a5e504a2c158a370a98faf2797f316d90654a
Håvard Rue <haavard.rue@kaust.edu.sa>
2011-05-08 00:11:38 +0200

    more of the same changes. the expand-dataframe.R code is still messy...

--- rinla/R/create.data.file.R
+++ rinla/R/create.data.file.R
@@ -12,42 +12,42 @@
              debug=FALSE)
 {
     if (is.null(y.orig)) {
-        y.orig = c(mf[, 1])
+        y.orig = c(mf[, 1L])
     } else if (inherits(y.orig,"inla.surv")) {
         y.orig = as.data.frame(unclass(y.orig))
     } else {
         y.orig = as.data.frame(y.orig)
     }
 
-    ##n.data=length(mf[, 1])
-    n.data = dim(y.orig)[1]
-    ind=seq(0, n.data-1)
+    ##n.data=length(mf[, 1L])
+    n.data = dim(y.orig)[1L]
+    ind=seq(0L, n.data-1L)
 
     if (debug)
         cat("inla.create.data.file: n.data = ", n.data, "\n")
 
     ## RESPONSE (gaussian and T can have the weights for the precision: y.i\sim N(\eta, prec\w.i))
     if (inla.one.of(family, c("gaussian", "normal", "t", "laplace", "sn", "skewnormal", "gev", "logistic"))) {
-        if(is.null(scale)) {
-            response=cbind(ind, rep(1, n.data), y.orig)
-            null.dat = is.na(response[, 3])
+        if (is.null(scale)) {
+            response=cbind(ind, rep(1L, n.data), y.orig)
+            null.dat = is.na(response[, 3L])
             response = response[!null.dat,]
         } else {
             response = cbind(ind, y.orig)
-            null.dat = is.na(response[, 2])
+            null.dat = is.na(response[, 2L])
             response = response[!null.dat,]
             scale = scale[!null.dat]
 
             if (debug)
                 cat(paste("length(scale), nrow(response)", length(scale), nrow(response),"\n", sep=" "  ))
             
-            if(length(scale)!=nrow(response)) {
+            if (length(scale)!=nrow(response)) {
                 file.remove(file)
                 file.remove(data.dir)
                 stop("Length of scale has to be the same as the length of data")
             }
             else
-                response=cbind(response[, 1], scale, response[, 2])
+                response=cbind(response[, 1L], scale, response[, 2L])
         }   
     } else if (inla.one.of(family, c("poisson",
                                      "zeroinflatedpoisson0",
@@ -59,69 +59,69 @@
                                      "zeroinflatednbinomial2"))) {
         ## Poisson/nbinomial family has the E field
         if (is.null(E)) {
-            response = cbind(ind, rep(1, n.data), y.orig)
-            null.dat = is.na(response[, 3])
+            response = cbind(ind, rep(1L, n.data), y.orig)
+            null.dat = is.na(response[, 3L])
             response = response[!null.dat,]
         } else {
             response = cbind(ind, y.orig)
-            null.dat = is.na(response[, 2])
+            null.dat = is.na(response[, 2L])
             response = response[!null.dat,]
             E = E[!null.dat]
-            if(length(E)!=nrow(response)) {
+            if (length(E)!=nrow(response)) {
                 file.remove(file)
                 file.remove(data.dir)
                 stop("Length of E has to be the same as the length of data")
             }
             else
-                response=cbind(response[, 1], E, response[, 2])
+                response=cbind(response[, 1L], E, response[, 2L])
         }       
     } else if (inla.one.of(family, c("poissonext"))) {
-        ## PoissonExt family has the E field, which is a n x 3 matrix
+        ## PoissonExt family has the E field, which is a n x 3L matrix
         if (is.null(E)) {
-            ## this is the default value '[1, 0, 0]'
-            response = cbind(ind, rep(1, n.data), rep(0, n.data), rep(0, n.data), y.orig)
-            null.dat = is.na(response[, 3])
+            ## this is the default value '[1L, 0L, 0]'
+            response = cbind(ind, rep(1.0, n.data), rep(0.0, n.data), rep(0.0, n.data), y.orig)
+            null.dat = is.na(response[, 3L])
             response = response[!null.dat,]
         } else {
             response = cbind(ind, y.orig)
-            null.dat = is.na(response[, 2])
+            null.dat = is.na(response[, 2L])
             response = response[!null.dat,]
-            if (!is.matrix(E) || dim(E)[2] != 3)
-                 stop(paste("Family 'poissonext': E has to be a 'n x 3' matrix;", dim(E)))
+            if (!is.matrix(E) || dim(E)[2L] != 3L)
+                 stop(paste("Family 'poissonext': E has to be a 'n x 3L' matrix;", dim(E)))
             E = E[!null.dat, ]
-            if (dim(E)[1] != nrow(response)) {
+            if (dim(E)[1L] != nrow(response)) {
                 file.remove(file)
                 file.remove(data.dir)
-                stop(paste("Family 'poissonext': dim(E)[1] has to be the same as the length of data;", dim(E)[1], nrow(response)))
+                stop(paste("Family 'poissonext': dim(E)[1L] has to be the same as the length of data;", dim(E)[1L], nrow(response)))
             }
             else
-                response=cbind(response[, 1], E, response[, 2])
+                response=cbind(response[, 1L], E, response[, 2L])
         }       
-    } else if(inla.one.of(family,
+    } else if (inla.one.of(family,
                           c("binomial",
                             "zeroinflatedbinomial0",
                             "zeroinflatedbinomial1",
                             "zeroinflatedbinomial2",
                             "zeroinflatedbetabinomial2"))) {
         response = cbind(ind, y.orig)
-        null.dat = is.na(response[, 2])
+        null.dat = is.na(response[, 2L])
         response = response[!null.dat,]
         if (is.null(Ntrials)) {
-            Ntrials = rep(1, n.data)
+            Ntrials = rep(1L, n.data)
         } else {
             Ntrials =  Ntrials[!null.dat]
         }
-        if(is.null(Ntrials)) {
+        if (is.null(Ntrials)) {
             file.remove(file)
             file.remove(data.dir)
             stop("Number of binomial trials has to be provided")
         } else {
-            if (any(response[, 2] > Ntrials)) {
+            if (any(response[, 2L] > Ntrials)) {
                 file.remove(file)
                 file.remove(data.dir)
                 stop("Number of trials have to be larger than y")
             } else {
-                response=cbind(response[, 1], Ntrials, response[, 2])
+                response=cbind(response[, 1L], Ntrials, response[, 2L])
             }
         }
     } else if (inla.one.of(family, c("exponential", "weibull", "weibullcure", "loglogistic",  "lognormal"))) {
@@ -146,7 +146,7 @@
             stop("NA in truncation/event/lower/upper/time is not allowed")
     } else if (inla.one.of(family, c("stochvol", "stochvolt", "stochvolnig", "loggammafrailty"))) {
         response = cbind(ind, y.orig)
-        null.dat = is.na(response[, 2])
+        null.dat = is.na(response[, 2L])
         response = response[!null.dat,]
     } else {
         file.remove(file)
