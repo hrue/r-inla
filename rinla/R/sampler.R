18cfad41b2a1fbcd29c6d94aaaae40f61927250b
Håvard Rue <haavard.rue@kaust.edu.sa>
2011-06-08 22:25:11 +0200

    added inla.sample.hyper() function to sample from the theta in user or internal scale. does not work for group parameters yet, as I havnt fixed the issue of a second argument to the to.theta and from.theta function.

---
+++ rinla/R/sampler.R
@@ -0,0 +1,56 @@
+inla.sample.hyper = function(n, result, intern=FALSE)
+{
+    ## generate 'n' samples from the joint for the hyperparameters
+    ## computed using the CCD approach. if intern==TRUE, then generate
+    ## the variables in their internal scale otherwise in the user
+    ## scale.
+    
+    stopifnot(!is.null(result))
+    stopifnot(any(class(result) == "inla"))
+    stopifnot(n > 0)
+
+    sigma = result$misc$cov.intern
+    if (dim(sigma)[1L] == 0L) {
+        return (NULL)
+    }
+    
+    p = dim(sigma)[1L]
+    sd.plus = result$misc$stdev.corr.positive
+    sd.neg  = result$misc$stdev.corr.negative
+
+    if (is.null(sd.plus) || is.null(sd.neg)) {
+        sd.plus = rep(1.0, p)
+        sd.neg = rep(1.0, p)
+    }
+
+    ## do each column  dim(z) = n x p,  each row is one sample
+    z = matrix(NA, n, p)
+    for(i in 1L:p) {
+        ## this is ok as n >> p, usually.
+        prob = c(sd.plus[i], sd.neg[i])
+        direction = sample.int(2L, n, prob = prob, replace=TRUE)
+        s = c(sd.plus[i], -sd.neg[i])
+        z[, i] = s[direction] * abs(rnorm(n))
+    }
+    A = result$misc$cov.intern.eigenvectors %*%
+        diag(sqrt(result$misc$cov.intern.eigenvalues), nrow = p, ncol = p)
+    theta = apply(z, 1L,
+            function(x, A, m) A %*% x + m,
+            A = A, m = result$misc$theta.mode)
+    ## fix for p > 1
+    if (p > 1L) {
+        theta = t(theta)
+    } else {
+        theta = matrix(theta, n, p)
+    }
+    
+    if (!intern) {
+        ## map to user-scale. do each column at the time as n >> p,
+        ## usually.
+        for(i in 1L:p) {
+            theta[, i] = result$misc$from.theta[[i]]( theta[, i] )
+        }
+    }
+
+    return (theta)
+}
