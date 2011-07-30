2f6fc6c5d1212afcbfea975610395116fc0db50a
Håvard Rue <haavard.rue@kaust.edu.sa>
2011-07-31 00:53:22 +0200

    added doc for expression-prior and ran top make

---
+++ r-inla.org/doc/prior/example-expression.R
@@ -0,0 +1,26 @@
+y = rnorm(1)
+
+## using buildt-in prior
+a = 1
+b = 0.1
+hyper = list(prec = list(prior = "loggamma", param = c(a, b)))
+r = inla(y ~ 1,  data = data.frame(y),
+        control.data = list(hyper = hyper))
+
+## implementing the loggamma-prior using "expression:"
+loggamma = "expression:
+            a = 1;
+            b = 0.1;
+            precision = exp(log_precision);
+            logdens = log(b^a) - lgamma(a)
+                      + (a-1)*log_precision - b*precision;
+            log_jacobian = log_precision;
+            return(logdens + log_jacobian);"
+
+hyper.new = list(prec = list(prior = loggamma))
+r.new = inla(y ~ 1,  data = data.frame(y),
+        control.data = list(hyper = hyper.new))
+
+## and we verify that we get the same result...
+print(r$summary.hyperpar[1,"mean"])
+print(r.new$summary.hyperpar[1,"mean"])
