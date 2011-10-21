569f4e35163821d666bd00b84e091e730eec4903
Finn Lindgren <finn.lindgren@gmail.com>
2011-10-21 12:51:14 +0200

    Code reorganisation.

--- sandbox/R/spde2test.R
+++ sandbox/R/spde2test.R
@@ -31,7 +31,7 @@ spde1$f$hyper.default =
 
 ##require(debug)
 ##mtrace(inla.spde.create.generic)
-spde2 =
+spde2a =
     inla.spde.generic2(M0=spde1$internal$c0,
                        M1=spde1$internal$g1,
                        M2=spde1$internal$g2,
@@ -42,34 +42,34 @@ spde2 =
                        theta.Q = diag(nrow=2)*prior.prec,
                        transform="identity")
 
-spde3 = inla.spde.matern(mesh)
+spde2b = inla.spde.matern(mesh)
 
 field = inla.spde.query(spde1, sample=list(tau=tau, kappa2=kappa2))$sample
 Y = 1+rep(field, n.obs.repl)+rnorm(mesh$n*n.obs.repl)/sqrt(prec)
 
-##data1 = list(Y=Y, field=rep(1:mesh$n, n.obs.repl), model=spde1)
-##formula1 = (as.formula("Y ~ 1 + f(field, model=spde1)"))
-data2 = list(Y=Y, field=rep(1:mesh$n, n.obs.repl), model=spde2)
-formula2 = (as.formula("Y ~ 1 + f(field, model=spde2)"))
-data3 = list(Y=Y, field=rep(1:mesh$n, n.obs.repl), model=spde3)
-formula3 = (as.formula("Y ~ 1 + f(field, model=spde3)"))
+data1 = list(Y=Y, field=rep(1:mesh$n, n.obs.repl), model=spde1)
+formula1 = (as.formula("Y ~ 1 + f(field, model=spde1)"))
+data2 = list(Y=Y, field=rep(1:mesh$n, n.obs.repl), model=spde2a)
+formula2 = (as.formula("Y ~ 1 + f(field, model=spde2a)"))
+data3 = list(Y=Y, field=rep(1:mesh$n, n.obs.repl), model=spde2b)
+formula3 = (as.formula("Y ~ 1 + f(field, model=spde2b)"))
 
 verbose = FALSE
-##result1 =
-##    inla(formula=formula1,
-##         family="gaussian",
-##         data=data1,
-##         control.predictor=list(compute=TRUE),
-##         control.compute=list(cpo=TRUE),
-##         verbose=verbose)
-result2 =
+result1 =
+    inla(formula=formula1,
+         family="gaussian",
+         data=data1,
+         control.predictor=list(compute=TRUE),
+         control.compute=list(cpo=TRUE),
+         verbose=verbose)
+result2a =
     inla(formula=formula2,
          family="gaussian",
          data=data2,
          control.predictor=list(compute=TRUE),
          control.compute=list(cpo=TRUE),
          verbose=verbose)
-result3 =
+result2b =
     inla(formula=formula3,
          family="gaussian",
          data=data3,
@@ -77,18 +77,18 @@ result3 =
          control.compute=list(cpo=TRUE),
          verbose=verbose)
 
-BLC.names = rownames(spde3$internal$param.generic$BLC)
-rownames(result3$summary.spde2.blc$field) <- BLC.names
-names(result3$marginals.spde2.blc$field) <-  BLC.names
+BLC.names = rownames(spde2b$internal$param.generic$BLC)
+rownames(result2b$summary.spde2.blc$field) <- BLC.names
+names(result2b$marginals.spde2.blc$field) <-  BLC.names
 
 result =
     cbind(theta,
-##          result1$summary.hyperpar[,c("0.025quant","0.975quant")],
-          result2$summary.hyperpar[,c("0.025quant","0.975quant")],
-          rbind(NA, result2$summary.spde2.blc$field[1:2,c("0.025quant","0.975quant")]),
-          result3$summary.hyperpar[,c("0.025quant","0.975quant")],
-          rbind(NA, result3$summary.spde2.blc$field[1:2,c("0.025quant","0.975quant")]))
-colnames(result) <- c("Truth", rep("SPDE2", 2), rep("SPDE2(BLC)", 2), rep("SPDE3", 2), rep("SPDE3(BLC)", 2))
+          result1$summary.hyperpar[,c("0.025quant","0.975quant")],
+          result2a$summary.hyperpar[,c("0.025quant","0.975quant")],
+          rbind(NA, result2a$summary.spde2.blc$field[1:2,c("0.025quant","0.975quant")]),
+          result2b$summary.hyperpar[,c("0.025quant","0.975quant")],
+          rbind(NA, result2b$summary.spde2.blc$field[1:2,c("0.025quant","0.975quant")]))
+colnames(result) <- c("Truth", "SPDE", rep("SPDE2A", 2), rep("SPDE2A(BLC)", 2), rep("SPDE2B", 2), rep("SPDE2B(BLC)", 2))
 rownames(result) <- c("Prec.", "theta1", "theta2")
 print(result[2:3,])
 
@@ -97,10 +97,10 @@ require(fields)
 plot(old.mesh.class(mesh), field,
      color.palette=tim.colors,
      draw.edges=FALSE, draw.vertices=FALSE)
-plot(old.mesh.class(mesh), result2$summary.random$field[,"mean"],
+plot(old.mesh.class(mesh), result2a$summary.random$field[,"mean"],
      color.palette=tim.colors,
      draw.edges=FALSE, draw.vertices=FALSE)
-plot(old.mesh.class(mesh), result2$summary.random$field[,"sd"],
+plot(old.mesh.class(mesh), result2a$summary.random$field[,"sd"],
      color.palette=tim.colors,
      draw.edges=FALSE, draw.vertices=FALSE)
 }
@@ -108,40 +108,40 @@ plot(old.mesh.class(mesh), result2$summary.random$field[,"sd"],
 ##dev.new()
 ##inla.ks.plot(result1$pit, punif)
 dev.new()
-inla.ks.plot(result2$pit, punif)
+inla.ks.plot(result2a$pit, punif)
 dev.new()
-inla.ks.plot(result3$pit, punif)
+inla.ks.plot(result2b$pit, punif)
 
 dev.new()
-plot(result2$marginals.hyperpar[[2]],
-     main="SPDE2, theta.1: hyperpar (circles), spde2.blc (solid)")
-lines(result2$marginals.spde2.blc$field[[1]])
+plot(result2a$marginals.hyperpar[[2]],
+     main="SPDE2A, theta.1: hyperpar (circles), spde2.blc (solid)")
+lines(result2a$marginals.spde2.blc$field[[1]])
 ##dev.new()
-##plot(result2$marginals.hyperpar[[3]],
-##     main="SPDE2, theta.2: hyperpar (circles), spde2.blc (solid)")
-##lines(result2$marginals.spde2.blc$field[[2]])
+##plot(result2a$marginals.hyperpar[[3]],
+##     main="SPDE2A, theta.2: hyperpar (circles), spde2.blc (solid)")
+##lines(result2a$marginals.spde2.blc$field[[2]])
 dev.new()
-plot(result3$marginals.hyperpar[[2]],
-     main="SPDE3, theta.1: hyperpar (circles), spde2.blc (solid)")
-lines(result3$marginals.spde2.blc$field[[1]])
+plot(result2b$marginals.hyperpar[[2]],
+     main="SPDE2B, theta.1: hyperpar (circles), spde2.blc (solid)")
+lines(result2b$marginals.spde2.blc$field[[1]])
 dev.new()
-plot(result3$marginals.hyperpar[[3]],
-     main="SPDE3, theta.2: hyperpar (circles), spde2.blc (solid)")
-lines(result3$marginals.spde2.blc$field[[2]])
+plot(result2b$marginals.hyperpar[[3]],
+     main="SPDE2B, theta.2: hyperpar (circles), spde2.blc (solid)")
+lines(result2b$marginals.spde2.blc$field[[2]])
 
 
 if (FALSE) {
 ## Option 1:
-spde3 = inla.spde.create(mesh, model="matern", param=list(alpha=2, basis.T=...))
-Q.epsilon = inla.spde.query(spde3, precision=list(tau=1, kappa2=0.5))$precision
+spde2b = inla.spde.create(mesh, model="matern", param=list(alpha=2, basis.T=...))
+Q.epsilon = inla.spde.query(spde2b, precision=list(tau=1, kappa2=0.5))$precision
 spde4 = inla.spde.create(mesh, model="heatequation",
    param=list(Q.epsilon=Q.epsilon))
 ## Option 1.5:
-spde3 = inla.spde.create(mesh, model="matern", alpha=2, basis.T=...)
-Q.epsilon = inla.spde.query(spde3, precision=TRUE, tau=1, kappa2=0.5)$precision
+spde2b = inla.spde.create(mesh, model="matern", alpha=2, basis.T=...)
+Q.epsilon = inla.spde.query(spde2b, precision=TRUE, tau=1, kappa2=0.5)$precision
 spde4 = inla.spde.create(mesh, model="heatequation", Q.epsilon=Q.epsilon)
 ## Option 2:
-spde3 = inla.spde.matern(mesh, alpha=2)
-Q.epsilon = inla.spde.precision(spde3, tau=1, kappa2=0.5)
+spde2b = inla.spde.matern(mesh, alpha=2)
+Q.epsilon = inla.spde.precision(spde2b, tau=1, kappa2=0.5)
 spde4 = inla.spde.heatequation(mesh, Q.epsilon=Q.epsilon)
 }
