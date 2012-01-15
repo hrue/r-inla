9685b3031dcc67687f12ddd82c59218ca8c78019
Håvard Rue <haavard.rue@kaust.edu.sa>
2012-01-15 17:33:22 +0100

    Added ...-argument to inla.dev.new() as dev.new() has it.

--- rinla/R/utils.R
+++ rinla/R/utils.R
@@ -1230,14 +1230,14 @@
     }
 }
 
-`inla.dev.new` = function()
+`inla.dev.new` = function(...)
 {
     ## If running in RStudio then don't open a new device,  otherwise,  do.
     dev = getOption("device")
     if (is.character(dev) && inla.strncasecmp(dev, "RStudioGD")) {
         return (NULL)
     } else {
-        return (dev.new())
+        return (dev.new(...))
     }
 }
 
