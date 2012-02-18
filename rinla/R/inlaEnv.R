11f679a19af757c321f4dc72e64f2af7f3ef8714
Havard Rue <havard.rue@math.ntnu.no>
2012-02-18 06:49:59 +0100

    Rewrote the internal cache-system using an own environment (.inlaEnv; use inla.get.inlaEnv() to get that environment), instead of using .GlobalEnv. Thanks to VGR for explaining me how this should be done. I added also the extention, that options to inla (inla.setOption) can be defined in the "usual" way without quotes, like inla.setOption(num.threads=10) and inla.setOption(num.threads=10, keep=TRUE) [can take many options in the same call], but the old-style, inla.setOption("num.threads", 10) still works.

---
+++ rinla/R/inlaEnv.R
@@ -0,0 +1,16 @@
+## Define the environment for INLA used to store options and the
+## model-list. Reuse the environment if it is there already.
+
+if (exists(".inlaEnv") && is.environment(.inlaEnv)) {
+    ## then reuse it
+} else {
+    .inlaEnv = new.env()
+}
+
+`inla.get.inlaEnv` = function(...)
+{
+    if (exists(".inlaEnv") && is.environment(.inlaEnv))
+        return (.inlaEnv)
+    stop("Environment '.inlaEnv' does not exists and is required for INLA to work. Restart 'R'.")
+}
+
