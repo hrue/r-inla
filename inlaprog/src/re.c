dc96f7ba01de1b87711c5a2b0346572ad827e310
Håvard Rue <haavard.rue@kaust.edu.sa>
2012-08-22 18:04:51 +0200

    More work on the SAS-model (reproject), now with the better parametersation!

--- inlaprog/src/re.c
+++ inlaprog/src/re.c
@@ -51,63 +51,73 @@ int re_shash_skew_kurt(double *skew, double *kurt, double epsilon, double delta)
 	double m3 = M3(epsilon, delta);
 	double m4 = M4(epsilon, delta);
 
-	if (skew){
-		*skew = (m3 - 3.0*m1*m2 + 3.0*gsl_pow_3(m1) - gsl_pow_3(m1)) / gsl_pow_3(sqrt(m2 - SQR(m1)));
+	if (skew) {
+		*skew = (m3 - 3.0 * m1 * m2 + 3.0 * gsl_pow_3(m1) - gsl_pow_3(m1)) / gsl_pow_3(sqrt(m2 - SQR(m1)));
 	}
-	if (kurt){
-		*kurt = (m4 - 4.0*m1*m3 + 6.0*SQR(m1)*m2 -4.0*gsl_pow_4(m1) + gsl_pow_4(m1)) / gsl_pow_4(sqrt(m2-SQR(m1)));
+	if (kurt) {
+		*kurt = (m4 - 4.0 * m1 * m3 + 6.0 * SQR(m1) * m2 - 4.0 * gsl_pow_4(m1) + gsl_pow_4(m1)) / gsl_pow_4(sqrt(m2 - SQR(m1)));
 	}
 
 	return GMRFLib_SUCCESS;
 }
 
 
-int re_shash_fit_parameters(re_shash_param_tp *param, double *mean, double *prec, double *skew, double *kurt)
+int re_shash_fit_parameters(re_shash_param_tp * param, double *mean, double *prec, double *skew, double *kurt)
 {
-	/* 
-	   for given mean, prec, skew and kurt, fit the parameters in the shash-model. Either none or both of 'skew' and 'kurt' can be NULL.
+	/*
+	 * for given mean, prec, skew and kurt, fit the parameters in the shash-model. Either none or both of 'skew' and 'kurt' can be NULL. 
 	 */
-	
-	static double input_p[4] = { 0, -1, 0, 0}; 
-#pragma omp threadprivate (input_p)
 
-	static re_shash_param_tp output_p;
-#pragma omp threadprivate (output_p)
+	int use_lookup = 1;
+
+	static double **pin = NULL;
+	static double **pout = NULL;
+	static int *perr = NULL;
 
 	assert((!skew && !kurt) || (skew && kurt));	       /* either non or both */
-	
-	double new_input_p[4];
 
-	new_input_p[0] = *mean;
-	new_input_p[1] = *prec;
-	new_input_p[2] = (skew ? *skew : NAN);
-	new_input_p[3] = (kurt ? *kurt : NAN);
+	if (use_lookup) {
+		if (!pin && !pout && !perr) {
+#pragma omp critical
+			{
+				if (!pin && !pout && !perr) {
+					pin = Calloc(GMRFLib_MAX_THREADS, double *);
+					pout = Calloc(GMRFLib_MAX_THREADS, double *);
+					perr = Calloc(GMRFLib_MAX_THREADS, int);
 
-	if (memcmp(new_input_p, input_p, sizeof(input_p)) == 0){
-		/* 
-		 * Match!
-		 */
-		memcpy(param, &output_p, sizeof(re_shash_param_tp));
-		return GMRFLib_SUCCESS;
+					int i;
+					for(i=0; i<GMRFLib_MAX_THREADS; i++){
+						pin[i] = Calloc(2, double);
+						pout[i] = Calloc(2, double);
+					}
+				}
+			}
+		}
 	}
 
-	if (!skew && !kurt){
-		/* 
-		 * only fit the mean and precision
-		 */
-		output_p.delta = 1.0;
-		output_p.epsilon = 0.0;
-		output_p.mu = *mean;
-		output_p.stdev = sqrt(*prec);
-	} else {
+	double npin[2], pout_tmp[2];
+	int thread_id, err = 0, debug = 0;
 
-		double epsilon = 0.3;
-		double delta = 1.1;
+	thread_id = omp_get_thread_num();
+	npin[0] = (skew ? *skew : 0.0);
+	npin[1] = (kurt ? *kurt : 3.0);
+
+	if (use_lookup && memcmp(npin, pin[thread_id], sizeof(npin)) == 0) {
+		/*
+		 * we have already computed these values
+		 */
+		memcpy(pout_tmp, pout[thread_id], sizeof(pout_tmp));
+	} else if (!skew && !kurt) {
+		pout_tmp[0] = 0.0;				       /* epsilon */
+		pout_tmp[1] = 1.0;				       /* delta */
+	} else {
+		double epsilon = 0.0;
+		double delta = 1.0;
 
 		gsl_vector *x = gsl_vector_alloc(2);
 		gsl_vector_set(x, 0, 0.0);		       /* initial values */
 		gsl_vector_set(x, 1, 1.0);
-		
+
 		double target[2];
 		target[0] = *skew;
 		target[1] = *kurt;
@@ -117,7 +127,7 @@ int re_shash_fit_parameters(re_shash_param_tp *param, double *mean, double *prec
 		int n = 2;
 		int p = 2;
 		int status;
-		
+
 		gsl_multifit_function_fdf f;
 		f.f = re_shash_f;
 		f.df = re_shash_df;
@@ -126,52 +136,64 @@ int re_shash_fit_parameters(re_shash_param_tp *param, double *mean, double *prec
 		f.p = p;
 		f.params = (void *) target;
 
-			
+
 		T = gsl_multifit_fdfsolver_lmsder;
 		s = gsl_multifit_fdfsolver_alloc(T, n, p);
 		gsl_multifit_fdfsolver_set(s, &f, (const gsl_vector *) x);
-		
-		int iter = 0, iter_max = 1000, debug = 1;
-		do
-		{
+
+		int iter = 0, iter_max = 1000;
+		double eps = 1e-4;
+		do {
 			iter++;
-			status = gsl_multifit_fdfsolver_iterate(s);	
-			if (status){
+			status = gsl_multifit_fdfsolver_iterate(s);
+			if (status) {
 				break;
 			}
-			if (debug){
-				printf ("status = %s\n", gsl_strerror(status));
-				printf("iter %1d: x %g %g |f| = %g\n", iter, gsl_vector_get(s->x, 0), gsl_vector_get(s->x, 1),
-				       gsl_blas_dnrm2(s->f));
+			if (debug) {
+				printf("status = %s\n", gsl_strerror(status));
+				printf("iter %1d: x %g %g |f| = %g\n", iter, gsl_vector_get(s->x, 0), gsl_vector_get(s->x, 1), gsl_blas_dnrm2(s->f));
 			}
-			status = gsl_multifit_test_delta(s->dx, s->x, 1e-4, 1e-4);
+			status = gsl_multifit_test_delta(s->dx, s->x, eps, eps);
 		}
 		while (status == GSL_CONTINUE && iter < iter_max);
 
-		if (gsl_blas_dnrm2(s->f) > 1e-6){
-			fprintf(stderr, "SHASH fail to fit target %g %g\n", target[0], target[1]);
+		err = (gsl_blas_dnrm2(s->f) > 1e-6);
+		if (err) {
+			fprintf(stderr, "SHASH fail to fit target skew=%g kurt=%g err=%g\n",
+				target[0], target[1], gsl_blas_dnrm2(s->f));
 		}
 
-		output_p.epsilon = gsl_vector_get(s->x, 0);
-		output_p.delta = gsl_vector_get(s->x, 1);
+		pout_tmp[0] = gsl_vector_get(s->x, 0);
+		pout_tmp[1] = gsl_vector_get(s->x, 1);
 
-		double m1 = M1(output_p.epsilon, output_p.delta);
-		double m2 = M2(output_p.epsilon, output_p.delta);
-		double stdev = sqrt(m2 - SQR(m1));
-		P(m1);
-		P(m2);
-		output_p.mu = *mean - m1/sqrt(*prec)/stdev;
-		output_p.stdev = 1.0/sqrt(*prec)/stdev;
+		gsl_vector_free(x);
+		gsl_multifit_fdfsolver_free(s);
 	}
 
-	memcpy(param, &output_p, sizeof(re_shash_param_tp));
+	double m1, m2, mu, stdev, ss;
+	
+	m1 = M1(pout_tmp[0], pout_tmp[1]);
+	m2 = M2(pout_tmp[0], pout_tmp[1]);
+	ss = sqrt(m2 - SQR(m1));
+	mu = *mean - m1 / sqrt(*prec) / ss;
+	stdev = 1.0 / sqrt(*prec) / ss;
 
-	return GMRFLib_SUCCESS;
+	param->mu = mu;
+	param->stdev = stdev;
+	param->epsilon = pout_tmp[0];
+	param->delta = pout_tmp[1];
+
+	if (use_lookup){
+		perr[thread_id] = err;
+		memcpy(pin[thread_id], npin, sizeof(npin));
+		memcpy(pout[thread_id], pout_tmp, sizeof(pout_tmp));
+	}
+		
+	return (err ? !GMRFLib_SUCCESS : GMRFLib_SUCCESS);
 }
-
-int re_shash_f(const gsl_vector *x, void *data, gsl_vector *f)
+int re_shash_f(const gsl_vector * x, void *data, gsl_vector * f)
 {
-	double epsilon, delta,  skew, sskew, kurt, kkurt, *ddata = (double *)data;
+	double epsilon, delta, skew, sskew, kurt, kkurt, *ddata = (double *) data;
 
 	epsilon = gsl_vector_get(x, 0);
 	delta = gsl_vector_get(x, 1);
@@ -179,39 +201,34 @@ int re_shash_f(const gsl_vector *x, void *data, gsl_vector *f)
 	kkurt = ddata[1];
 
 	re_shash_skew_kurt(&skew, &kurt, epsilon, delta);
-	gsl_vector_set(f, 0, skew -sskew);
-	gsl_vector_set(f, 1, kurt -kkurt);
+	gsl_vector_set(f, 0, skew - sskew);
+	gsl_vector_set(f, 1, kurt - kkurt);
 
 	return GSL_SUCCESS;
 }
-int re_shash_df(const gsl_vector *x, void *data, gsl_matrix *J)
+int re_shash_df(const gsl_vector * x, void *data, gsl_matrix * J)
 {
-	double h = GMRFLib_eps(1./3.), epsilon, delta,  skew_h, skew_mh, kurt_h, kurt_mh, *ddata = (double *)data;
+	double h = GMRFLib_eps(1. / 3.), epsilon, delta, skew_h, skew_mh, kurt_h, kurt_mh; 
 
 	epsilon = gsl_vector_get(x, 0);
 	delta = gsl_vector_get(x, 1);
 
 	re_shash_skew_kurt(&skew_h, &kurt_h, epsilon + h, delta);
 	re_shash_skew_kurt(&skew_mh, &kurt_mh, epsilon - h, delta);
-	gsl_matrix_set(J, 0, 0, (skew_h - skew_mh)/2.0/h);
-	gsl_matrix_set(J, 1, 0, (kurt_h - kurt_mh)/2.0/h);
-	
+	gsl_matrix_set(J, 0, 0, (skew_h - skew_mh) / 2.0 / h);
+	gsl_matrix_set(J, 1, 0, (kurt_h - kurt_mh) / 2.0 / h);
+
 	re_shash_skew_kurt(&skew_h, &kurt_h, epsilon, delta + h);
 	re_shash_skew_kurt(&skew_mh, &kurt_mh, epsilon, delta - h);
-	gsl_matrix_set(J, 1, 1, (kurt_h - kurt_mh)/2.0/h);
-	gsl_matrix_set(J, 0, 1, (skew_h - skew_mh)/2.0/h);
-	
+	gsl_matrix_set(J, 0, 1, (skew_h - skew_mh) / 2.0 / h);
+	gsl_matrix_set(J, 1, 1, (kurt_h - kurt_mh) / 2.0 / h);
+
 	return GSL_SUCCESS;
 }
-int re_shash_fdf(const gsl_vector *x, void *data, gsl_vector *f, gsl_matrix *J)
+int re_shash_fdf(const gsl_vector * x, void *data, gsl_vector * f, gsl_matrix * J)
 {
 	re_shash_f(x, data, f);
 	re_shash_df(x, data, J);
 
 	return GSL_SUCCESS;
 }
-	
-	
-	
-	
-	
