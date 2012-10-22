abca2e17f5e0dd230b030746276a336b6a1faec7
Håvard Rue <haavard.rue@kaust.edu.sa>
2012-10-22 17:45:34 +0200

    More work on the AR(p)-model; ready for its first build.

---
+++ r-inla.org/doc/latent/example-ar.R
@@ -0,0 +1,37 @@
+n = 100L
+p = 2L
+pacf = runif(p)
+phi = inla.ar.pacf2phi(pacf)
+y = arima.sim(n, model = list(ar = phi)) +
+    rnorm(n, sd=sd(y)/100.0)
+idx = 1L:n
+
+param.prec = c(1, 0.01)
+param.psi.mean = rep(0, p)
+param.psi.prec = 0.15 * diag(p)
+param.psi = c(param.psi.mean, param.psi.prec)
+
+r = inla(y ~ -1 + f(
+        idx, model='ar',
+        order = p, 
+        hyper = list(
+                ## marginal precision
+                prec = list(param = param.prec), 
+                ## the parameters for the joint normal prior for the
+                ## transformed pacf's, goes here.
+                pacf1 = list(param = param.psi))), 
+        family = "gaussian", 
+        data = data.frame(y, idx))
+
+## we will now estimate the posterior marginals of the phi-parameters
+## using the (experimental) function 'inla.hyperpar.sampler', which
+## creates samples from the approximated joint distribution for the
+## hyperparameters.
+nsamples = 100000
+pacfs = inla.hyperpar.sampler(nsamples, r)[, 3L:(3L+(p-1L))]
+phis = apply(pacfs, 1L, inla.ar.pacf2phi)
+for(i in 1:p) {
+    inla.dev.new()
+    plot(density(phis[i, ]), main = paste("phi", i, sep=""))
+    abline(v = phi[i])
+}
