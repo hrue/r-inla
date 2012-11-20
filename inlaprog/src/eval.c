da18901c4b0d1f06cda8577d1d58aeaaa2b1a944
Håvard Rue <haavard.rue@kaust.edu.sa>
2012-11-20 07:21:47 +0100

    Small fix.

--- inlaprog/src/eval.c
+++ inlaprog/src/eval.c
@@ -142,7 +142,7 @@ muFloat_t *inla_eval_AddVariable(const muChar_t * a_szName, void *pUserData)
 
 double inla_eval(char *expression, double *x)
 {
-	if (debug){
+	if (debug) {
 		printf("call inla_eval with %s\n", expression);
 	}
 
@@ -226,20 +226,20 @@ double inla_eval_table(char *expression, double *xval)
 	GMRFLib_spline_tp *s;
 	double *table = NULL, *x = NULL, *y = NULL, value;
 	int nelm = 0, n, i, k;
-	
+
 	inla_sread_doubles_q(&table, &nelm, expression);
 	assert(GSL_IS_EVEN(nelm));
 	assert(nelm >= 4);
-	
+
 	n = nelm / 2;
 	x = Calloc(n, double);
 	y = Calloc(n, double);
-	
-	for(i=k=0; i < n; i++, k += 2) {
+
+	for (i = k = 0; i < n; i++, k += 2) {
 		x[i] = table[k + 0];
 		y[i] = table[k + 1];
 
-		if (debug){
+		if (debug) {
 			printf("table %d: %g %g\n", i, x[i], y[i]);
 		}
 	}
@@ -247,17 +247,17 @@ double inla_eval_table(char *expression, double *xval)
 	s = inla_spline_create(x, y, n);
 	value = inla_spline_eval(*xval, s);
 
-	if (1){
+	if (0) {
 		static int first = 1;
 
-		if (first){
+		if (first) {
 			first = 0;
 #pragma omp critical
 			{
 				FILE *fp = fopen("table", "w");
 				double xx;
-				
-				for(xx = -9; xx < 9;  xx += 0.001){
+
+				for (xx = -9; xx < 9; xx += 0.001) {
 					fprintf(fp, "%g %g\n", xx, inla_spline_eval(xx, s));
 				}
 				fclose(fp);
