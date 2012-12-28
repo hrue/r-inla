49d0916e6a06328916893e00d036314cc1bba146
Håvard Rue <haavard.rue@kaust.edu.sa>
2012-12-28 23:46:06 +0100

    Working on new likelihoodmodel: binomialre, which includes one "random effect" pr observation. Not fully done yet...

--- rinla/R/models.R
+++ rinla/R/models.R
@@ -2609,6 +2609,25 @@
                       pdf = "binomial"
                       ),
 
+              binomialre = list(
+                      hyper = list(
+                              theta = list(
+                                      name = "log precision",
+                                      short.name = "prec",
+                                      initial = 4,
+                                      fixed = FALSE,
+                                      prior = "loggamma",
+                                      param = c(1, 0.01),
+                                      to.theta = function(x) log(x),
+                                      from.theta = function(x) exp(x)
+                                      )
+                              ),
+                      survival = FALSE,
+                      discrete = TRUE,
+                      link = c("default", "logit", "probit", "cloglog"),
+                      pdf = "binomialre"
+                      ),
+
               testbinomial1 = list(
                       hyper = list(
                               theta1 = list(
