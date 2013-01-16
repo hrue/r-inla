827b40bd1be7016dc0fd7e1f3736b74ee3edd6d7
Håvard Rue <haavard.rue@kaust.edu.sa>
2013-01-16 12:51:03 +0100

    Added also the logdensities to the inla.posterior.sample()-output.

--- rinla/R/posterior.sample.R
+++ rinla/R/posterior.sample.R
@@ -11,32 +11,37 @@
 ##! 
 ##! \arguments{
 ##!   \item{n}{Number of samples.}
-##!   \item{result}{The inla-object, ie the output from an \code{inla}-call}
+##!   \item{result}{The inla-object, ie the output from an \code{inla}-call. The \code{inla}-object must be created with
+##!                 \code{control.compute=list(config=TRUE)}.}
 ##!   \item{hyper.user.scale}{Use values of hyperparameters in the user scale, not in the internal representation}
 ##!}
 ##!\value{
-##!  A list of samples,  where each samples is a list with names \code{hyperpar} and \code{latent}.
+##!  A list of the samples,  where each sample is a list with names \code{hyperpar} and \code{latent},  and with
+##!  their marginal densities in \code{logdens$hyperpar} and \code{logdens$latent}
+##!  and the joint density is in \code{logdens$joint}.
 ##!  THIS IS AN EXPERIMENTAL FUNCTION AND CHANGES MAY APPEAR AT ANY TIME!
 ##!}
 ##!\author{Havard Rue \email{hrue@math.ntnu.no}}
 ##! 
 ##!\examples{
+##!  r = inla(y ~ 1 ,data = data.frame(y=rnorm(1)), control.compute = list(config=TRUE))
+##!  samples = inla.posterior.sample(2,r)
 ##!}
 
 
-`inla.posterior.sample` = function(n = 1, r, hyper.user.scale = TRUE)
+`inla.posterior.sample` = function(n = 1, result, hyper.user.scale = TRUE)
 {
     warning("THIS FUNCTION IS EXPERIMENTAL!!!")
     
-    stopifnot(any(class(r) == "inla"))
-    if (is.null(r$misc$configs)) {
+    stopifnot(!missing(result) && any(class(result) == "inla"))
+    if (is.null(result$misc$configs)) {
         stop("You need an inla-object computed with option 'control.compute=list(config = TRUE)'.")
     }
     n = as.integer(n)
     stopifnot(is.integer(n) && n > 0L)
     
     ## first sample the theta(-index)
-    cs = r$misc$configs
+    cs = result$misc$configs
     ld = numeric(cs$nconfig)
     for (i in 1:cs$nconfig) {
         ld[i] = cs$config[[i]]$log.posterior
@@ -56,29 +61,31 @@
     for(k in 1:cs$nconfig) {
         if (n.idx[k] > 0) {
             ## then the latent field
-            xx = cs$config[[k]]$mean +
-                inla.qsample(n=n.idx[k],
-                             Q=cs$config[[k]]$Q,
-                             constr = cs$constr)
+            tmp = inla.qsample(n=n.idx[k], Q=cs$config[[k]]$Q, constr = cs$constr, logdens = TRUE)
+            xx = cs$config[[k]]$mean + tmp$sample
             nm = c()
+            ld.theta = cs$max.log.posterior + cs$config[[k]]$log.posterior
             for(j in 1:length(cs$contents$tag)) {
                 ii = seq(cs$contents$start[j], length = cs$contents$length[j])
                 nm = c(nm,
                         paste(cs$contents$tag[j],
                               ".",
-                              inla.num(1:cs$contents$length[j]), sep=""))
+                              inla.ifelse(cs$contents$length[j] == 1L, 1, inla.num(1:cs$contents$length[j])), 
+                              sep=""))
             }
             
             theta = cs$config[[k]]$theta
             if (hyper.user.scale) {
                 for(j in 1:length(theta)) {
-                    theta[j] = do.call(r$misc$from.theta[[j]], args = list(theta[j]))
+                    theta[j] = do.call(result$misc$from.theta[[j]], args = list(theta[j]))
                 }
                 names(theta) = paste(names(theta), "-- in user scale")
             } 
 
             for(i in 1:n.idx[k]) {
-                a.sample = list(hyperpar = theta,  latent = xx[ , i, drop=FALSE])
+                a.sample = list(hyperpar = theta,  latent = xx[ , i, drop=FALSE],
+                        logdens = list(hyperpar = ld.theta, latent = as.numeric(tmp$logdens[i]),
+                                joint = as.numeric(ld.theta + tmp$logdens[i])))
                 rownames(a.sample$latent) = nm
                 all.samples[[i.sample]] = a.sample
                 i.sample = i.sample + 1L
