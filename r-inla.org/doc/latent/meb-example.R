9fabae79fbcc7cf5e2a80088732eaa58b53ddd01
Havard Rue <havard.rue@math.ntnu.no>
2013-02-05 22:44:56 +0100

    SM revised the documentation  and the examples for the mec and meb model; thanks!

--- r-inla.org/doc/latent/meb-example.R
+++ r-inla.org/doc/latent/meb-example.R
@@ -1,14 +1,38 @@
 n = 100
 beta = 2
 w = rnorm(n)
-prec.u = 100
-prec.y = 1000
-s = runif(n, min = 0.5, max = 1/0.5)
+prec.u = 1
+prec.y = 1
+s = runif(n,min=0,max=1)
 x = w + rnorm(n, sd = 1/sqrt(s*prec.u))
-y = 1 + beta * x + rnorm(n, sd = 1/sqrt(prec.y))
+y = 1 + beta*x + rnorm(n, sd = 1/sqrt(prec.y))
 
-formula = y ~ f(w, model="meb",
-        hyper = list(prec = list(param = c(1, 0.01))))
+## prior parameters
+prior.beta = c(0, 0.0001)
+prior.prec.u = c(10, 9/prec.u)
+prior.prec.y = c(10, 9/prec.y)
+
+
+formula = y ~ f(w,model="meb", scale=s,
+                 hyper = list(
+                      beta = list( 
+                          param = prior.beta, 
+                          fixed = FALSE
+                          ),
+                      prec.obs = list(
+                          param = prior.prec.u, 
+                          fixed = FALSE
+                          )
+                      )
+               )
 
 r = inla(formula, data = data.frame(y, w, s), 
-        family = "gaussian")
+        family = "gaussian", 
+        control.family = list(
+           hyper = list(
+                    prec = list(param = prior.prec.y, 
+                                fixed = FALSE
+                           )
+                    )
+        )
+    )
