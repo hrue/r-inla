042b4469c2bbfccab531a17c74e6e3ac0b965cbd
Håvard Rue <haavard.rue@kaust.edu.sa>
2013-02-05 14:49:38 +0100

    Revised the doc for model "meb"; thanks to SM!

--- r-inla.org/doc/latent/meb-example.R
+++ r-inla.org/doc/latent/meb-example.R
@@ -1,12 +1,14 @@
-n = 300
+n = 100
 beta = 2
-x = rnorm(n)
-prec.x = 10
+w = rnorm(n)
+prec.u = 100
 prec.y = 1000
-s = runif(n)
-x.tilde = x + rnorm(n, sd = 1/sqrt(s*prec.x))
-y = 1 + beta * x.tilde + rnorm(n, sd = 1/sqrt(prec.y))
+s = runif(n, min = 0.5, max = 1/0.5)
+x = w + rnorm(n, sd = 1/sqrt(s*prec.u))
+y = 1 + beta * x + rnorm(n, sd = 1/sqrt(prec.y))
 
-r = inla(y ~ f(x, model="meb", scale = s), 
-        family = "gaussian", 
-        data = data.frame(y, x, s))
+formula = y ~ f(w, model="meb",
+        hyper = list(prec = list(param = c(1, 0.01))))
+
+r = inla(formula, data = data.frame(y, w, s), 
+        family = "gaussian")
