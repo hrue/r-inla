3b6abf208cb49cc0bbd1fa8032dd6e3414be77d7
eliaskrainski <eliaskrainski@gmail.com>
2013-03-01 14:25:01 +0100

    Add the new spde tutorial files

---
+++ tutorials/spde/runCacheSweaveSPDEtutorial.R
@@ -0,0 +1,6 @@
+library(cacheSweave)
+Sweave('spde-tutorial-likelihoodintro.Rnw', drive=cacheSweaveDriver)
+Sweave('spde-tutorial-toy.Rnw', drive=cacheSweaveDriver)
+Sweave('spde-tutorial-rain.Rnw', drive=cacheSweaveDriver)
+Sweave('spde-tutorial-semicontinuous.Rnw', drive=cacheSweaveDriver)
+
