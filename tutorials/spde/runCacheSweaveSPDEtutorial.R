5d7c7f3084636ccf2fea0ba6caa3102ada49d68d
eliaskrainski <eliaskrainski@gmail.com>
2013-03-04 16:59:43 +0100

    runCacheSweave file removed

--- tutorials/spde/runCacheSweaveSPDEtutorial.R
+++
@@ -1,6 +0,0 @@
-library(cacheSweave)
-Sweave('spde-tutorial-likelihoodintro.Rnw', drive=cacheSweaveDriver)
-Sweave('spde-tutorial-toy.Rnw', drive=cacheSweaveDriver)
-Sweave('spde-tutorial-rain.Rnw', drive=cacheSweaveDriver)
-Sweave('spde-tutorial-semicontinuous.Rnw', drive=cacheSweaveDriver)
-
