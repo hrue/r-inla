ad051ecbaa4f2a7b8df5ad697071e78c84b07e9b
Havard Rue <havard.rue@math.ntnu.no>
2013-03-26 08:27:16 +0100

    Forgot a debug statement

--- rinla/R/collect.R
+++ rinla/R/collect.R
@@ -25,7 +25,6 @@ inla.internal.experimental.mode = FALSE
         tags = NULL
     }
 
-browser()
     fnm = paste(d, "/theta-from", sep="")
     if (file.exists(fnm)) {
         theta.from = readLines(fnm)
