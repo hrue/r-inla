8ed5afef2307810155d4bd430314d59e1c8ada09
Håvard Rue <haavard.rue@kaust.edu.sa>
2013-03-26 11:48:21 +0100

    Updated the code for the Leuk-demo.

--- rinla/demo/Leuk.R
+++ rinla/demo/Leuk.R
@@ -1,5 +1,4 @@
 data(Leuk)
-
 ## get the function to plot the map
 source(system.file("demodata/Leuk-map.R", package="INLA"))
 g = system.file("demodata/Leuk.graph", package="INLA")
@@ -9,13 +8,7 @@ formula = inla.surv(Leuk$time, Leuk$cens) ~ sex + age +
     f(inla.group(tpi), model="rw2")+
     f(district,model="besag",graph = g)
 
-result = inla(formula, family="coxph",
-        data=Leuk, verbose=TRUE, 
-        control.hazard=list(model="rw1", n.intervals=20),
-        control.inla=list(h=0.05))
-
+result = inla(formula, family="coxph", data=Leuk)
 summary(result)
-
 Leuk.map(result$summary.random$district$mean)
-
 plot(result)
