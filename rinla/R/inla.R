a4180d04ff6051ca0cea723daf73adab0b048938
Håvard Rue <haavard.rue@kaust.edu.sa>
2013-07-03 07:37:41 +0200

    Small fix.

--- rinla/R/inla.R
+++ rinla/R/inla.R
@@ -374,7 +374,7 @@
     if (!is.data.frame(data) && !is.list(data)) {
         stop("\n\tArgument `data' must be a data.frame or a list.")
     }
-    if (!is.null(weights)) {
+    if (!missing(weights)) {
         if (!inla.getOption("enable.inla.argument.weights")) {
             stop(paste("Argument 'weights' must be enabled before use due to the risk of mis-interpreting the results.\n",
                        "\tUse 'inla.setOption(\"enable.inla.argument.weights\", TRUE)' to enable it; see ?inla"))
