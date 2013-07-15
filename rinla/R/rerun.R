ebb2f863576af05eedb13ba8eecdebfc6d07add2
Håvard Rue <haavard.rue@kaust.edu.sa>
2013-07-15 09:37:51 +0200

    Moved inla.rerun into a separate file and then also a separate help-page. merged inla.self.call into inla.rerun.

---
+++ rinla/R/rerun.R
@@ -0,0 +1,42 @@
+##! \name{inla.rerun}
+##! \alias{inla.rerun}
+##! \alias{rerun}
+##!
+##! \title{Rerun an inla-call}
+##! \usage{
+##! inla.rerun(object)
+##! }
+##! \arguments{
+##!
+`inla.rerun` = function(
+        ##!\item{object}{An \code{inla}-object, ie the output from an \code{inla}-call} 
+        object, 
+        ##!\item{plain}{Logical. If \code{FALSE} (default), then make
+        ##!changes in \code{object} to improve the performance}
+        plain = FALSE) 
+    ##!}
+    ##!\value{This function will take the result in \code{object},  and rerun \code{inla} again.
+    ##!       If \code{plain}  is \code{FALSE},  start  the optimization from the mode in \code{object} so that
+    ##!       we can obtain an  improvement the mode for the hyperparameters. Otherwise,  start from the same configuration
+    ##!       as for \code{object}.
+    ##!       The returned value is an \code{inla}-object.}
+    ##!\seealso{\code{\link{inla}}}
+    ##!\examples{
+    ##!r = inla(y ~ 1,  data = data.frame(y=1:10))
+    ##!r = inla.rerun(r)
+    ##!}
+{
+
+    stopifnot(any(inherits(object, "inla")))
+
+    if (!plain) {
+        object$.args$control.mode$result = NULL
+        object$.args$control.mode$restart = TRUE
+        object$.args$control.mode$theta = object$mode$theta
+        object$.args$control.mode$x = object$mode$x
+        ## do not want to change this one
+        ##object$.args$control.mode$fixed = FALSE
+    }
+    
+    return (do.call("inla",  args = object$.args))
+}
