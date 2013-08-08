84d8f304d55f6310c54925b019ee796d61733902
Håvard Rue <haavard.rue@kaust.edu.sa>
2013-08-08 17:12:33 +0200

    Added doc for inla.dev.new()

--- rinla/R/dev.new.R
+++ rinla/R/dev.new.R
@@ -1,5 +1,22 @@
 ## Export: inla.dev.new
 
+##! \name{inla.dev.new}
+##! \alias{inla.dev.new}
+##!
+##! \title{Opens a new device}
+##!
+##! \description{Open a new device using \code{\link{dev.new}} unless using RStudio}
+##! \usage{
+##!     inla.dev.new(...)
+##! }
+##! \arguments{
+##!   \item{...}{Optional arguments to \code{\link{dev.new}}} 
+##!}
+##! \value{
+##!   The value of \code{\link{dev.new}} if not running RStudio, otherwise \code{NULL} 
+##! }
+##! \author{Havard Rue \email{hrue@math.ntnu.no}}
+
 `inla.dev.new` = function(...)
 {
     ## If running in RStudio then don't open a new device,  otherwise,  do.
