8982c4dfd9f3564b58dc557bb85840bf63636f17
Havard Rue <havard.rue@math.ntnu.no>
2013-09-15 13:04:44 +0200

    Fixed a missprint.

--- rinla/R/inla.R
+++ rinla/R/inla.R
@@ -1965,8 +1965,8 @@
             }
             ## remove the .Evironment attribute, as it will fail if
             ## its rerun if .Environment is not there.
-            attr(r$.args$formula, ".Environment") = NULL
-
+            attr(the.args$formula, ".Environment") = NULL
+            
             ## further fix for $control.family until the c(param = numeric(0)) issue is solved. 
             if (n.family > 1L) {
                 ## we need to fix the case where there this option is
