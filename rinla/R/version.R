6b17d27ee7b3deb80c0d965db8d964be48599ca6
Havard Rue <havard.rue@math.ntnu.no>
2013-10-15 23:01:40 +0200

    Added Elias to the list ot Maintainers.

--- rinla/R/version.R
+++ rinla/R/version.R
@@ -67,6 +67,7 @@
         cat(        "\t                         : Finn Lindgren <finn.lindgren@gmail.com>\n")
         cat(        "\t                         : Daniel Simpson <dp.simpson@gmail.com>\n")
         cat(        "\t                         : Andrea Riebler <andrea.riebler@math.ntnu.no>\n")
+        cat(        "\t                         : Elias Teixeira Krainski <elias.krainski@math.ntnu.no>\n")
         cat(        "\tWeb-page ................: http://www.r-inla.org\n")
         cat(        "\tEmail support ...........: help@r-inla.org\n")
         cat(        "\t                         : r-inla-discussion-group@googlegroups.com\n")
