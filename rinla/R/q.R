d88817f690738e64c1bc1d2f1206c7e5322ef0e7
Håvard Rue <haavard.rue@kaust.edu.sa>
2013-11-27 03:26:45 +0100

    More work on handling remote jobs.

--- rinla/R/q.R
+++ rinla/R/q.R
@@ -1,45 +1,87 @@
-## Export: inla.get print!inla.get summary!inla.get
+## Export: inla.qstat inla.qdel inla.qget print!inla.q summary!inla.q
 
-##!\name{inla.get}
-##!\alias{inla.get}
-##!\alias{summary.inla.get}
-##!\alias{print.inla.get}
-##!\title{Get and view submitted jobs}
+##!\name{inla.qstat}
+##!\alias{inla.qstat}
+##!\alias{inla.qget}
+##!\alias{inla.qdel}
+##!\alias{summary.inla.q}
+##!\alias{print.inla.q}
+##!\title{Control and view a remote inla-queue}
 ##!\description{
-##!Functions to get and view submitted jobs
+##!Control and view a remote inla-queue of submitte jobs
 ##!}
 ##!\usage{
-##!inla.get(id = NULL, remove = TRUE)
-##!\method{summary}{inla.get}(object,...)
-##!\method{print}{inla.get}(x,...)
+##!inla.qget(id, remove = TRUE)
+##!inla.qdel(id)
+##!inla.qstat()
+##!\method{summary}{inla.q}(object,...)
+##!\method{print}{inla.q}(x,...)
 ##!}
 ##!\arguments{
 ##!  \item{id}{The job-id which is the output from \code{inla} when the job is submitted,  the
 ##!            job-number or job-name}
-##!  \item{remove}{Logical If FALSE, leave the job on the server after getting-it,  otherwise remove it.}
+##!  \item{remove}{Logical If FALSE, leave the job on the server after getting-it, otherwise remove it.}
 ##!  \item{x}{An \code{inla.get}-object,  which is the output from \code{inla.get()}}
 ##!  \item{object}{An \code{inla.get}-object,  which is the output from \code{inla.get()}}
 ##!  \item{...}{ other arguments.}
 ##!}
 ##!\details{
-##!\code{inla.get} returns a list of all current jobs if \code{id=NULL},  otherwise it will get the results
-##!from the job with job-id/number/name \code{id}.
+##!\code{inla.qstat} shows jobs on the server,
+##!\code{inla.qget} fetch the results (and by default remove
+##!the files on the server),  wheras \code{inla.qdel} removes 
+##!a job on the server.
 ##!}
 ##!\value{
-##!  \code{inla.get()} returns an \code{inla.get}-object with information about current jobs.
+##!  \code{inla.qstat} returns an \code{inla.q}-object with information about current jobs.
 ##!}
 ##!\author{Havard Rue}
 ##!\seealso{ \code{\link{inla}} }
 ##!\examples{
 ##!\dontrun{
 ##!r = inla(y~1, data = data.frame(y=rnorm(10)), inla.call="submit")
-##!inla.get()
-##!r = inla.get(r)
+##!inla.qstat()
+##!r = inla.get(r, remove=FALSE)
+##!inla.qdel(1)
 ##!}
 ##!}
 
-`inla.get` = function(id = NULL, remove = TRUE)
+`summary.inla.q` = function(object, ...)
 {
+    print(object, ...)
+}
+
+`print.inla.q` = function(x, ...)
+{
+    if (length(x) == 0) {
+        cat("No jobs available\n")
+    } else {
+        if (length(x) == 1) {
+            cat("You have", length(x), "job\n")
+        } else {
+            cat("You have", length(x), "jobs\n")
+        }
+        for(k in seq_along(x)) {
+            cat("\t Job:", k, "\tName:", x[[k]]$id, "\tStatus:", x[[k]]$status, "\n")
+        }
+    }
+}
+
+`inla.qget` = function(id, remove = TRUE)
+{
+    return (inla.q(cmd = "get", id = id, remove = remove))
+}
+`inla.qdel` = function(id)
+{
+    return (inla.q(cmd = "del", id = id))
+}
+`inla.qstat` = function()
+{
+    return (inla.q(cmd = "stat"))
+}
+
+`inla.q` = function(cmd = c("get", "del", "stat"), id, remove = TRUE)
+{
+    cmd = match.arg(cmd)
     ## define some environment variables for remote computing
     inla.eval(paste("Sys.setenv(", "\"INLA_PATH\"", "=\"", system.file("bin", package="INLA"), "\"", ")", sep=""))
     inla.eval(paste("Sys.setenv(", "\"INLA_OS\"", "=\"", inla.os.type() , "\"", ")", sep=""))
@@ -57,12 +99,12 @@
         }
     }
 
-    inla.call = system.file("bin/remote/inla.get", package="INLA")
+    inla.call = system.file("bin/remote/inla.q", package="INLA")
     if (inla.os("windows")) {
         inla.call = paste(inla.call, ".cygwin", sep="")
     }
 
-    if (is.list(id)) {
+    if (!missing(id) && is.list(id)) {
         ret = id$ret
         id = id$id
     } else {
@@ -70,17 +112,21 @@
     }
 
     if (inla.os("linux") || inla.os("mac")) {
-        output = system(paste(shQuote(inla.call), inla.ifelse(is.null(id), "NULL", id), as.numeric(remove)), intern=TRUE)
+        output = system(paste(shQuote(inla.call), cmd, inla.ifelse(missing(id) || is.null(id), "NULL", id), as.numeric(remove)), intern=TRUE)
     } else {
         bat.file = paste(tempfile(), ".BAT",  sep="")
         cat("@echo off\n",  file=bat.file, append=FALSE)
-        cat(paste(shQuote(inla.call), inla.ifelse(is.null(id), "NULL", id), as.numeric(remove)))
+        cat(paste(shQuote(inla.call), cmd, inla.ifelse(missing(id) || is.null(id), "NULL", id), as.numeric(remove)))
         output = try(shell(paste("@", shQuote(bat.file), sep=""), wait=TRUE, intern=TRUE))
         unlink(bat.file)
     }
 
     if (length(grep("^ERROR", output)) > 0) {
+        output = gsub("^ERROR *", "", output)
         stop(output)
+    } else if (length(grep("^DELETE", output)) > 0) {
+        output = gsub("^DELETE *", "", output)
+        cat("Delete job", output, "\n")
     } else if (length(output) > 0 && length(strsplit(output, " ")[[1]]) == 1) {
         r = inla.collect.results(output)
         rr = c(r, ret)
@@ -109,28 +155,7 @@
         } else {
             output = list()
         }
-        class(output) = "inla.get"
+        class(output) = "inla.q"
         return (output)
-    }
-}
-
-`summary.inla.get` = function(object, ...)
-{
-    print(object, ...)
-}
-
-`print.inla.get` = function(x, ...)
-{
-    if (length(x) == 0) {
-        cat("No jobs available\n")
-    } else {
-        if (length(x) == 1) {
-            cat("You have", length(x), "job\n")
-        } else {
-            cat("You have", length(x), "jobs\n")
-        }
-        for(k in seq_along(x)) {
-            cat("\t Job:", k, "\tName:", x[[k]]$id, "\tStatus:", x[[k]]$status, "\n")
-        }
     }
 }
