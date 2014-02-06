3edb9563f10da992c6911ef14cc775ce7d38a164
Andrea Riebler <andrea.riebler@ifpsm.uzh.ch>
2014-02-06 11:04:16 +0100

    update in the documentation of the meb and mec model. Thanks to SM and MR.

--- r-inla.org/doc/latent/meb-example.R
+++ r-inla.org/doc/latent/meb-example.R
@@ -3,7 +3,9 @@ beta = 2
 w = rnorm(n)
 prec.u = 1
 prec.y = 1
+## heteroscedastic scaling
 s = runif(n,min=0,max=1)
+## true but unobserved covariate
 x = w + rnorm(n, sd = 1/sqrt(s*prec.u))
 y = 1 + beta*x + rnorm(n, sd = 1/sqrt(prec.y))
 
@@ -13,26 +15,29 @@ prior.prec.u = c(10, 9/prec.u)
 prior.prec.y = c(10, 9/prec.y)
 
 
-formula = y ~ f(w,model="meb", scale=s,
-                 hyper = list(
-                      beta = list( 
-                          param = prior.beta, 
-                          fixed = FALSE
-                          ),
-                      prec.u = list(
-                          param = prior.prec.u, 
-                          fixed = FALSE
-                          )
-                      )
-               )
-
-r = inla(formula, data = data.frame(y, w, s), 
-        family = "gaussian", 
-        control.family = list(
-           hyper = list(
-                    prec = list(param = prior.prec.y, 
-                                fixed = FALSE
-                           )
-                    )
+formula = y ~ f(w, model="meb", scale=s, values=w,
+    hyper = list(
+        beta = list(
+            prior = "gaussian",
+            param = prior.beta,
+            fixed = FALSE
+        ),
+        prec.u = list(
+            prior = "loggamma",
+            param = prior.prec.u,
+            initial = log(prec.u),
+            fixed = FALSE
         )
     )
+)
+
+r = inla(formula, data = data.frame(y, w, s),
+    family = "gaussian",
+    control.family = list(
+        hyper = list(
+            prec = list(param = prior.prec.y,
+                fixed = FALSE
+            )
+        )
+    )
+)
