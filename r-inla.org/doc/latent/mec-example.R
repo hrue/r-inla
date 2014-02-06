8d098ffefb10ccc559fcef797c7b2174903a7afd
Andrea Riebler <andrea.riebler@ifpsm.uzh.ch>
2014-02-06 11:04:16 +0100

    update in the documentation of the meb and mec model. Thanks to SM and MR.

--- r-inla.org/doc/latent/mec-example.R
+++ r-inla.org/doc/latent/mec-example.R
@@ -4,8 +4,8 @@ prec.y = 1
 prec.u = 1
 prec.x = 1
 ## true unobserved covariate
-x = rnorm(n, sd = 1/sqrt(prec.x)) 
-## the observed covariate
+x = rnorm(n, sd = 1/sqrt(prec.x))
+## the observed covariate with heteroscedastic scaling
 s = runif(n,min=0.5,max=2)
 w = x + rnorm(n, sd = 1/sqrt(prec.u*s))
 ## regression model using the unobserved 'x'
@@ -18,41 +18,47 @@ prior.prec.x = c(10, 9)
 prior.prec.y = c(10, 9)
 
 
-formula = y ~ 1 + 
-    f(w, model="mec", scale=s,
-      hyper = list(
-              beta = list(
-                      param = prior.beta,
-                      fixed = FALSE
-                      ),
-              prec.u = list(
-                      param = prior.prec.u,
-                      initial = log(prec.u),
-                      fixed = FALSE
-                      ),
-              prec.x = list(
-                      param = prior.prec.x,
-                      initial = log(prec.x),
-                      fixed = FALSE
-                      ),
-              mean.x = list(
-                      initial = 0,
-                      fixed=TRUE
-                      )
-              )
-      )
+formula = y ~ 1 +
+f(w, model="mec", scale=s, values=w,
+    hyper = list(
+        beta = list(
+            prior = "gaussian",
+            param = prior.beta,
+            fixed = FALSE
+        ),
+        prec.u = list(  
+            prior = "loggamma",
+            param = prior.prec.u,
+            initial = log(prec.u),
+            fixed = FALSE
+        ),
+        prec.x = list(
+            prior = "loggamma",
+            param = prior.prec.x,
+            initial = log(prec.x),
+            fixed = FALSE
+        ),
+        mean.x = list(
+            prior = "gaussian",
+            initial = 0,
+            fixed=TRUE
+        )
+    )
+)
 
 r = inla(formula,
-        data = data.frame(y, w, s), 
-        family = "gaussian",
-        control.family = list(
-                hyper = list(
-                        prec = list(param = prior.prec.y, 
-                                initial = log(prec.y),
-                                fixed=FALSE
-                                )
-                        )
-                )
+    data = data.frame(y, w, s),
+    family = "gaussian",
+    control.family = list(
+        hyper = list(
+            prec = list(param = prior.prec.y,
+                initial = log(prec.y),
+                fixed=FALSE
+            )
         )
+    )
+)
 
 summary(r)
+
+
