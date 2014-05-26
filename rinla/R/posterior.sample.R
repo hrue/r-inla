774a0edc006f5c24d359cb2d7ba18c3e082ad196
eliaskrainski <eliaskrainski@localhost>
2014-05-26 14:33:01 +0200

    small fix

--- rinla/R/posterior.sample.R
+++ rinla/R/posterior.sample.R
@@ -107,15 +107,16 @@
                                 deriv.func = inla.eval(paste("function(", arg[1L], ",",  arg[2L], "=", arg.val[2L], ",", arg[3L], "=", arg.val[3L], ") {}"))
                             }
                         }
-                        body(deriv.func) = try(D(body(result$misc$from.theta[[i]]), arg[1L]), silent=TRUE)
-                        if (class(.Last.value)=='try-error') {
+                        temptest <- try(D(body(result$misc$from.theta[[i]]), arg[1L]), silent=TRUE)
+                        if (!inherits(temptest, "try-error")) {
+                            body(deriv.func) <- temptest 
+                            log.J = log.J - log(abs(deriv.func(cs$config[[k]]$theta[i]))) ## Yes, it's a minus...
+                        }
+                        else {
                             h = .Machine$double.eps^0.25
                             theta.1 = do.call(result$misc$from.theta[[i]], args = list(cs$config[[k]]$theta[i] - h))
                             theta.2 = do.call(result$misc$from.theta[[i]], args = list(cs$config[[k]]$theta[i] + h))
                             log.J = log.J - log(abs((theta.2 - theta.1)/(2.0*h))) ## Yes, it's a minus...
-                        }
-                        else {
-                            log.J = log.J - log(abs(deriv.func(cs$config[[k]]$theta[i]))) ## Yes, it's a minus...
                         }
                     }
                     ## print(paste("logJ", log.J))
