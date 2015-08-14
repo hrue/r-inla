4db7410253140e9afc16505de2742befa88e5038
Havard Rue <havard.rue@math.ntnu.no>
2015-08-14 13:35:42 +0200

    Small fix (I hope) to protect some special characters when using directories with space-char in, on Windows

--- rinla/R/cygwin.R
+++ rinla/R/cygwin.R
@@ -5,13 +5,19 @@
     return (file.exists(path) && file.info(path)$isdir)
 }
 
+`inla.cygwin.protect.special.chars` = function(cmd)
+{
+    cmd = gsub(" ", "\\\\ ", cmd)
+    return (cmd) 
+}
+
 `inla.cygwin.run.command` = function(command, file.log = NULL, ...)
 {
     if (inla.cygwin.check.path()) {
         cmd = paste(inla.getOption("cygwin"),
                      "/bin/bash.exe -c ",
                      shQuote(paste("export PATH=/bin:/usr/bin:$PATH;",
-                                   command,
+                                   inla.cygwin.protect.special.chars(command),
                                    inla.ifelse(is.null(file.log), "", paste(" > ", file.log)), 
                                    sep=""), type="cmd"), sep="")
         system(cmd, ...)
@@ -22,10 +28,16 @@
 
 `inla.cygwin.map.filename` = function(filename, windows2cygwin = TRUE)
 {
-    if (windows2cygwin)
-        return (inla.paste(inla.cygwin.run.command(paste("cygpath -u ", filename), intern = TRUE, ignore.stderr = TRUE)))
-    else
-        return (inla.paste(inla.cygwin.run.command(paste("cygpath -w -m -s ", filename), intern = TRUE, ignore.stderr = TRUE)))
+    if (windows2cygwin) {
+        fnm = inla.paste(inla.cygwin.run.command(paste("cygpath -u ",
+            inla.cygwin.protect.special.chars(filename)),
+            intern = TRUE, ignore.stderr = TRUE))
+    } else {
+        fnm = inla.paste(inla.cygwin.run.command(paste("cygpath -w -m -s ",
+            inla.cygwin.protect.special.chars(filename)),
+            intern = TRUE, ignore.stderr = TRUE))
+    }
+    return (fnm)
 }
 
 
