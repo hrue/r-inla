cae7bdc4329396e64533c905dadab44798d09193
Håvard Rue <haavard.rue@kaust.edu.sa>
2015-11-12 14:32:22 +0100

    Added cpoisson likelihood. Fixed an error with prior-table for a group hyperparameter (thanks to MHF)

---
+++ r-inla.org/doc/likelihood/example-cpoisson.R
@@ -0,0 +1,16 @@
+n=100
+a = 1
+b = 1
+z = rnorm(n)
+eta = a + b*z
+C = 5
+E = sample(1:10, n, replace=TRUE)
+lambda = E*exp(eta)
+y = rpois(n, lambda = lambda)
+y[y <= C] = 0
+
+data = list(y=y,z=z)
+formula = y ~ 1+z
+result = inla(formula, family = "cpoisson", data = data, E=E,
+    control.family = list(cpoisson.C = C))
+summary(result)
