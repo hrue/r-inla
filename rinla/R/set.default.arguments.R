a849466da31ec0bbb576ddb657dba722bed8681f
Havard Rue <havard.rue@math.ntnu.no>
2015-11-15 09:22:29 +0100

    Changed the cenpoission censoring spesification into an interval [low, high], so that all values in that interval is censored. (Thanks to PB.)

--- rinla/R/set.default.arguments.R
+++ rinla/R/set.default.arguments.R
@@ -290,8 +290,8 @@
         ##:ARGUMENT: gev.scale.xi The internal scaling of the shape-parameter for the GEV distribution. (default 0.01)
         gev.scale.xi = 0.01,
 
-        ##:ARGUMENT: cenpoisson.C The censoring value for the censored Poisson.
-        cenpoisson.C = -1L,
+        ##:ARGUMENT: cenpoisson.I The censoring interval for the censored Poisson
+        cenpoisson.I = c(-1L, -1L),
 
         ##:ARGUMENT: variant This variable is used to give options for various variants of the likelihood,  like chosing different parameterisations for example. See the relevant likelihood documentations for options (does only apply to some likelihoods).
         variant = 0L,
