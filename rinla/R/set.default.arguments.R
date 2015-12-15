8f09b5ca3d2e5ae89aab6116c3829390a149fbd4
Havard Rue <havard.rue@math.ntnu.no>
2015-12-15 09:57:39 +0100

    Improved the doc for control.inla

--- rinla/R/set.default.arguments.R
+++ rinla/R/set.default.arguments.R
@@ -357,153 +357,149 @@
     ##:EXTRA: 
     ##:NAME: control.inla
     ans = list(
-        ##:ARGUMENT: strategy  The strategy to use for the approximations; one of 'gaussian', 'simplified.laplace' (default) or 'laplace'
+        ##:ARGUMENT: strategy  Character The strategy to use for the approximations; one of 'gaussian', 'simplified.laplace' (default) or 'laplace'
         strategy="simplified.laplace",
 
-        ##:ARGUMENT: int.strategy  The integration strategy to use; one of 'ccd' (default), 'grid' or 'eb' (empirical bayes)
+        ##:ARGUMENT: int.strategy  Character The integration strategy to use; one of 'ccd' (default), 'grid' or 'eb' (empirical bayes)
         int.strategy="ccd",
 
-        ##:ARGUMENT: interpolator  The interpolator used to compute the marginals for the hyperparameters. One of 'auto', 'nearest', 'quadratic', 'weighted.distance', 'ccd', 'ccdintegrate', 'gridsum', 'gaussian'. Default is 'auto'.
+        ##:ARGUMENT: interpolator  Character The interpolator used to compute the marginals for the hyperparameters. One of 'auto', 'nearest', 'quadratic', 'weighted.distance', 'ccd', 'ccdintegrate', 'gridsum', 'gaussian'. Default is 'auto'.
         interpolator="auto",
 
-        ##:ARGUMENT: fast Fast mode? If on, then replace conditional modes in the Laplace approximation with conditional expectation (default TRUE)
+        ##:ARGUMENT: fast Logical If TRUE, then replace conditional modes in the Laplace approximation with conditional expectation (default TRUE)
         fast = TRUE,
             
-        ##:ARGUMENT: linear.correction  Default TRUE for the 'strategy = laplace' option.
+        ##:ARGUMENT: linear.correction Logical Default TRUE for the 'strategy = laplace' option.
         linear.correction=NULL,
 
-        ##:ARGUMENT: h The step-length for the gradient calculations for the hyperparameters. Default 0.01.
+        ##:ARGUMENT: h Numerical The step-length for the gradient calculations for the hyperparameters. Default 0.01.
         h=0.01,
 
-        ##:ARGUMENT: dz The step-length in the standarised scale for the integration of the hyperparameters. Default 1.0.
+        ##:ARGUMENT: dz Numerical The step-length in the standarised scale for the integration of the hyperparameters. Default 1.0.
         dz=1.0,
 
-        ##:ARGUMENT: diff.logdens The difference of the log.density for the hyperpameters to stop numerical integration using int.strategy='grid'. Default 2.5.
+        ##:ARGUMENT: diff.logdens Numerical The difference of the log.density for the hyperpameters to stop numerical integration using int.strategy='grid'. Default 2.5.
         diff.logdens=2.5,
 
-        ##:ARGUMENT: print.joint.hyper If TRUE, the store also the joint distribution of the hyperparameters (without any costs). Default TRUE.
+        ##:ARGUMENT: print.joint.hyper Logical If TRUE, the store also the joint distribution of the hyperparameters (without any costs). Default TRUE.
         print.joint.hyper=TRUE,
 
-        ##:ARGUMENT: force.diagonal A boolean variable, if TRUE, then force the Hessian to be diagonal. (Default FALSE.)
+        ##:ARGUMENT: force.diagonal Logical If TRUE, then force the Hessian to be diagonal. (Default FALSE.)
         force.diagonal=FALSE,
 
-        ##:ARGUMENT: skip.configurations A boolean variable; skip configurations if the values at the main axis are to small. (Default TRUE.)
+        ##:ARGUMENT: skip.configurations Logical Skip configurations if the values at the main axis are to small. (Default TRUE.)
         skip.configurations=TRUE,
 
-        ##:ARGUMENT: mode.known A boolean variable: If TRUE then no optimisation is done. (Default FALSE.)
+        ##:ARGUMENT: mode.known Logical If TRUE then no optimisation is done. (Default FALSE.)
         mode.known=FALSE,
 
-        ##:ARGUMENT: adjust.weights A boolean variable; If TRUE then just more accurate integration weights. (Default TRUE.)
+        ##:ARGUMENT: adjust.weights Logical If TRUE then just more accurate integration weights. (Default TRUE.)
         adjust.weights=TRUE,
 
-        ##:ARGUMENT: tolerance The tolerance for the optimisation of the hyperparameters. If set, this is the default value for for 'tolerance.f^(2/3)',  'tolerance.g' and  'tolerance.x'; see below.
+        ##:ARGUMENT: tolerance Numerical The tolerance for the optimisation of the hyperparameters. If set, this is the default value for for 'tolerance.f^(2/3)',  'tolerance.g' and  'tolerance.x'; see below.
         tolerance = 0.005,
 
-        ##:ARGUMENT: tolerance.f The tolerance for the absolute change in the log posterior in the optimisation of the hyperparameters.
+        ##:ARGUMENT: tolerance.f Numerical The tolerance for the absolute change in the log posterior in the optimisation of the hyperparameters.
         tolerance.f = NULL,
 
-        ##:ARGUMENT: tolerance.g The tolerance for the absolute change in the gradient of the log posterior in the optimisation of the hyperparameters.
+        ##:ARGUMENT: tolerance.g Numerical The tolerance for the absolute change in the gradient of the log posterior in the optimisation of the hyperparameters.
         tolerance.g = NULL,
 
-        ##:ARGUMENT: tolerance.x The tolerance for the change in the hyperparameters (root-mean-square) in the optimisation of the hyperparameters.
+        ##:ARGUMENT: tolerance.x Numerical The tolerance for the change in the hyperparameters (root-mean-square) in the optimisation of the hyperparameters.
         tolerance.x = NULL, 
 
-        ##:ARGUMENT: restart To improve the optimisation, the optimiser is restarted at the found optimum 'restart' number of times.
+        ##:ARGUMENT: restart Numerical To improve the optimisation, the optimiser is restarted at the found optimum 'restart' number of times.
         restart = 0L,
 
-        ##:ARGUMENT: optimiser The optimiser to use; one of 'gsl', 'domin' or 'default'.
+        ##:ARGUMENT: optimiser Character The optimiser to use; one of 'gsl', 'domin' or 'default'.
         optimiser = "default",
 
-        ##:ARGUMENT: verbose A boolean variable; run in verbose mode? (Default FALSE)
+        ##:ARGUMENT: verbose Logical Run in verbose mode? (Default FALSE)
         verbose = NULL,
 
-        ##:ARGUMENT: reordering Type of reordering to use. (EXPERT OPTION; one of "AUTO", "DEFAULT", "IDENTITY", "REVERSEIDENTITY",  "BAND", "METIS", "GENMMD", "AMD", "MD", "MMD", "AMDBAR", "AMDC", "AMDBARC",  or the output from \code{inla.qreordering}.)
+        ##:ARGUMENT: reordering Character Type of reordering to use. (EXPERT OPTION; one of "AUTO", "DEFAULT", "IDENTITY", "REVERSEIDENTITY",  "BAND", "METIS", "GENMMD", "AMD", "MD", "MMD", "AMDBAR", "AMDC", "AMDBARC",  or the output from \code{inla.qreordering}.)
         reordering = "auto",
 
-        ##:ARGUMENT: cpo.diff Threshold to define when the cpo-calculations are inaccurate. (EXPERT OPTION.)
+        ##:ARGUMENT: cpo.diff Numerical Threshold to define when the cpo-calculations are inaccurate. (EXPERT OPTION.)
         cpo.diff = NULL,
 
-        ##:ARGUMENT: npoints Number of points to use in the 'stratey=laplace' approximation
+        ##:ARGUMENT: npoints Numerical Number of points to use in the 'stratey=laplace' approximation
         npoints = 9,
 
-        ##:ARGUMENT: cutoff The cutoff used in the 'stratey=laplace' approximation. (Smaller value is more accurate and more slow.)
+        ##:ARGUMENT: cutoff Numerical The cutoff used in the 'stratey=laplace' approximation. (Smaller value is more accurate and more slow.)
         cutoff = 1e-4,
 
-        ##:ARGUMENT: adapt.hessian.mode A boolean variable; should optimisation be continued if the Hessian estimate is void? (Default TRUE)
+        ##:ARGUMENT: adapt.hessian.mode Logical Should optimisation be continued if the Hessian estimate is void? (Default TRUE)
         adapt.hessian.mode = NULL,
 
-        ##:ARGUMENT: adapt.hessian.max.trials Number of steps in the adaptive Hessian optimisation
+        ##:ARGUMENT: adapt.hessian.max.trials Numerical Number of steps in the adaptive Hessian optimisation
         adapt.hessian.max.trials = NULL,
 
-        ##:ARGUMENT: adapt.hessian.scale The scaling of the 'h' after each trial.
+        ##:ARGUMENT: adapt.hessian.scale Numerical The scaling of the 'h' after each trial.
         adapt.hessian.scale = NULL, 
 
-        ##:ARGUMENT: huge A boolean variable; if TRUE then try to do some of the internal parallisations differently. Hopefully this will be of benefite for 'HUGE' models. (Default FALSE.) [THIS OPTION IS OBSOLETE AND NOT USED!]
+        ##:ARGUMENT: huge Logical If TRUE then try to do some of the internal parallisations differently. Hopefully this will be of benefite for 'HUGE' models. (Default FALSE.) [THIS OPTION IS OBSOLETE AND NOT USED!]
         huge = FALSE,
 
-        ##:ARGUMENT: step.len The step-length used to compute numerical derivaties of the log-likelihood
+        ##:ARGUMENT: step.len Numerical The step-length used to compute numerical derivaties of the log-likelihood
         step.len = .Machine$double.eps^(1.0/5.5),
 
-        ##:ARGUMENT: stencil Number of points in the stencil used to compute the numerical derivaties of the log-likelihood (3, 5 or 7).
+        ##:ARGUMENT: stencil Numerical Number of points in the stencil used to compute the numerical derivaties of the log-likelihood (3, 5 or 7).
         stencil = 5L, 
 
-        ##:ARGUMENT: lincomb.derived.only A boolean variable: if TRUE the only compute the marginals for the derived linear combinations and if FALSE, the and also the linear combinations to the graph (Default TRUE)
+        ##:ARGUMENT: lincomb.derived.only Logical If TRUE the only compute the marginals for the derived linear combinations and if FALSE, the and also the linear combinations to the graph (Default TRUE)
         lincomb.derived.only = TRUE,
 
-        ##:ARGUMENT: lincomb.derived.correlation.matrix A boolean variable: if TRUE compute also the correlations for the derived linear combinations, if FALSE do not (Default FALSE)
+        ##:ARGUMENT: lincomb.derived.correlation.matrix Logical If TRUE compute also the correlations for the derived linear combinations, if FALSE do not (Default FALSE)
         lincomb.derived.correlation.matrix = FALSE,
 
-        ##:ARGUMENT: diagonal Expert use only! Add a this value on the diagonal of the joint precision matrix.
+        ##:ARGUMENT: diagonal Numerical Expert use only! Add a this value on the diagonal of the joint precision matrix.
         diagonal = 0.0,
 
-        ##:ARGUMENT: numint.maxfeval Maximum number of function evaluations in the the numerical integration for the hyperparameters. (Default 10000.)
+        ##:ARGUMENT: numint.maxfeval Numerical Maximum number of function evaluations in the the numerical integration for the hyperparameters. (Default 10000.)
         numint.maxfeval = 100000,
 
-        ##:ARGUMENT: numint.relerr Relative error requirement in the the numerical integration for the hyperparameters. (Default 1e-5)
+        ##:ARGUMENT: numint.relerr Numerical Relative error requirement in the the numerical integration for the hyperparameters. (Default 1e-5)
         numint.relerr = 1e-5,
 
-        ##:ARGUMENT: numint.abserr Absolute error requirement in the the numerical integration for the hyperparameters. (Default 1e-6)
+        ##:ARGUMENT: numint.abserr Numerical Absolute error requirement in the the numerical integration for the hyperparameters. (Default 1e-6)
         numint.abserr = 1e-6,
 
-        ##:ARGUMENT: cmin The minimum value for the negative Hessian from the likelihood. Increasing this value will stabalise the optimisation. (Default 0.0)
+        ##:ARGUMENT: cmin Numerical The minimum value for the negative Hessian from the likelihood. Increasing this value will stabalise the optimisation. (Default 0.0)
         cmin = 0.0,
 
-        ##:ARGUMENT: step.factor The step factor in the Newton-Raphson algorithm saying how large step to take (Default 1.0)
+        ##:ARGUMENT: step.factor Numerical The step factor in the Newton-Raphson algorithm saying how large step to take (Default 1.0)
         ## YES! setting this to a negative values means = 1,  EXCEPT the first time (for each thread) where |step.factor| is used.
         ## This is an hidden option.
         step.factor = -0.1,
 
-        ##:ARGUMENT: global.node.factor The factor which defines the degree required (how many neighbors), as a fraction of n-1, that is required to be classified as a global node and numbered last (whatever the reordering routine says). Here,  n,  is the size of the graph. (Disabled if larger than 1.)
+        ##:ARGUMENT: global.node.factor Numerical The factor which defines the degree required (how many neighbors), as a fraction of n-1, that is required to be classified as a global node and numbered last (whatever the reordering routine says). Here,  n,  is the size of the graph. (Disabled if larger than 1.)
         global.node.factor = 2.0, 
 
-        ##:ARGUMENT: global.node.degree The degree required (number of neighbors) to be classified as a global node and numbered last (whatever the reordering routine says).
+        ##:ARGUMENT: global.node.degree Numerical The degree required (number of neighbors) to be classified as a global node and numbered last (whatever the reordering routine says).
         global.node.degree = .Machine$integer.max,
 
-        ##:ARGUMENT: stupid.search Enable or disable the stupid-search-algorithm, if the Hessian calculations reveals that the mode is not found. (Default \code{TRUE}.)
+        ##:ARGUMENT: stupid.search Logical Enable or disable the stupid-search-algorithm, if the Hessian calculations reveals that the mode is not found. (Default \code{TRUE}.)
         stupid.search = TRUE,
             
-        ##:ARGUMENT: stupid.search.max.iter Maximum number of iterations allowed for the stupid-search-algorithm.
+        ##:ARGUMENT: stupid.search.max.iter Numerical Maximum number of iterations allowed for the stupid-search-algorithm.
         stupid.search.max.iter = 1000L, 
 
-        ##:ARGUMENT: stupid.search.factor Factor (>=1) to increase the step-length with after each new interation.
+        ##:ARGUMENT: stupid.search.factor Numerical Factor (>=1) to increase the step-length with after each new interation.
         stupid.search.factor = 1.05,
         
-        ##:ARGUMENT: correct Add correction for the Laplace approximation.
+        ##:ARGUMENT: correct Logical Add correction for the Laplace approximation.
         correct = FALSE,
 
-        ##:ARGUMENT: correct.factor Factor used in adjusting the correction factor (default=10) if correct=TRUE
+        ##:ARGUMENT: correct.factor Numerical Factor used in adjusting the correction factor (default=10) if correct=TRUE
         correct.factor = 10.0,
 
-        ##:ARGUMENT: correct.strategy  The strategy used to compute the correction; one of 'simplified.laplace' (default) or 'laplace'
+        ##:ARGUMENT: correct.strategy  Character The strategy used to compute the correction; one of 'simplified.laplace' (default) or 'laplace'
         correct.strategy = "simplified.laplace", 
 
-        ##:ARGUMENT: correct.verbose  Be verbose when computing the correction?
+        ##:ARGUMENT: correct.verbose  Logical Be verbose when computing the correction?
         correct.verbose = FALSE)
  
-    ## use default Gaussian strategy if the observations are gaussian
-    if (all(tolower(family) %in% "gaussian"))
-        ans$strategy = "gaussian"
-
     ##:SEEALSO: inla
 
     return (ans)
