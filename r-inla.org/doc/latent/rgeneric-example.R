22a10c3792d5809b214801a89060a2e5921a70a8
Håvard Rue <haavard.rue@kaust.edu.sa>
2016-03-01 21:15:42 +0100

    Fixed missprint in the doc for rgeneric

--- r-inla.org/doc/latent/rgeneric-example.R
+++ r-inla.org/doc/latent/rgeneric-example.R
@@ -2,6 +2,6 @@ n = 100
 rho=0.9
 x = arima.sim(n, model = list(ar = rho)) * sqrt(1-rho^2)
 y = x + rnorm(n, sd = 0.1)
-model = inla.rgeneric2.define(inla.rgeneric.ar1.model, n=n)
+model = inla.rgeneric.define(inla.rgeneric.ar1.model, n=n)
 formula = y ~ -1 + f(idx, model=model)
 r = inla(formula, data = data.frame(y, idx = 1:n), family = "gaussian")
