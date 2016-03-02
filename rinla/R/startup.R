05030e282a8829f87bc4303b42292db5bf5f5d4c
Håvard Rue <haavard.rue@kaust.edu.sa>
2016-03-02 13:22:13 +0100

    Added startup message

---
+++ rinla/R/startup.R
@@ -0,0 +1,30 @@
+## copied from the mgcv-code...
+
+inla.print.version <- function()
+{
+    info = library(help=INLA)$info[[1]]
+    version <- info[pmatch("Version", info)]
+    date <- info[pmatch("Date", info)]
+    um <- strsplit(version," ")[[1]]
+    version <- um[nchar(um)>0][2]
+    um <- strsplit(date," ")[[1]]
+    date <- um[nchar(um)>0]
+
+    hello <- paste("This is INLA ", version, ", dated ",
+                   date[2], " (", date[9], date[11], ".", "\n",
+                   "See www.r-inla.org/contact-us for how to get help.", 
+                   sep="")
+    packageStartupMessage(hello)
+}
+
+.onLoad <- function(...)
+{
+    ##
+}
+
+.onAttach <- function(...)
+{ 
+    inla.print.version()
+}
+
+.onUnload <- function(libpath) library.dynam.unload("INLA", libpath)
