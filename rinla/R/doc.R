fd83708a31dcef01b9b5b990f6dd418519a0a230
Håvard Rue <haavard.rue@kaust.edu.sa>
2016-03-05 18:32:05 +0100

    Changed from http:// to https://

--- rinla/R/doc.R
+++ rinla/R/doc.R
@@ -16,7 +16,7 @@
 ##!\item{verbose}{Logical if \code{TRUE} then run in verbose mode}
 ##!}
 ##!\author{Havard Rue \email{hrue@math.ntnu.no}}
-##!\seealso{\code{http://www.r-inla.org}}
+##!\seealso{\code{https://www.r-inla.org}}
 ##!\examples{
 ##!\dontrun{inla.doc("rw2")}
 ##!\dontrun{inla.doc("gaussian")}
