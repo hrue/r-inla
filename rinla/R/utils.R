088ef7d410ff13b80f6e0ad4575b37cc4f665f7e
Havard Rue <havard.rue@math.ntnu.no>
2016-06-22 09:33:08 +0200

    A small fix in an error message

--- rinla/R/utils.R
+++ rinla/R/utils.R
@@ -1024,7 +1024,7 @@
                    "Locations are too close for f(",
                    term, ", model=\"",
                    model, "\", ...): ", 
-                   " min.diff = ",
+                   " min.diff/range = ",
                    format(min.diff, scientific=TRUE, digits=4),
                    " < ", format(lim, scientific=TRUE, digits=4), "\n", 
                    "  You can fix this by some kind of binning, see ?inla.group", "\n",
