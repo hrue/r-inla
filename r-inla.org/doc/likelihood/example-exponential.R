bb2c5842bc1acffe6cfe514a3b488299b828cddc
Havard Rue <havard.rue@math.ntnu.no>
2016-08-06 19:54:30 +0200

    Converted exponential likelihood into exponential and exponential.surv.

--- r-inla.org/doc/likelihood/example-exponential.R
+++ r-inla.org/doc/likelihood/example-exponential.R
@@ -1,8 +1,13 @@
-n = 10000
+n = 1000
 x = runif(n)
 lambda = exp(1+x)
 y = rexp(n, rate=lambda)
 event = rep(1,n)
 data = list(y=y, event=event, x=x)
-formula = inla.surv(y,event)~ x 
-model = inla(formula, family ="exponential", data=data, verbose=T)
+formula = inla.surv(y,event) ~ x 
+model = inla(formula, family ="exponential.surv", data=data)
+summary(model)
+
+formula = y ~ x 
+model = inla(formula, family ="exponential", data=data)
+summary(model)
