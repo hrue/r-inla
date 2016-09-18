2f36d1ab687b0f3158949dc4c89869d8ee025ff0
Havard Rue <havard.rue@math.ntnu.no>
2016-09-18 23:33:49 +0200

    Some initial work

---
+++ inlaprog/src/fgn.h
@@ -0,0 +1,70 @@
+
+/* fgn.h
+ * 
+ * Copyright (C) 2016 Havard Rue
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * The author's contact information:
+ *
+ *       H{\aa}vard Rue
+ *       Department of Mathematical Sciences
+ *       The Norwegian University of Science and Technology
+ *       N-7491 Trondheim, Norway
+ *       Voice: +47-7359-3533    URL  : http://www.math.ntnu.no/~hrue  
+ *       Fax  : +47-7359-3524    Email: havard.rue@math.ntnu.no
+ *
+ */
+#ifndef __INLA_FGN_H__
+#define __INLA_FGN_H__
+#undef __BEGIN_DECLS
+#undef __END_DECLS
+#ifdef __cplusplus
+#define __BEGIN_DECLS extern "C" {
+#define __END_DECLS }
+#else
+#define __BEGIN_DECLS					       /* empty */
+#define __END_DECLS					       /* empty */
+#endif
+__BEGIN_DECLS
+
+/* 
+ *
+ */
+
+#include "inla.h"
+
+typedef struct
+{
+	int N;					               /* total size = (k+1)*n */
+	int n;						       /* size of each component */
+	int k;						       /* number of components */
+
+	double prec_eps;				       /* fixed tiny noise */
+	double **log_prec;				       /* theta[0] */
+	double **H_intern;				       /* theta[1] */
+
+	double *w;					       /* the weights for the k components */
+	double *phi;					       /* the phi's for the k components */
+}
+	inla_fgn_arg_tp;
+
+int inla_make_fgn_graph(GMRFLib_graph_tp **graph, inla_fgn_arg_tp *def);
+double Qfunc_fgn(int i, int j, void *arg);
+int inla_fng_get(double *phi, double *w, double H);
+
+
+__END_DECLS
+#endif
