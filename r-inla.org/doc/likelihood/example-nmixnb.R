f5b0b61dab31a3f508123bfb7d4882d10012d5d0
Havard Rue <havard.rue@math.ntnu.no>
2017-02-06 11:09:30 +0300

    Added model nmix with negative binomial for the N

--- r-inla.org/doc/likelihood/example-nmixnb.R
+++ r-inla.org/doc/likelihood/example-nmixnb.R
@@ -3,12 +3,14 @@ n = 50
 y = matrix(NA, n, nrep)
 x = c()
 xx = c()
+size = 3
+overdispersion = 1/size
 intercept = 1
-
+    
 for(i in 1:n) {
     local.x = runif(1) - 0.5
     lambda = exp(2 + local.x)
-    N = rpois(1, lambda)
+    N = rnbinom(1, mu=lambda, size=size)
     local.xx = runif(1) - 0.5
     eta = intercept + local.xx
     p = exp(eta)/(exp(eta) + 1)
@@ -21,6 +23,6 @@ for(i in 1:n) {
 Y = inla.mdata(y, 1, x)
 r = inla(Y ~ 1 + xx,
          data = list(Y=Y, xx=xx), 
-         family = "nmix",
+         family = "nmixnb",
          control.fixed = list(prec.intercept=1,  prec=1))
 
--- rinla/R/create.data.file.R
+++ rinla/R/create.data.file.R
@@ -255,9 +255,11 @@
         null.dat = is.na(response[, 2L])
         response = response[!null.dat,]
 
-    } else if (inla.one.of(family, c("nmix"))) {
+    } else if (inla.one.of(family, c("nmix", "nmixnb"))) {
 
+        ## yes. it must be the same for both model nmix and nmixnb
         mmax = length(inla.model.properties(model="nmix", section="likelihood")$hyper)
+
         response = cbind(IDX=ind, y.orig)
         col.idx = grep("^IDX$", names(response))
         col.x = grep("^X[0-9]+", names(response))
