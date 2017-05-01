6b65bf746f8f264f4807095d40457ed241fd51f5
Håvard Rue <haavard.rue@kaust.edu.sa>
2017-05-01 08:27:49 +0300

    Made function inla.fgn vectorize

--- rinla/R/fgn.R
+++ rinla/R/fgn.R
@@ -14,36 +14,39 @@
 ##! } 
 ##!
 ##! \arguments{
-##!   \item{H}{The Hurst coeffcient (0<H<1)}
+##!   \item{H}{The Hurst coeffcient (0<H<1),  or a vector of those}
 ##!  }
 ##! \value{
-##!  A named vector of length 7, where the first element is \code{H},
-##!  the next three are the lag one correlations (or phi's),
-##!  and the last three are the weights.
+##!  A named matrix with 7 columns, where the first column is \code{H},
+##!  column 2,  3 and 4 are lag one correlations (or phi's),
+##!  and column 5,  6 and 7 are the weights.
 ##!
 ##!  This function is EXPERIMENTAL!!!
 ##! }
 ##! \author{Havard Rue \email{hrue@r-inla.org}}
 ##!
 ##! \examples{
-##!     inla.fgn(0.7)
+##!     r = c(inla.fgn(0.7))
+##!     r_m = inla.fgn(seq(0.6, 0.8, by=0.01))
 ##! } 
 
 `inla.fgn` = function(H)
 {
-    stopifnot(is.numeric(H) && length(H) == 1)
+    in.file = inla.tempfile()
     out.file = inla.tempfile()
+    inla.write.fmesher.file(matrix(as.numeric(H), ncol = 1), file = in.file)
     if (inla.os("linux") || inla.os("mac")) {
-        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m fgn", as.character(H), out.file), intern=TRUE)
+        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m fgn", in.file, out.file), intern=TRUE)
     } else if(inla.os("windows")) {
-        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m fgn", as.character(H), out.file), intern=TRUE)
+        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m fgn", in.file, out.file), intern=TRUE)
     } else {
         stop("\n\tNot supported architecture.")
     }
 
-    res = c(inla.read.fmesher.file(out.file))
-    k = (length(res)-1) %/% 2L
-    names(res) = c("H",  paste0("phi", 1:k), paste0("weight", 1:k))
+    res = inla.read.fmesher.file(out.file)
+    k = (ncol(res)-1L) %/% 2L
+    colnames(res) = c("H",  paste0("phi", 1:k), paste0("weight", 1:k))
+    unlink(in.file)
     unlink(out.file)
 
     return (res)
