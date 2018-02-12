34159276eb1a2d7946258bcd984ee0ce314db987
Håvard Rue <haavard.rue@kaust.edu.sa>
2018-02-12 13:53:27 +0300

    more work

--- gmrflib/smtp-pardiso.h
+++ gmrflib/smtp-pardiso.h
@@ -1,4 +1,5 @@
-/* GMRFLib-smtp-pardiso.h
+
+/* smtp-pardiso.h
  * 
  * Copyright (C) 2018 Havard Rue & Alexander Litvinenko
  * 
@@ -51,27 +52,105 @@
 #define __END_DECLS					       /* empty */
 #endif
 
-__BEGIN_DECLS
+__BEGIN_DECLS 
 
-typedef struct
-{
+/* 
+   
+ */
+
+typedef struct {
+	int base;					       /* 0 or 1 */
 	int n;
 	int na;
-	int nnz;
 	int *ia;
 	int *ja;
 	double *a;
-}
-	GMRFLib_csr_tp;
+} GMRFLib_csr_tp;
 
-int GMRFLib_Q2csr(GMRFLib_csr_tp **csr, GMRFLib_graph_tp *graph, GMRFLib_Qfunc_tp *Qfunc, void *Qfunc_arg);
-int GMRFLib_print_csr(FILE *fp, GMRFLib_csr_tp *csr);
-int GMRFLib_csr2Q(GMRFLib_tabulate_Qfunc_tp **Qtab, GMRFLib_graph_tp **graph, GMRFLib_csr_tp *csr);
-int GMRFLib_duplicate_csr(GMRFLib_csr_tp ** csr_to, GMRFLib_csr_tp *csr_from);
-int GMRFLib_free_csr(GMRFLib_csr_tp *csr);
+typedef enum {
+	GMRFLib_PARDISO_FLAG_REORDER=1,
+	GMRFLib_PARDISO_FLAG_SYMFACT,
+	GMRFLib_PARDISO_FLAG_CHOL,
+	GMRFLib_PARDISO_FLAG_INV,
+	GMRFLib_PARDISO_FLAG_SOLVE
+} GMRFLib_pardiso_flag_tp;
 
 
-int pardiso_test();
+typedef struct {
+	void *pt[64];
+	int iparm[64];
+	double dparm[64];
+
+	int init_done;
+	
+	char *var;					       /* Auxiliary variables. */
+	double dummy;
+	int err_code;
+	int idummy;
+	int maxfct;
+	int mnum;
+	int msglvl;
+	int mtype;
+	int nrhs;
+	int num_procs;					       /* Number of processors. */
+	int phase;
+	int solver;
+	int *my_perm;
+	int *perm;
+	int L_nnz;
+	double log_det_Q;
+	GMRFLib_csr_tp *L;
+	GMRFLib_csr_tp *Q;
+	GMRFLib_csr_tp *Qinv;
+} GMRFLib_pardiso_store_tp;
+
+#define STDOUT_TO_DEV_NULL_START(_silent)			\
+	int XX_stdout_dupfd;					\
+	int XX_silent = _silent;				\
+	FILE *XX_temp_out;					\
+	if (XX_silent) {					\
+		XX_stdout_dupfd = dup(1);			\
+		XX_temp_out = fopen("/dev/null", "w");		\
+		dup2(fileno(XX_temp_out), 1);			\
+	}
+	
+
+#define STDOUT_TO_DEV_NULL_END				\
+	if (XX_silent) {				\
+		fflush(stdout);				\
+		fclose(XX_temp_out);			\
+		dup2(XX_stdout_dupfd, 1);		\
+		close(XX_stdout_dupfd);			\
+	}	
+
+
+int GMRFLib_Q2csr(GMRFLib_csr_tp ** csr, GMRFLib_graph_tp * graph, GMRFLib_Qfunc_tp * Qfunc, void *Qfunc_arg);
+int GMRFLib_print_csr(FILE * fp, GMRFLib_csr_tp * csr);
+int GMRFLib_csr2Q(GMRFLib_tabulate_Qfunc_tp ** Qtab, GMRFLib_graph_tp ** graph, GMRFLib_csr_tp * csr);
+int GMRFLib_duplicate_csr(GMRFLib_csr_tp ** csr_to, GMRFLib_csr_tp * csr_from);
+int GMRFLib_free_csr(GMRFLib_csr_tp ** csr);
+int GMRFLib_pardiso_setparam(GMRFLib_pardiso_flag_tp flag, GMRFLib_pardiso_store_tp * store);
+int GMRFLib_pardiso_check_install(int quiet);
+int GMRFLib_csr_base(int base, GMRFLib_csr_tp *M);
+int GMRFLib_csr_convert(GMRFLib_csr_tp *M);
+int GMRFLib_Q2csr_check(GMRFLib_csr_tp *M);
+int GMRFLib_pardiso_init(GMRFLib_pardiso_store_tp *store);
+int GMRFLib_pardiso_symfact(GMRFLib_pardiso_store_tp *store);
+int GMRFLib_pardiso_chol(GMRFLib_pardiso_store_tp *store, GMRFLib_graph_tp *graph,
+			 GMRFLib_Qfunc_tp *Qfunc, void *Qfunc_arg);
+
+double GMRFLib_pardiso_Qfunc_default(int i, int j, void *arg);
+int GMRFLib_pardiso_reorder(GMRFLib_pardiso_store_tp *store, GMRFLib_graph_tp *graph, int *reordering);
+
+
+void pardisoinit(void *, int *, int *, int *, double *, int *);
+void pardiso(void *, int *, int *, int *, int *, int *, double *, int *, int *, int *, int *, int *, int *, double *, double *, int *, double *);
+void pardiso_chkmatrix(int *, int *, double *, int *, int *, int *);
+void pardiso_chkvec(int *, int *, double *, int *);
+void pardiso_printstats(int *, int *, double *, int *, int *, int *, double *, int *);
+void pardiso_get_factor_csc(void **, double *, int *, int *, double *, int *, int *, int *, int);
+void pardiso_get_inverse_factor_csc(void **, double *, int *, int *, int *, int);
+
 
 __END_DECLS
 #endif
