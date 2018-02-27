a883dcec2abec93bfc267c56de840b43e4efcfb5
Håvard Rue <haavard.rue@kaust.edu.sa>
2018-02-27 17:25:08 +0300

    More work. It is working, but not as efficient yet. Still some issues, but basics are fine it seems.

--- gmrflib/GMRFLib.h
+++ gmrflib/GMRFLib.h
@@ -104,6 +104,7 @@ __BEGIN_DECLS
 #include "GMRFLib/seasonal.h"
 #include "GMRFLib/rw.h"
 #include "GMRFLib/tabulate-Qfunc.h"
+#include "GMRFLib/smtp-pardiso.h"
 #include "GMRFLib/sparse-interface.h"
 #include "GMRFLib/problem-setup.h"
 #include "GMRFLib/openmp.h"
@@ -120,7 +121,6 @@ __BEGIN_DECLS
 #include "GMRFLib/wa.h"
 #include "GMRFLib/smtp-band.h"
 #include "GMRFLib/smtp-taucs.h"
-#include "GMRFLib/smtp-pardiso.h"
 #include "GMRFLib/bitmap.h"				       /* needs both graph and problem and sparse */
 #include "GMRFLib/geo.h"
 #include "GMRFLib/sphere.h"
