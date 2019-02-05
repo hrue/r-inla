a303a47db7adacf3442c81778ee5f49171fa2325
Håvard Rue <haavard.rue@kaust.edu.sa>
2019-02-05 10:26:48 +0300

    Ran indent

--- inlaprog/src/inla.h
+++ inlaprog/src/inla.h
@@ -484,7 +484,7 @@ typedef enum {
 	L_QLOGLOGISTICSURV,
 	L_POM,
 	L_NBINOMIAL2,
-	L_GAMMASURV, 
+	L_GAMMASURV,
 	F_RW2D = 1000,					       /* f-models */
 	F_BESAG,
 	F_BESAG2,					       /* the [a*x, x/a] model */
