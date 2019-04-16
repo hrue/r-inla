55c458b7df2c3f2e2da6d6cf6b79fddc09635ed7
Håvard Rue <haavard.rue@kaust.edu.sa>
2019-04-16 12:14:04 +0300

    Added generic0 to the test-case no 1

--- sandbox/test-cases/1/runme.R
+++ sandbox/test-cases/1/runme.R
@@ -1,5 +1,8 @@
+library(INLA)
+inla.pardiso.check()
+
 library(mvtnorm)
-n = 200
+n = 1000
 A = matrix(rnorm(n^2), n, n)
 Q = A %*% t(A)
 y = c(rmvnorm(1, sigma = solve(Q)))
@@ -24,7 +27,9 @@ rg.test = function (cmd = c("graph", "Q", "mu", "initial",
         return(numeric(0))
     }
     log.norm.const = function() {
-        return(numeric(0))
+        ## ignore the |Q.matrix| like model=generic0
+        prec = interpret.theta()$prec
+        return (dim(Q.matrix)[1]/2.0 * (log(prec) - log(2*pi)))
     }
     log.prior = function() {
         prec = interpret.theta()$prec
@@ -47,57 +52,62 @@ rg.test = function (cmd = c("graph", "Q", "mu", "initial",
 }
 
 rg = inla.rgeneric.define(rg.test,  Q.matrix = Q, debug=FALSE)
+models = c('y ~ -1 + f(idx, model = rg)',
+           'y ~ -1 + f(idx, model="generic0", Cmatrix=Q, hyper = list(prec = list(prior = "loggamma", param = c(1, 1))))')
+           
+##
+for(model in models) {
+    formula = as.formula(model)
+    
+    inla.setOption(mkl=FALSE)
+    r.pp = inla(formula, 
+                data = data.frame(y, idx = 1:n),
+                control.compute = list(smtp = "pardiso", openmp.strategy = "pardiso.parallel"))
 
-inla.pardiso.check()
-inla.setOption(num.threads = 4)
+    inla.setOption(mkl=TRUE)
+    r.ppm = inla(formula, 
+                 data = data.frame(y, idx = 1:n),
+                 control.compute = list(smtp = "pardiso", openmp.strategy = "pardiso.parallel"))
 
-inla.setOption(mkl=FALSE)
-r.pp = inla(y ~ -1 + f(idx, model = rg),
-           data = data.frame(y, idx = 1:n),
-           control.compute = list(smtp = "pardiso", openmp.strategy = "pardiso.parallel"))
+    inla.setOption(mkl=FALSE)
+    r.p = inla(formula, 
+               data = data.frame(y, idx = 1:n),
+               control.compute = list(smtp = "pardiso", openmp.strategy = "pardiso.serial"))
 
-inla.setOption(mkl=TRUE)
-r.ppm = inla(y ~ -1 + f(idx, model = rg),
-           data = data.frame(y, idx = 1:n),
-           control.compute = list(smtp = "pardiso", openmp.strategy = "pardiso.parallel"))
+    inla.setOption(mkl=TRUE)
+    r.pm = inla(formula, 
+                data = data.frame(y, idx = 1:n),
+                control.inla = list(int.strategy = "eb"), 
+                control.compute = list(smtp = "pardiso", openmp.strategy = "pardiso.serial"))
 
-inla.setOption(mkl=FALSE)
-r.p = inla(y ~ -1 + f(idx, model = rg),
-           data = data.frame(y, idx = 1:n),
-           control.compute = list(smtp = "pardiso", openmp.strategy = "pardiso.serial"))
+    inla.setOption(mkl=FALSE)
+    r.t = inla(formula, 
+               data = data.frame(y, idx = 1:n),
+               control.compute = list(smtp = "taucs", openmp.strategy = "huge"))
 
-inla.setOption(mkl=TRUE)
-r.pm = inla(y ~ -1 + f(idx, model = rg),
-           data = data.frame(y, idx = 1:n),
-           control.inla = list(int.strategy = "eb"), 
-           control.compute = list(smtp = "pardiso", openmp.strategy = "pardiso.serial"))
+    inla.setOption(mkl=TRUE)
+    r.tm = inla(formula, 
+                data = data.frame(y, idx = 1:n),
+                control.compute = list(smtp = "taucs", openmp.strategy = "huge"))
 
-inla.setOption(mkl=FALSE)
-r.t = inla(y ~ -1 + f(idx, model = rg),
-         data = data.frame(y, idx = 1:n),
-         control.compute = list(smtp = "taucs", openmp.strategy = "huge"))
+    inla.setOption(mkl=FALSE)
+    r.b = inla(formula, 
+               data = data.frame(y, idx = 1:n),
+               control.compute = list(smtp = "band", openmp.strategy = "huge"))
 
-inla.setOption(mkl=TRUE)
-r.tm = inla(y ~ -1 + f(idx, model = rg),
-         data = data.frame(y, idx = 1:n),
-         control.compute = list(smtp = "taucs", openmp.strategy = "huge"))
+    inla.setOption(mkl=TRUE)
+    r.bm = inla(formula, 
+                data = data.frame(y, idx = 1:n),
+                control.compute = list(smtp = "band", openmp.strategy = "huge"))
 
-inla.setOption(mkl=FALSE)
-r.b = inla(y ~ -1 + f(idx, model = rg),
-         data = data.frame(y, idx = 1:n),
-         control.compute = list(smtp = "band", openmp.strategy = "huge"))
-
-inla.setOption(mkl=TRUE)
-r.bm = inla(y ~ -1 + f(idx, model = rg),
-         data = data.frame(y, idx = 1:n),
-         control.compute = list(smtp = "band", openmp.strategy = "huge"))
-
-cat("\n\n\n")
-print(rbind(PARDISO.PARALLEL=c(round(r.pp$cpu, digits = 2), "#func"=r.pp$misc$nfunc), 
-            PARDISO.PARALLEL.MKL=c(round(r.ppm$cpu, digits = 2), "#func"=r.ppm$misc$nfunc), 
-            PARDISO.SERIAL=c(round(r.p$cpu, digits = 2), "#func"=r.p$misc$nfunc), 
-            PARDISO.SERIAL.MKL=c(round(r.pm$cpu, digits = 2), "#func"=r.pm$misc$nfunc), 
-            TAUCS=c(round(r.t$cpu, digits = 2), "#func" = r.t$misc$nfunc), 
-            TAUCS.MKL=c(round(r.tm$cpu, digits = 2), "#func" = r.tm$misc$nfunc), 
-            BAND=c(round(r.b$cpu, digits = 2), "#func" = r.b$misc$nfunc), 
-            BAND.MKL=c(round(r.bm$cpu, digits = 2), "#func" = r.bm$misc$nfunc)))
+    cat("\n\n\n")
+    print(paste0("Using model = [", model, "]"))
+    print(rbind(PARDISO.PARALLEL=c(round(r.pp$cpu, digits = 2), "#func"=r.pp$misc$nfunc), 
+                PARDISO.PARALLEL.MKL=c(round(r.ppm$cpu, digits = 2), "#func"=r.ppm$misc$nfunc), 
+                PARDISO.SERIAL=c(round(r.p$cpu, digits = 2), "#func"=r.p$misc$nfunc), 
+                PARDISO.SERIAL.MKL=c(round(r.pm$cpu, digits = 2), "#func"=r.pm$misc$nfunc), 
+                TAUCS=c(round(r.t$cpu, digits = 2), "#func" = r.t$misc$nfunc), 
+                TAUCS.MKL=c(round(r.tm$cpu, digits = 2), "#func" = r.tm$misc$nfunc), 
+                BAND=c(round(r.b$cpu, digits = 2), "#func" = r.b$misc$nfunc), 
+                BAND.MKL=c(round(r.bm$cpu, digits = 2), "#func" = r.bm$misc$nfunc)))
+}
