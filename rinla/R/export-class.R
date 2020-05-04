77944d362058d680b5c6d29502fcc92eb534cd53
Håvard Rue <haavard.rue@kaust.edu.sa>
2020-05-04 17:47:45 +0300

    Added CC to inla.my.update()

--- rinla/R/export-class.R
+++ rinla/R/export-class.R
@@ -1,6 +1,6 @@
 #' inla
 #'
-#' the inla class is defined in the INLA package
+#' The inla class is defined in the INLA package
 #' @name inla-class
 #' @seealso \link[INLA]{inla}
 #' @importFrom methods setOldClass
