f560dd165f7f1354bce273473323e77c7807fc40
Haavard Rue <haavard.rue@kaust.edu.sa>
2020-06-30 22:17:15 +0300

    More fixes
    I didn't got it right in the first place...

--- inlaprog/src/eval.h
+++ inlaprog/src/eval.h
@@ -1,7 +1,7 @@
 
 /* eval.h
  * 
- * Copyright (C) 2011-2020 Havard Rue
+ * Copyright (C) 2011 Havard Rue
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
