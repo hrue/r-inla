1ba38c48c93d7c6d25b93a2b3296c32755e072bd
Haavard Rue <haavard.rue@kaust.edu.sa>
2020-12-13 16:51:24 +0300

    Fix compiler warnings

--- inlaprog/src/inla.c
+++ inlaprog/src/inla.c
@@ -34119,7 +34119,9 @@ int main(int argc, char **argv)
 #define _BUGS_intern(fp) fprintf(fp, "Report bugs to <help@r-inla.org>\n")
 #define _BUGS _BUGS_intern(stdout)
 	int i, verbose = 0, silent = 0, opt, report = 0, arg, ntt[2] = { 0, 0 }, err;
+#if !defined(WINDOWS)
 	int enable_core_file = 0;		       /* allow for core files */
+#endif
 	int blas_num_threads_set = 0;
 	int blas_num_threads_default = 1;
 	char *program = argv[0];
@@ -34376,7 +34378,9 @@ int main(int argc, char **argv)
 			break;
 
 		case 'c':
+#if !defined(WINDOWS)
 			enable_core_file = 1;		       /* allow for core files */
+#endif
 			break;
 
 		case 'p':
