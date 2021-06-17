c9cac3f060c0d96be725ed6509d2cd0a9255b5e5
Haavard Rue <haavard.rue@kaust.edu.sa>
2021-06-17 15:21:24 +0300

    fix

--- gmrflib/smtp-pardiso.c
+++ gmrflib/smtp-pardiso.c
@@ -452,7 +452,7 @@ int GMRFLib_pardiso_init(GMRFLib_pardiso_store_tp ** store)
 	s->iparm_default[4] = 0;			       /* use internal reordering */
 	s->iparm_default[7] = 0;			       /* maximum number of refinement steps */
 	FIXME1("DISABLE iparm[7]");
-	s->iparm_default[7] = 4;			       /* maximum number of refinement steps */
+	//s->iparm_default[7] = 4;			       /* maximum number of refinement steps */
 	s->iparm_default[10] = 0;			       /* These are the default, but... */
 	s->iparm_default[12] = 0;			       /* I need these for the divided LDL^Tx=b solver to work */
 	s->iparm_default[20] = 0;			       /* Diagonal pivoting, and... */
