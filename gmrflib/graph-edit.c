abb85fd685b02dcc32f7d2d125428f7af891b752
Haavard Rue <haavard.rue@kaust.edu.sa>
2022-01-06 18:52:43 +0100

    Copyright, update 2021 to 2022

--- gmrflib/graph-edit.c
+++ gmrflib/graph-edit.c
@@ -1,7 +1,7 @@
 
 /* graph-edit.c
  * 
- * Copyright (C) 2006-2021 Havard Rue
+ * Copyright (C) 2006-2022 Havard Rue
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- inlaprog/src/bessel.c
+++ inlaprog/src/bessel.c
@@ -1,7 +1,7 @@
 
 /* bessel.c
  * 
- * Copyright (C) 2012-2020 Havard Rue
+ * Copyright (C) 2012-2022 Havard Rue
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
