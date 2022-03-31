a83b8fd5ca683a9bccac587b2b4a84ed8b307dc1
Haavard Rue <haavard.rue@kaust.edu.sa>
2022-03-31 14:29:48 +0300

    Save before cleaning up

--- gmrflib/graph.c
+++ gmrflib/graph.c
@@ -1282,7 +1282,7 @@ int GMRFLib_Qx2(double *result, double *x, GMRFLib_graph_tp * graph, GMRFLib_Qfu
 	if (debug)
 		P(max_t);
 
-	if (ISNAN(res)) {
+	if (1 || ISNAN(res)) {
 		if (run_parallel) {
 			if (debug)
 				FIXME("Qx2: run parallel");
@@ -1405,29 +1405,35 @@ int GMRFLib_QM(gsl_matrix * result, gsl_matrix * x, GMRFLib_graph_tp * graph, GM
 			}
 		} else {
 			double *p1, *p2, *p3, *p4, qij;
+			int one = 1;
 			for (int i = 0; i < graph->n; i++) {
 				qij = Qfunc(i, i, NULL, Qfunc_arg);
 				p1 = gsl_matrix_ptr(result, i, 0);
 				p3 = gsl_matrix_ptr(x, i, 0);
-#pragma GCC ivdep
-#pragma GCC unroll 8
-				for (int k = 0; k < ncol; k++) {
-					p1[k] += p3[k] * qij;
+				if (0) {
+					for (int k = 0; k < ncol; k++) {
+						p1[k] += p3[k] * qij;
+					}
+				} else {
+					daxpy_(&ncol, &qij, p3, &one, p1, &one);
 				}
 				for (int jj = 0; jj < graph->lnnbs[i]; jj++) {
 					int j = graph->lnbs[i][jj];
 					qij = Qfunc(i, j, NULL, Qfunc_arg);
 					p2 = gsl_matrix_ptr(result, j, 0);
 					p4 = gsl_matrix_ptr(x, j, 0);
-#pragma GCC ivdep
-#pragma GCC unroll 8
-					for (int k = 0; k < ncol; k++) {
-						p1[k] += qij * p4[k];
-						p2[k] += qij * p3[k];
+					if (0) {
+						for (int k = 0; k < ncol; k++) {
+							p1[k] += qij * p4[k];
+							p2[k] += qij * p3[k];
+						}
+					} else {
+						int one = 1;
+						daxpy_(&ncol, &qij, p4, &one, p1, &one);
+						daxpy_(&ncol, &qij, p3, &one, p2, &one);
 					}
 				}
 			}
-
 		}
 	} else {
 		if (0 &&				       // TURN OFF THIS AS THE SERIAL IS JUST SO MUCH BETTER for the moment
@@ -1454,25 +1460,33 @@ int GMRFLib_QM(gsl_matrix * result, gsl_matrix * x, GMRFLib_graph_tp * graph, GM
 			// better one, as the 'k' loop is sequential with inc=1
 			double *p1, *p2, *p3, *p4;
 			for (int i = 0; i < graph->n; i++) {
+				int one = 1;
 				Qfunc(i, -1, values, Qfunc_arg);
 				p1 = gsl_matrix_ptr(result, i, 0);
 				p3 = gsl_matrix_ptr(x, i, 0);
-#pragma GCC ivdep
-#pragma GCC unroll 8
-				for (int k = 0; k < ncol; k++) {
-					p1[k] += p3[k] * values[0];
+				if (0) {
+					for (int k = 0; k < ncol; k++) {
+						p1[k] += p3[k] * values[0];
+					}
+				} else {
+					daxpy_(&ncol, &(values[0]), p3, &one, p1, &one);
 				}
 				for (int jj = 0; jj < graph->lnnbs[i]; jj++) {
 					int j = graph->lnbs[i][jj];
 					double qij = values[1 + jj];
 					p2 = gsl_matrix_ptr(result, j, 0);
 					p4 = gsl_matrix_ptr(x, j, 0);
-#pragma GCC ivdep
-#pragma GCC unroll 8
-					for (int k = 0; k < ncol; k++) {
-						p1[k] += qij * p4[k];
-						p2[k] += qij * p3[k];
+					if (0) {
+						for (int k = 0; k < ncol; k++) {
+							p1[k] += qij * p4[k];
+							p2[k] += qij * p3[k];
+						}
+					} else {
+						daxpy_(&ncol, &qij, p4, &one, p1, &one);
+						daxpy_(&ncol, &qij, p3, &one, p2, &one);
 					}
+					
+
 				}
 			}
 
