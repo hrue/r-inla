d2e9ac9d3034d1e1a6250c6e5d16f02c30db70e7
Haavard Rue <haavard.rue@kaust.edu.sa>
2022-04-25 10:35:28 +0300

    Added myinit.R

---
+++ rinla/vignettes/myinit.R
@@ -0,0 +1,8 @@
+if (FALSE) {
+    INLA:::inla.my.update()
+    inla.setOption(inla.call="inla.mkl.work")
+    inla.setOption(num.threads = "2:4")
+    inla.setOption(smtp = "pardiso")
+    inla.setOption(pardiso.license="~/sys/licenses/pardiso.lic")
+}
+##inla.setOption(inla.mode = "experimental")
