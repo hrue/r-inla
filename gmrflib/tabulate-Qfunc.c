099017ef8195a346588858ea6e46f9b1d326aec3
Haavard Rue <haavard.rue@kaust.edu.sa>
2022-07-18 09:02:46 +0200

    Add l1_cacheline to guess

--- gmrflib/tabulate-Qfunc.c
+++ gmrflib/tabulate-Qfunc.c
@@ -109,21 +109,20 @@ static unsigned char ADD_MULTIPLE_ENTRIES = 0;		       /* 1: allow, 0: no allow 
 
 double GMRFLib_tabulate_Qfunction(int thread_id, int node, int nnode, double *values, void *arg)
 {
-	static int **gguess = NULL;
+	static int *gguess = NULL;
+	int l1_cacheline = 8L;
+
 	if (!gguess) {
 #pragma omp critical (Name_ed019f1aad7e7d2a67d1fbc75e1e79976657700b)
 		{
 			if (!gguess) {
-				gguess = Calloc(GMRFLib_CACHE_LEN, int *);
+				gguess = Calloc(GMRFLib_CACHE_LEN * (2L + l1_cacheline), int);
 			}
 		}
 	}
 	int idx = -1;
 	GMRFLib_CACHE_SET_ID(idx);
-	if (!gguess[idx]) {
-		gguess[idx] = Calloc(2, int);
-	}
-	int *guess = gguess[idx];
+	int *guess = gguess + (2L + l1_cacheline) * idx;
 
 	double val = 0.0;
 	TAB_FUNC_CORE(1);
@@ -133,17 +132,18 @@ double GMRFLib_tabulate_Qfunction(int thread_id, int node, int nnode, double *va
 double GMRFLib_tabulate_Qfunction_std(int thread_id, int node, int nnode, double *values, void *arg)
 {
 	static int *gguess = NULL;
+	int l1_cacheline = 8L;
 	if (!gguess) {
 #pragma omp critical (Name_b56890f7ff5cd3567a3aff6f33a6c54d2abc91dc)
 		{
 			if (!gguess) {
-				gguess = Calloc(2L * GMRFLib_CACHE_LEN, int);
+				gguess = Calloc((2L + l1_cacheline) * GMRFLib_CACHE_LEN, int);
 			}
 		}
 	}
 	int idx = -1;
 	GMRFLib_CACHE_SET_ID(idx);
-	int *guess = gguess + 2 * idx;
+	int *guess = gguess + (2L + l1_cacheline) * idx;
 
 	double val = 0.0;
 	TAB_FUNC_CORE(0);
