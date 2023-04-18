dc23f1853dc1eebf89a24244f5f85d569a49fcf4
Haavard Rue <haavard.rue@kaust.edu.sa>
2023-04-18 11:26:24 +0200

    Fixed comma-error

--- rinla/R/barrier.R
+++ rinla/R/barrier.R
@@ -63,7 +63,7 @@
             range.fraction = range.fraction))
     } else {
 	warning(paste(
-	"Please install the `INLAspacetime` package\n"
+	"Please install the `INLAspacetime` package\n", 
         "which contains an implementation that runs faster!"))
     }
 
@@ -472,7 +472,7 @@
 	    barrier.triangles = Omega[[2L]])
     } else {
         warning(paste(
-        "Please install the `INLAspacetime` package\n"
+        "Please install the `INLAspacetime` package\n", 
         "which contains an implementation that runs faster!"))
         fem <- list()
         fem$I <- dt.fem.identity(mesh)
