d5e55eba0111c58e0d11c643d82851db6b952e9b
Haavard Rue <haavard.rue@kaust.edu.sa>
2023-06-14 19:13:50 +0300

    WIP

--- inlaprog/src/spde2.c
+++ inlaprog/src/spde2.c
@@ -618,13 +618,11 @@ double inla_spde2_Qfunction_ref(int thread_id, int ii, int jj, double *UNUSED(va
 double inla_spde2_Qfunction(int thread_id, int ii, int jj, double *values, void *arg)
 {
 	if (jj < 0) {
-
 		inla_spde2_tp *model = (inla_spde2_tp *) arg;
 		int nc = model->B[0]->ncol;
 		double *v = NULL;
 		double *vals = (double *) *map_ivp_ptr(&(model->vmatrix->vmat[ii]), ii);
 		double d_i[6];
-		double *d_j = d_i + 3;
 
 #pragma omp simd
 		for (int k = 0; k < 3; k++) {
@@ -665,52 +663,77 @@ double inla_spde2_Qfunction(int thread_id, int ii, int jj, double *values, void 
 		v = vals + 3 * nc;
 		values[0] = Sqr(d_i[0]) * (Sqr(d_i[1]) * v[0] + d_i[2] * d_i[1] * (v[1] + v[2]) + v[3]);
 
-		for (int j = 0; j < model->graph->lnnbs[ii]; j++) {
+		int nb = model->graph->lnnbs[ii];
+
+		if (!nb) {
+			return 0.0;
+		}
 		
+		int m = 6 * nc + 4;
+		double VALS[nb][m];
+		double D_J[nb][3];
+		
+		for (int j = 0; j < model->graph->lnnbs[ii]; j++) {
 			jj = model->graph->lnbs[ii][j];
 			vals = (double *) *map_ivp_ptr(&(model->vmatrix->vmat[ii]), jj);
-
+			Memcpy(VALS[j], vals, m * sizeof(double));
+		}
+		
+		for (int k = 3; k < 6; k++) {
 #pragma omp simd
-			for (int k = 3; k < 6; k++) {
-				d_i[k] = vals[k * nc];
+			for(int j = 0; j < nb; j++) {
+				D_J[j][k-3] = VALS[j][k * nc];
 			}
-
-			for (int k = 1; k < nc; k++) {
-				double th = model->theta[k - 1][thread_id][0];
-				v = vals + k;
-#pragma omp simd
-				for (int kk = 3; kk < 6; kk++) {
-					d_i[kk] += v[kk * nc] * th;
-				}
-			}
-
-#pragma omp simd
-			for (int k = 0; k < 2; k++) {
-				d_j[k] = exp(d_j[k]);
-			}
-
-			if (model->transform != SPDE2_TRANSFORM_IDENTITY) {
-				switch (model->transform) {
-				case SPDE2_TRANSFORM_IDENTITY:
-					break;
-
-				case SPDE2_TRANSFORM_LOG:
-					d_j[2] = 2.0 * exp(d_j[2]) - 1.0;
-					break;
-
-				case SPDE2_TRANSFORM_LOGIT:
-					d_j[2] = cos(M_PI * map_probability(d_j[2], MAP_FORWARD, NULL));
-					break;
-
-				default:
-					assert(0 == 1);
-				}
-			}
-
-			v = vals + 6 * nc;
-			values[1 + j] = d_i[0] * d_j[0] * (d_i[1] * d_j[1] * v[0] + d_i[2] * d_i[1] * v[1] + d_j[1] * d_j[2] * v[2] + v[3]);
 		}
 
+		for (int k = 1; k < nc; k++) {
+			double th = model->theta[k - 1][thread_id][0];
+
+			for (int kk = 3; kk < 6; kk++) {
+#pragma omp simd
+				for(int j = 0; j < nb; j++) {
+					v = VALS[j] + k;
+					D_J[j][kk-3] += v[kk * nc] * th;
+				}
+			}
+		}
+
+		for (int k = 0; k < 2; k++) {
+#pragma omp simd
+			for(int j = 0; j < nb; j++) {
+				D_J[j][k] = exp(D_J[j][k]);
+			}
+		}
+
+		if (model->transform != SPDE2_TRANSFORM_IDENTITY) {
+			switch (model->transform) {
+			case SPDE2_TRANSFORM_IDENTITY:
+				break;
+
+			case SPDE2_TRANSFORM_LOG:
+#pragma omp simd
+				for(int j = 0; j < nb; j++) {
+					D_J[j][2] = 2.0 * exp(D_J[j][2]) - 1.0;
+				}
+				break;
+
+			case SPDE2_TRANSFORM_LOGIT:
+#pragma omp simd
+				for(int j = 0; j < nb; j++) {
+					D_J[j][2] = cos(M_PI * map_probability(D_J[j][2], MAP_FORWARD, NULL));
+				}
+				break;
+
+			default:
+				assert(0 == 1);
+			}
+		}
+
+		for(int j = 0; j < nb; j++) {
+			v = VALS[j] + 6 * nc;
+			values[1 + j] = d_i[0] * D_J[j][0] * (d_i[1] * D_J[j][1] * v[0] + d_i[2] * d_i[1] * v[1] + D_J[j][1] * D_J[j][2] * v[2] + v[3]);
+		}
+		
 		return 0.0;
 	}
 
