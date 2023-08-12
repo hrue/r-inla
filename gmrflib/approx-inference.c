e3f01c452902b01d4e7aa46594eb90c6983c82e0
Haavard Rue <haavard.rue@kaust.edu.sa>
2023-08-12 11:05:01 +0200

    Smart opt: check num_threads
    Only switch to forward differences if there are not enough threads in the outer layer.

--- gmrflib/approx-inference.c
+++ gmrflib/approx-inference.c
@@ -1297,9 +1297,15 @@ int GMRFLib_ai_INLA_experimental(GMRFLib_density_tp ***density,
 				int retval;
 				int fd_save = ai_par->gradient_forward_finite_difference;
 				if (ai_par->optimise_smart) {
-					ai_par->gradient_forward_finite_difference = GMRFLib_TRUE;
-					if (ai_par->fp_log) {
-						fprintf(ai_par->fp_log, "Smart optimise part I: estimate gradient using forward differences\n");
+					if (GMRFLib_openmp->max_threads_outer < nhyper * 2) {
+						ai_par->gradient_forward_finite_difference = GMRFLib_TRUE;
+						if (ai_par->fp_log) {
+							fprintf(ai_par->fp_log, "Smart optimise part I: estimate gradient using forward differences\n");
+						}
+					} else {
+						if (ai_par->fp_log) {
+							fprintf(ai_par->fp_log, "Smart optimise part I: estimate gradient using central differences\n");
+						}
 					}
 				}
 
