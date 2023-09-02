ac85dc2356a46634a81b98ebeb60c549f30de22d
Haavard Rue <haavard.rue@kaust.edu.sa>
2023-09-02 22:33:29 +0300

    Add error check for zero-rows in the predictor A-matrix

--- rinla/R/sections.R
+++ rinla/R/sections.R
@@ -1188,9 +1188,13 @@
         }
         A <- inla.sparse.check(A, must.be.squared = FALSE)
 
-        ## check dimensions
+        ## check dimensions and if some rows are just zero's.
         stopifnot(dim(A)[1] == m)
         stopifnot(dim(A)[2] == n)
+        if (any(rowSums(abs(A)) == 0)) {
+            nz <- sum(rowSums(abs(A)) == 0)
+            stop(paste0(nz, " rows in 'control.predictor=list(A=A)'-matrix, contain only zeros. This is not allowed."))
+        }
 
         file.A <- inla.tempfile(tmpdir = data.dir)
         inla.write.fmesher.file(A, filename = file.A)
