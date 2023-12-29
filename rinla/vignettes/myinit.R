6f393da8d8ef1b94389535472f9f2e94e3f30ff5
Haavard Rue <haavard.rue@kaust.edu.sa>
2023-12-29 18:06:56 +0300

    Update vignettes

--- rinla/vignettes/myinit.R
+++ rinla/vignettes/myinit.R
@@ -1,7 +1,7 @@
 if (FALSE) {
     INLA:::inla.my.update()
     inla.setOption(inla.call="inla.mkl.work")
-    inla.setOption(num.threads = "2:1")
+    inla.setOption(num.threads = "4:1")
     ##inla.setOption(smtp = "pardiso")
     ##inla.setOption(pardiso.license="~/sys/licenses/pardiso.lic")
 }
