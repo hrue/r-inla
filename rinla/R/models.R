359e191ce2788a66ed85428b5fd16d1593c25003
Haavard Rue <haavard.rue@kaust.edu.sa>
2024-05-27 20:23:43 +0300

    Fixed an regression with `iidkd`
    Default prior would fail unless  its parameters were set.

--- rinla/R/models.R
+++ rinla/R/models.R
@@ -4018,7 +4018,7 @@
                             initial = inla.special.number(),
                             fixed = FALSE,
                             prior = "wishartkd",
-                            param = c(24, rep(inla.special.number(), (24*25)/2)), 
+                            param = c(30, rep(inla.special.number(), (24*25)/2)), 
                             to.theta = function(x) x,
                             from.theta = function(x) x
                         ),
@@ -14766,7 +14766,7 @@
 
                 wishartkd =  list(
                     doc = "Wishart prior",
-                    nparameters = 324L,
+                    nparameters = 301L,
                     pdf = NULL
                 ), 
 
