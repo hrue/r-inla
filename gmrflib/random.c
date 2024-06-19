e36e8154414976b8244f00e9c4c293467f08888b
Haavard Rue <haavard.rue@kaust.edu.sa>
2024-06-19 16:21:20 +0900

    Minor code edits (analyzer)

--- gmrflib/random.c
+++ gmrflib/random.c
@@ -75,7 +75,7 @@ int GMRFLib_rng_set_default_seed(void)
 #else							       /* !defined(WINDOWS) */
 		{
 			int fd = open("/dev/urandom", O_RDONLY);
-			if (fd > 0) {
+			if (fd >= 0) {
 				ssize_t nb = read(fd, (void *) &seed, len);
 				if (nb != (ssize_t) len) {
 					seed = seed_default;
