5fe3651caea2540e3daabda81a9a50d4b248edb1
Haavard Rue <haavard.rue@kaust.edu.sa>
2024-09-07 10:11:24 +0100

    Add workaround

--- gmrflib/smtp-taucs.h
+++ gmrflib/smtp-taucs.h
@@ -53,6 +53,12 @@
 __BEGIN_DECLS
 //
 //
+
+#if defined(INLA_WITH_PARDISO_WORKAROUND)
+int METIS51PARDISO_NodeND(int *i, int *j, int *k, int *l, int *m, int *n, int *o);
+#endif
+
+
     GMRFLib_taucs_cache_tp * GMRFLib_taucs_cache_duplicate(GMRFLib_taucs_cache_tp * cache);
 void GMRFLib_taucs_cache_free(GMRFLib_taucs_cache_tp * cache);
 
