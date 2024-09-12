a916f4032c7d91930a1a4272c052852b9d4b709d
Haavard Rue <haavard.rue@kaust.edu.sa>
2024-09-12 08:02:04 +0200

    Improve code
    More changes
    
    Cleanup

--- inlaprog/src/inla.h
+++ inlaprog/src/inla.h
@@ -755,8 +755,9 @@ typedef struct {
 	int occ_nbeta;
 	int occ_ny_max;
 	int *occ_yzero;
-	double *occ_y;
-	double *occ_x;
+	int *occ_ny; 
+	int **occ_y;
+	double **occ_x;
 	double ***occ_beta;
 
 	/*
