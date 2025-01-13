hrue/r-inla

commit cc7433a8abefebea8012c521f986d5923e97a321

Cleanup some lp_scale code

inlaprog/src/inla-likelihood.c
@@ -627,15 +627,22 @@ int inla_read_data_likelihood(inla_tp *mb, dictionary *UNUSED(ini), int UNUSED(s
 		assert(nw == mb->predictor_ndata);
 	}
 
-	double *lp_scale = NULL;
+	double *lp_scale_d = NULL;			       /* I need a tmp one to be double */
 	int n_lp_scale = 0;
-	inla_read_data_all(&lp_scale, &n_lp_scale, ds->lp_scale_file.name, NULL);
+	inla_read_data_all(&lp_scale_d, &n_lp_scale, ds->lp_scale_file.name, NULL);
 	if (n_lp_scale) {
 		assert(n_lp_scale == mb->predictor_ndata);
 	}
-	for (i = 0; i < n_lp_scale; i++) {
-		lp_scale[i] = (int) (lp_scale[i] - 1.0);
+	int *lp_scale = Calloc(mb->predictor_ndata, int);
+#pragma omp simd
+	for (int i3 = 0; i3 < n_lp_scale; i3++) {
+		if (ISNAN(lp_scale_d[i3])) {
+			lp_scale[i3] = -1;
+		} else {
+			lp_scale[i3] = (int) lp_scale_d[i3] - 1;
+		}
 	}
+	Free(lp_scale_d);
 	mb->data_sections[0].lp_scale = lp_scale;
 
 	for (i = j = 0; i < n; i += idiv, j++) {

inlaprog/src/inla-parse.c
@@ -18715,12 +18715,10 @@ int inla_parse_lp_scale(inla_tp *mb, dictionary *ini, int sec)
 	}
 
 	ds->lp_scale_in_use = Calloc(INLA_LP_SCALE_MAX, int);
+	GMRFLib_ifill(INLA_LP_SCALE_MAX, 0, ds->lp_scale_in_use);
 
-	for (i = 0; i < INLA_LP_SCALE_MAX; i++) {
-		ds->lp_scale_in_use[i] = 0;
-	}
 	for (i = 0; i < mb->predictor_ndata; i++) {
-		if ((k = (int) ds->lp_scale[i]) >= 0) {
+		if ((k = ds->lp_scale[i]) >= 0) {
 			ds->lp_scale_in_use[k] = 1;
 			GMRFLib_ASSERT(k < INLA_LP_SCALE_MAX, GMRFLib_EPARAMETER);
 		}

inlaprog/src/inla.c
@@ -163,8 +163,8 @@ int R_load_INLA = 0;
 		GMRFLib_matrix_get_row(_link_covariates, idx, ds->link_covariates); \
 	}								\
 	double _lp_scale = 1.0;						\
-	if (ds->lp_scale && ds->lp_scale[idx] >= 0) {					\
-		_lp_scale = ds->lp_scale_beta[(int)ds->lp_scale[idx]][thread_id][0]; \
+	if (ds->lp_scale && ds->lp_scale[idx] >= 0) {			\
+		_lp_scale = ds->lp_scale_beta[ds->lp_scale[idx]][thread_id][0]; \
 	}
 
 

inlaprog/src/inla.h
@@ -983,10 +983,10 @@ typedef struct {
 	double *offset;
 	inla_tp *mb;					       /* to get the off_.... */
 
-	double *lp_scale;				       /* index vector */
 	double ***lp_scale_beta;
-	int *lp_scale_nfixed;
+	int *lp_scale;					       /* index vector */
 	int *lp_scale_in_use;
+	int *lp_scale_nfixed;
 	Prior_tp *lp_scale_nprior;
 
 	/*

commit c5ad9209b0dbfba3879120a2b51c38b52c8a9a75

Fix an regression in commit cc7433a8abefebea8012c521f986d5923e97a321

inlaprog/src/inla-likelihood.c
@@ -629,19 +629,23 @@ int inla_read_data_likelihood(inla_tp *mb, dictionary *UNUSED(ini), int UNUSED(s
 
 	double *lp_scale_d = NULL;			       /* I need a tmp one to be double */
 	int n_lp_scale = 0;
+	int *lp_scale = Calloc(mb->predictor_ndata, int);
+
 	inla_read_data_all(&lp_scale_d, &n_lp_scale, ds->lp_scale_file.name, NULL);
 	if (n_lp_scale) {
 		assert(n_lp_scale == mb->predictor_ndata);
-	}
-	int *lp_scale = Calloc(mb->predictor_ndata, int);
 #pragma omp simd
-	for (int i3 = 0; i3 < n_lp_scale; i3++) {
-		if (ISNAN(lp_scale_d[i3])) {
-			lp_scale[i3] = -1;
-		} else {
-			lp_scale[i3] = (int) lp_scale_d[i3] - 1;
+		for (int i3 = 0; i3 < n_lp_scale; i3++) {
+			if (ISNAN(lp_scale_d[i3])) {
+				lp_scale[i3] = -1;
+			} else {
+				lp_scale[i3] = (int) lp_scale_d[i3] - 1;
+			}
 		}
+	} else {
+		GMRFLib_ifill(mb->predictor_ndata, -1, lp_scale);
 	}
+
 	Free(lp_scale_d);
 	mb->data_sections[0].lp_scale = lp_scale;
 