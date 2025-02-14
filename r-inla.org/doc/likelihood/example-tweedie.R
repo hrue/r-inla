d804e2c13d3f01d6f0aff160f5471066aa55237f
Haavard Rue <haavard.rue@kaust.edu.sa>
2025-02-14 12:18:29 +0300

    Updated example-tweedie.R in the documentaiton

--- r-inla.org/doc/likelihood/example-tweedie.R
+++ r-inla.org/doc/likelihood/example-tweedie.R
@@ -15,16 +15,11 @@ for(i in 1:n) {
 
 r <- inla(y ~ 1 + x,
           data = data.frame(y, x),
-          ## offset = rep(log(mean(y)), n), 
           family = "tweedie",
           control.family = list(hyper =  list(
-                                    theta1 = list(initial = 0), 
-                                    theta2 = list(initial = -4,
-                                                  prior = "loggamma",
-                                                  param = c(100, 100)))), 
-          ## control.fixed = list(prec = 0, prec.intercept = 1), 
-          ## control.inla = list(cmin = 0, b.strategy = "skip"), 
-          ## inla.mode = "experimental", 
-          num.threads = "4:1", 
+                                    p = list(initial = 0), 
+                                    phi = list(initial = -4,
+                                               prior = "loggamma",
+                                               param = c(100, 100)))), 
           verbose = T)
 summary(r)
