c1351d1741c02a2dc987bb43d81afab4f955470c
Haavard Rue <haavard.rue@kaust.edu.sa>
2025-05-12 11:47:45 +0300

    Test

--- gmrflib/blockupdate.c
+++ gmrflib/blockupdate.c
@@ -269,7 +269,10 @@ int GMRFLib_2order_approx_core(int thread_id, int *lcache_idx, double *a, double
 #pragma omp critical (Name_4eb4719ffe22f0af964510f0aec612baccccbb0d)
 				if (!(w->wf[stenc])) {
 					int ok = 0;
+					int ntry = 0;
 					while(ok == 0) {
+						try++;
+						
 						double *ww = Calloc(3 * wlength, double);
 						ww[0] = 0.0833333333333333333333333;
 						ww[1] = -0.666666666666666666666667;
@@ -291,11 +294,11 @@ int GMRFLib_2order_approx_core(int thread_id, int *lcache_idx, double *a, double
 						int nnode_ptr = GMRFLib_numa_node_of_ptr(ww);
 
 						if (nnode_ptr != nnode) {
-							printf("allocate MEM but first touch fail %1d %1d\n", nnode, nnode_ptr);
+							printf("allocate MEM but first touch fail %1d %1d try %1d\n", nnode, nnode_ptr, try);
 							Free(ww);
 						} else {
 							ok = 1;
-							printf("allocate MEM first touch OK %1d %1d\n", nnode, nnode_ptr);
+							printf("allocate MEM first touch OK %1d %1d try %1d\n", nnode, nnode_ptr, try);
 						}
 					}
 				}
