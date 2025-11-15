de8ef8e642cf030d4cc75a2df575f137326eebd9
Haavard Rue <haavard.rue@kaust.edu.sa>
2025-11-15 15:37:17 +0300

    Always use 'fsort'

--- gmrflib/fsort.c
+++ gmrflib/fsort.c
@@ -1,3 +1,5 @@
+#include "GMRFLib/GMRFLib.h"
+
 #include <stddef.h>
 
 #pragma GCC diagnostic push
@@ -12,11 +14,34 @@
 
 void quadfluxsort(void *array, size_t nmemb, size_t size, int (*cmp)(const void *, const void *))
 {
-	if(nmemb <= 64L) {
-		quadsort(array, nmemb, size, cmp);
-	} else {
-		fluxsort(array, nmemb, size, cmp);
-	}
+#if 0
+	void *a = (void *) malloc(nmemb * size);
+	void *aa = (void *) malloc(nmemb * size);
+
+	Memcpy(a, array, nmemb * size);
+	Memcpy(aa, array, nmemb * size);
+
+	static double win = 0.0;
+	double tq = 0.0, tf = 0.0;
+
+	tq = -GMRFLib_timer();
+	quadsort(a, nmemb, size, cmp);
+	tq += GMRFLib_timer();
+		
+	tf = -GMRFLib_timer();
+	fluxsort(aa, nmemb, size, cmp);
+	tf += GMRFLib_timer();
+
+	double lwin = ABS(tf - tq);
+	win += lwin;
+	printf("SORT n %1d winner %s win %g total.win %g\n", (int) nmemb, (tf < tq ? "f" : "q"), lwin, win);
+
+	Free(a);
+	Free(aa);
+#endif
+		
+	// quadsort(array, nmemb, size, cmp);
+	fluxsort(array, nmemb, size, cmp);
 }
 
 #pragma GCC diagnostic pop
