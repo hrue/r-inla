0e744ce44e2f7e9d1d8a5a5f42574e189dfd6c3e
Haavard Rue <haavard.rue@kaust.edu.sa>
2026-01-10 18:59:31 +0300

    Updated _pack for SVE

--- gmrflib/intrinsics/aarch64/pack.h
+++ gmrflib/intrinsics/aarch64/pack.h
@@ -1,14 +1,23 @@
-// disabled: not yet tested
+// looks like the SVE version is slower then NEON, so I disable it for the moment
 #if 0 && defined(__ARM_FEATURE_SVE) && defined(__ARM_NEON_SVE_BRIDGE)
 	size_t i = 0;
+	const size_t vl = svcntd();
+	svbool_t pg_all = svptrue_b64();
+	while (i + (2 * vl) <= n) {
+		svint64_t idx0 = svld1sw_s64(pg_all, ia + i);
+		svint64_t idx1 = svld1sw_s64(pg_all, ia + i + vl);
+		svfloat64_t val0 = svld1_gather_s64index_f64(pg_all, a, idx0);
+		svfloat64_t val1 = svld1_gather_s64index_f64(pg_all, a, idx1);
+		svst1_f64(pg_all, y + i,      val0);
+		svst1_f64(pg_all, y + i + vl, val1);
+		i += 2 * vl;
+	}
 	while (i < n) {
 		svbool_t pg = svwhilelt_b64(i, n);
-		svuint32_t idx32 = svld1_u32(pg, (uint32_t*)(ia + i));
-		svuint64_t idx = svunziplo_u64(svreinterpret_u64_u32(idx32), svdup_n_u64(0));
-		svuint64_t offset = svmul_n_u64_z(pg, idx, 8);
-		svfloat64_t gathered = svld1_gather_u64offset_f64(pg, a, offset);
-		svst1_f64(pg, y + i, gathered);
-		i += svcntd();
+		svint64_t idx = svld1sw_s64(pg, ia + i);
+		svfloat64_t val = svld1_gather_s64index_f64(pg, a, idx);
+		svst1_f64(pg, y + i, val);
+		i += vl;
 	}
 #else
 	size_t i = 0;
