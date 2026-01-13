f66052d46dc6adcfdf3c547972bed59c0d433205
Haavard Rue <haavard.rue@kaust.edu.sa>
2026-01-14 01:22:53 +0300

    Fix

--- gmrflib/intrinsics/simde/dscale2-avx2.h
+++ gmrflib/intrinsics/simde/dscale2-avx2.h
@@ -1,9 +1,9 @@
 	simde__m256d scalar = simde_mm256_set1_pd(a);
 	int limit = n & ~3;
 	for (int i = 0; i < limit; i += 4) {
-		__m256d yvec = _mm256_loadu_pd(&x[i]);
-		__m256d xvec = _mm256_mul_pd(yvec, scalar);
-		_mm256_storeu_pd(&y[i], xvec);
+		simde__m256d yvec = simde_mm256_loadu_pd(&x[i]);
+		simde__m256d xvec = simde_mm256_mul_pd(yvec, scalar);
+		simde_mm256_storeu_pd(&y[i], xvec);
 	}
 	for (int i = limit; i < n; i++) {
 		y[i] = x[i] * a;
