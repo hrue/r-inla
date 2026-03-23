c19ba5f8edf1445b727e12da5ded4235a1212032
Haavard Rue <haavard.rue@kaust.edu.sa>
2026-03-23 18:02:08 +0300

    Add cloglike example of beta distribution and covariate

--- rinla/vignettes/myinit.R
+++ rinla/vignettes/myinit.R
@@ -1,8 +1,8 @@
-inla.setOption(num.threads = "1:1")
+inla.setOption(num.threads = "2:1:1")
 if (FALSE) {
     INLA:::inla.my.update()
     inla.setOption(inla.call="inla.mkl.work")
-    inla.setOption(num.threads = "4:1")
+    inla.setOption(num.threads = "4:1:2")
     ##inla.setOption(smtp = "pardiso")
     ##inla.setOption(pardiso.license="~/sys/licenses/pardiso.lic")
 }
