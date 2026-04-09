d9adefec804764a7e596d6ed1fab6253aeb76131
Haavard Rue <haavard.rue@kaust.edu.sa>
2026-04-09 21:14:02 +0300

    Convert intrinsics to simde

--- gmrflib/intrinsics/simde/dsum-avx2.h
+++ gmrflib/intrinsics/simde/dsum-avx2.h
@@ -1,25 +1,25 @@
 	if (n >= 16) {
-		__m256d sum0 = _mm256_setzero_pd();
-		__m256d sum1 = _mm256_setzero_pd();
-		__m256d sum2 = _mm256_setzero_pd();
-		__m256d sum3 = _mm256_setzero_pd();
+		__m256d sum0 = simde_mm256_setzero_pd();
+		__m256d sum1 = simde_mm256_setzero_pd();
+		__m256d sum2 = simde_mm256_setzero_pd();
+		__m256d sum3 = simde_mm256_setzero_pd();
 		int i = 0;
 		for (; i <= n - 16; i += 16) {
-			sum0 = _mm256_add_pd(sum0, _mm256_loadu_pd(&x[i]));
-			sum1 = _mm256_add_pd(sum1, _mm256_loadu_pd(&x[i + 4]));
-			sum2 = _mm256_add_pd(sum2, _mm256_loadu_pd(&x[i + 8]));
-			sum3 = _mm256_add_pd(sum3, _mm256_loadu_pd(&x[i + 12]));
+			sum0 = simde_mm256_add_pd(sum0, simde_mm256_loadu_pd(&x[i]));
+			sum1 = simde_mm256_add_pd(sum1, simde_mm256_loadu_pd(&x[i + 4]));
+			sum2 = simde_mm256_add_pd(sum2, simde_mm256_loadu_pd(&x[i + 8]));
+			sum3 = simde_mm256_add_pd(sum3, simde_mm256_loadu_pd(&x[i + 12]));
 		}
-		__m256d final_sum = _mm256_add_pd(_mm256_add_pd(sum0, sum1), 
-						  _mm256_add_pd(sum2, sum3));
+		__m256d final_sum = simde_mm256_add_pd(simde_mm256_add_pd(sum0, sum1), 
+						  simde_mm256_add_pd(sum2, sum3));
 
-		__m128d low = _mm256_castpd256_pd128(final_sum);
-		__m128d high = _mm256_extractf128_pd(final_sum, 1);
-		__m128d combined = _mm_add_pd(low, high);
+		__m128d low = simde_mm256_castpd256_pd128(final_sum);
+		__m128d high = simde_mm256_extractf128_pd(final_sum, 1);
+		__m128d combined = simde_mm_add_pd(low, high);
     
-		__m128d shuffled = _mm_unpackhi_pd(combined, combined);
-		__m128d result_vec = _mm_add_pd(combined, shuffled);
-		double r = _mm_cvtsd_f64(result_vec);
+		__m128d shuffled = simde_mm_unpackhi_pd(combined, combined);
+		__m128d result_vec = simde_mm_add_pd(combined, shuffled);
+		double r = simde_mm_cvtsd_f64(result_vec);
 		for (; i < n; i++) {
 			r += x[i];
 		}
--- gmrflib/intrinsics/simde/dsum-sse2.h
+++ gmrflib/intrinsics/simde/dsum-sse2.h
@@ -1,19 +1,19 @@
 	if (n >= 8) {
-		__m128d s0 = _mm_setzero_pd();
-		__m128d s1 = _mm_setzero_pd();
-		__m128d s2 = _mm_setzero_pd();
-		__m128d s3 = _mm_setzero_pd();
+		__m128d s0 = simde_mm_setzero_pd();
+		__m128d s1 = simde_mm_setzero_pd();
+		__m128d s2 = simde_mm_setzero_pd();
+		__m128d s3 = simde_mm_setzero_pd();
 		int i = 0;
 		for (; i <= n - 8; i += 8) {
-			s0 = _mm_add_pd(s0, _mm_loadu_pd(&x[i]));
-			s1 = _mm_add_pd(s1, _mm_loadu_pd(&x[i + 2]));
-			s2 = _mm_add_pd(s2, _mm_loadu_pd(&x[i + 4]));
-			s3 = _mm_add_pd(s3, _mm_loadu_pd(&x[i + 6]));
+			s0 = simde_mm_add_pd(s0, simde_mm_loadu_pd(&x[i]));
+			s1 = simde_mm_add_pd(s1, simde_mm_loadu_pd(&x[i + 2]));
+			s2 = simde_mm_add_pd(s2, simde_mm_loadu_pd(&x[i + 4]));
+			s3 = simde_mm_add_pd(s3, simde_mm_loadu_pd(&x[i + 6]));
 		}
-		__m128d final_sum = _mm_add_pd(_mm_add_pd(s0, s1), _mm_add_pd(s2, s3));
-		__m128d shuffled = _mm_unpackhi_pd(final_sum, final_sum);
-		__m128d res_vec = _mm_add_pd(final_sum, shuffled);
-		double r = _mm_cvtsd_f64(res_vec);
+		__m128d final_sum = simde_mm_add_pd(simde_mm_add_pd(s0, s1), simde_mm_add_pd(s2, s3));
+		__m128d shuffled = simde_mm_unpackhi_pd(final_sum, final_sum);
+		__m128d res_vec = simde_mm_add_pd(final_sum, shuffled);
+		double r = simde_mm_cvtsd_f64(res_vec);
 		for (; i < n; i++) {
 			r += x[i];
 		}
