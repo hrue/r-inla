8a55452c6fa8d9142f329ae90a6183c502a6dbc8
Haavard Rue <haavard.rue@kaust.edu.sa>
2026-05-01 12:35:51 +0300

    Fix variable names

--- gmrflib/intrinsics/simde/pack-avx512f.h
+++ gmrflib/intrinsics/simde/pack-avx512f.h
@@ -1,14 +1,14 @@
 {
 	int i = 0;
 	for (; i <= n - 16; i += 16) {
-		simde__m256i v_idx0 = simde_mm256_loadu_si256((const simde__m256i*)&idx[i]);
-		simde__m256i v_idx1 = simde_mm256_loadu_si256((const simde__m256i*)&idx[i + 8]);
-		simde__m512d v_val0 = simde_mm512_i32gather_pd(v_idx0, x, 8);
-		simde__m512d v_val1 = simde_mm512_i32gather_pd(v_idx1, x, 8);
+		simde__m256i v_idx0 = simde_mm256_loadu_si256((const simde__m256i*)&ia[i]);
+		simde__m256i v_idx1 = simde_mm256_loadu_si256((const simde__m256i*)&ia[i + 8]);
+		simde__m512d v_val0 = simde_mm512_i32gather_pd(v_idx0, a, 8);
+		simde__m512d v_val1 = simde_mm512_i32gather_pd(v_idx1, a, 8);
 		simde_mm512_storeu_pd(&y[i], v_val0);
 		simde_mm512_storeu_pd(&y[i + 8], v_val1);
 	}
 	for (; i < n; i++) {
-		y[i] = x[idx[i]];
+		y[i] = a[ia[i]];
 	}
 }
