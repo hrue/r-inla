142011e599700fd28820183e5afb168af495b968
Haavard Rue <haavard.rue@kaust.edu.sa>
2026-05-21 10:45:41 +0200

    Add timing code for LLT solve
    Default off

--- gmrflib/sparse-interface.c
+++ gmrflib/sparse-interface.c
@@ -1002,7 +1002,6 @@ int GMRFLib_solve_llt_sparse_matrix(double *rhs, int nrhs, GMRFLib_sm_fact_tp *s
 	} else if (sm_fact->smtp == GMRFLib_SMTP_PARDISO) {
 		GMRFLib_EWRAP1(GMRFLib_pardiso_solve_LLT(sm_fact->PARDISO_fact, rhs, rhs, nrhs));
 	} else if (sm_fact->smtp == GMRFLib_SMTP_STILES) {
-		printf("Call stiles solve llt with nrhs %d\n", nrhs);
 		GMRFLib_stiles_idx_tp s_idx = { 0, -1, nrhs };
 		if (stiles_idx) {
 			s_idx.in_group = stiles_idx->in_group;
@@ -1012,7 +1011,22 @@ int GMRFLib_solve_llt_sparse_matrix(double *rhs, int nrhs, GMRFLib_sm_fact_tp *s
 			s_idx.within_group = problem->stiles_idx->within_group;
 		}
 		GMRFLib_stiles_set_idx(&s_idx, nrhs);
+#if 0
+		static double tref = 0.0;
+		static double trefc = 0.0;
+		static double trefc_min = 0;
+		if (trefc_min >= 25 && nrhs == 1) 
+			tref += -GMRFLib_timer();
+#endif
 		GMRFLib_stiles_solve_LLT(&s_idx, rhs);
+#if 0
+		if (trefc_min >= 25 && nrhs == 1) {
+			tref += GMRFLib_timer();
+			trefc += nrhs;
+			printf("Solve %.6f\n", tref / trefc);
+		}
+		trefc_min++;
+#endif
 	} else {
 		GMRFLib_ERROR(GMRFLib_ESNH);
 	}
