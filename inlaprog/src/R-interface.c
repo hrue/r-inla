72967f7538e1f527a5b94a5b6527f611aad46f13
Haavard Rue <haavard.rue@kaust.edu.sa>
2026-06-30 16:30:26 +0200

    FIx: Rinterface.h is not for Windows

--- inlaprog/src/R-interface.c
+++ inlaprog/src/R-interface.c
@@ -7,12 +7,19 @@
 #include <omp.h>
 #include <unistd.h>
 
-#define CSTACK_DEFNS 1
 #include <R.h>
 #include <Rembedded.h>
 #include <Rinternals.h>
 #include <Rdefines.h>
 #include <Rinterface.h>
+
+// Rinterface.h is Unix-only; R.dll still exports this symbol 
+#if !defined(_WIN32)
+#       define CSTACK_DEFNS 1
+#       include <Rinterface.h>
+#else
+extern uintptr_t R_CStackLimit;
+#endif
 
 #include "GMRFLib/timer.h"
 extern char *GMRFLib_tmpdir;
