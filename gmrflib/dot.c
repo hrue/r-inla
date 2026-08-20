a557f4b80d6a4e9dcf521f86b9ce4117e6d43ee7
Haavard Rue <haavard.rue@kaust.edu.sa>
2026-08-20 09:00:59 +0300

    Add 'flatten' to force inline for critical functions

--- gmrflib/dot.c
+++ gmrflib/dot.c
@@ -137,7 +137,7 @@ forceinline double GMRFLib_sparse_ddot_(GMRFLib_idxval_tp *__restrict ELM_, doub
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wattributes"
 __attribute__((optimize("O3")))
-    __attribute__((target_clones(INLA_CLONE_TARGETS "default")))
+    __attribute__((flatten, target_clones(INLA_CLONE_TARGETS "default")))
 double GMRFLib_sparse_ddot_group_(GMRFLib_idxval_tp *__restrict ELM_, double *__restrict ARR_)
 {
 	double value = 0.0;
@@ -183,7 +183,7 @@ double GMRFLib_sparse_ddot_group_(GMRFLib_idxval_tp *__restrict ELM_, double *__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wattributes"
 __attribute__((optimize("O3")))
-    __attribute__((target_clones(INLA_CLONE_TARGETS "default")))
+    __attribute__((flatten, target_clones(INLA_CLONE_TARGETS "default")))
 double GMRFLib_sparse_ddot_group_simple_(GMRFLib_idxval_tp *__restrict ELM_, double *__restrict ARR_)
 {
 	// in this case, there is only one group giving dense vector calls to 'dot' or 'sum'
