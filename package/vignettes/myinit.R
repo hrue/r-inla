e2cd6955399cc395fe3a4264a7823ef399c57937
Esmail Abdul Fattah <esmail.abdulfattah@kaust.edu.sa>
2026-09-16 13:53:57 +0300

    resync package

--- package/vignettes/myinit.R
+++ package/vignettes/myinit.R
@@ -1,8 +1,2 @@
-inla.setOption(num.threads = "2:1:1")
-if (FALSE) {
-    INLA:::inla.my.update()
-    inla.setOption(inla.call="inla.mkl.work")
-    inla.setOption(num.threads = "4:1:2")
-    ##inla.setOption(smtp = "pardiso")
-    ##inla.setOption(pardiso.license="~/sys/licenses/pardiso.lic")
-}
+inla.setOption(inla.call="/home/hrue/bin/inla.mkl.work")
+inla.setOption(num.threads = "8:1:2")
